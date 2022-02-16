896084022ffd13590cacc2c217fb700efc8af646
ardeay <apuzzo@gmail.com>
2022-02-16 14:24:16 -0800

    config setup and test script`

--- scripts/zesty-nextjs.js
+++ scripts/zesty-nextjs.js
@@ -29,32 +29,97 @@
 // set domain, consider that domains have password protection
 
 let zestyConfig = {
-    "domain_stage" : `https://kfg6bckb-dev.webengine.zesty.io`,
-    "domain_published" : `https://www.zesty.io`,
-    "password" : `12345`,
+    "stage" : `https://kfg6bckb-dev.webengine.zesty.io`,
+    "production" : `https://www.zesty.io`,
+    "stage_password" : ``,
     "auth" : ``, // this can set APP_SID as cookie to get access, or a user login
-    "src_dir" : `src` // leave blank for src/ as default
+    "src_dir" : `/src` // leave blank for /src as default
 }
 // lives as a script file (zesty-config.js ?)
 // for now: can use password param for preview url (readonly)
 // if it creates developer token, need to account for token expiration
 // 0b include needed packages
 const fs = require("fs") // need to check folder and write files
+const chalk = require('chalk');
+const { promisify } = require('util');
+const { exit } = require("process");
+const readFileAsync = promisify(fs.readFile)
+const writeFileAsync = promisify(fs.writeFile)
+// access doesnt promisfy, needs a custom function
+const accessAsync = (s) => new Promise(r => fs.access(s, fs.F_OK, e => r(!e)))
+//console output shorthands
+const successMark = `      ${chalk.gray.bold('[')}${chalk.green.bold('V')}${chalk.gray.bold(']')}`
+const failMark = `      ${chalk.gray.bold('[')}${chalk.red.bold('X')}${chalk.gray.bold(']')}`
+const warningMark = `      ${chalk.gray.bold('[')}${chalk.yellow.bold('O')}${chalk.gray.bold(']')}`
+const stageMark = (num) => `   ${chalk.gray.bold('[')}${chalk.white.bold(num)}${chalk.gray.bold(']')}`
+
+
 
 // 1
 // spot check variables and 
 
-function checkVariables(zestyConfig){
+async function checkVariables(zestyConfig){
     // verify https is or isnt there
-    // verify the src_dir exists and is a folder
-    // return true or err with message
-    console.log(`Working Project
-     Directory: ${process.cwd()}`);
+    
+    // output the working directory
+    console.log(`${successMark} Working Project Directory: ${process.cwd()}`);
     const workingDir = process.cwd();
     const srcDir = workingDir + zestyConfig.src_dir
+    let success = true;
+    // verify the src_dir exists and is a folder
+    success = await accessAsync(srcDir)
+    if(success) {
+        console.log(`${successMark} src_dir accessible: ${srcDir}`)
+    } else {
+        console.log(`${failMark} src_dir does not exist: ${srcDir}`);
+        success = false
+    }
+    // check the domain variables
+    // stage check
+    if(zestyConfig.hasOwnProperty('stage') && zestyConfig.stage !== ''){
+        console.log(`${successMark} Stage value present: ${zestyConfig.stage} `);
+    } else {
+        console.log(chalk.red.bold(`${failMark} stage value missing, set this value to your zesty stage domain e.g. https://xyz-dev.preview.zesty.io`));
+        success = false
+    }
+    // production check
+    if(zestyConfig.hasOwnProperty('production') && zestyConfig.production !== ''){
+        console.log(`${successMark} Production value present: ${zestyConfig.production} `);
+    } else {
+        console.log(chalk.red.bold(`${failMark} production value missing, set this value to your zesty production domain e.g. https://www.acme.com`));
+        success = false
+    }
+
+    // stage password check
+    if(zestyConfig.hasOwnProperty('stage_password') && zestyConfig.stage_password !== ''){
+        console.log(`${successMark} Stage password present: ${zestyConfig.stage_password} `);
+    } else {
+        console.log(`${warningMark} ${chalk.yellow.bold('Warning!')} No stage password present, this may cause issues if your zesty instance is password protected.`);
+    }
+
+    return success;
+
 }
 
-checkVariables(zestyConfig)
+async function run(){
+    // terminal output of start
+    console.log(`${chalk.gray.bold(' ')}`)
+    console.log(`   ${chalk.gray.bold('-----------------------------------------------------------------------------')}`)
+    console.log(`                          ${chalk.hex('#FF5D0A').bold('Zesty.io')} ${chalk.gray.bold('NextJS')} Integration Script Running`)
+    console.log(`   ${chalk.gray.bold('-----------------------------------------------------------------------------')}`)
+    console.log(`${chalk.gray.bold(' ')}`)
+
+    // Step 1 config check
+    console.log(stageMark(1)+ ' config check')
+    const configCheckSuccess = await checkVariables(zestyConfig)
+
+    if (!configCheckSuccess){
+        console.log(`${chalk.gray.bold('[')}${chalk.red.bold('X')}${chalk.gray.bold(']')} Config not setup Correctly`);
+        exit()
+    }
+}
+
+run();
 
 // 2
 // make network request to ensure the headless or hybrid mode is turned on, return errors or docs otherwise
