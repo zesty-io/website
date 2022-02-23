eb107ade4834732c2002986a0212e460ac5d6c64
ardeay <apuzzo@gmail.com>
2022-02-23 08:43:55 -0800

    new position for zesty models

---
+++ src/components/zesty/PricingTierZestyModel.js
@@ -0,0 +1,54 @@
+/**
+ * Zesty.io Content Model Component
+ * When the ZestyLoader [..slug].js file is used, this component will autoload if it associated with the URL
+ * 
+ * Label: Pricing Tiers 
+ * Name: pricing_tiers 
+ * Model ZUID: 6-c46cb0-mtzfzx
+ * File Created On: Mon Feb 21 2022 07:38:12 GMT-0800 (Pacific Standard Time)
+ * 
+ * Model Fields:
+ * 
+  * name (text)
+ * sort_order (sort)
+ * price (text)
+ * users (text)
+ * content_records (text)
+ * description (textarea)
+ * domain (text)
+ * data_plan (text)
+ * support (text)
+ * features (text)
+ * card_color (dropdown)
+
+ * 
+ * In the render function, text fields can be accessed like {content.field_name}, relationships are arrays,
+ * images are objects {content.image_name.data[0].url}
+ * 
+ * This file is expected to be customized; because of that, it is not overwritten by the integration script.
+ * Model and field changes in Zesty.io will not be reflected in this comment.
+ * 
+ * View and Edit this model's current schema on Zesty.io at https://8-aaeffee09b-7w6v22.manager.zesty.io/schema/6-c46cb0-mtzfzx
+ * 
+ * Data Output Example: https://zesty.org/services/web-engine/introduction-to-parsley/parsley-index#tojson
+ * Images API: https://zesty.org/services/media-storage-micro-dam/on-the-fly-media-optimization-and-dynamic-image-manipulation
+ */
+
+import React  from 'react';
+
+function PricingTierZestyModel({content}) {
+    return (
+        <>
+            {/* Zesty.io Output Example and accessible JSON object for this component. Delete or comment out when needed.  */}
+            <h1 dangerouslySetInnerHTML={{__html:content.meta.web.seo_meta_title}}></h1>
+            <div>{content.meta.web.seo_meta_description}</div>
+            <div style={{background: '#eee', border: '1px #000 solid', margin: '10px', padding: '20px'}}>
+                <h2>Accessible Zesty.io JSON Object</h2>
+                <pre>{JSON.stringify(content, null, 2)}</pre>
+            </div>
+            {/* End of Zesty.io output example */}
+        </>
+    );
+}
+  
+export default PricingTierZestyModel;
---
+++ src/components/zesty/ReviewZestyModel.js
@@ -0,0 +1,49 @@
+/**
+ * Zesty.io Content Model Component
+ * When the ZestyLoader [..slug].js file is used, this component will autoload if it associated with the URL
+ * 
+ * Label: Reviews 
+ * Name: reviews 
+ * Model ZUID: 6-082af4-ckkm8z
+ * File Created On: Mon Feb 21 2022 07:38:12 GMT-0800 (Pacific Standard Time)
+ * 
+ * Model Fields:
+ * 
+  * title (text)
+ * review (textarea)
+ * reviewer_title (text)
+ * reviewer_headshot (images)
+ * category (dropdown)
+ * company (text)
+
+ * 
+ * In the render function, text fields can be accessed like {content.field_name}, relationships are arrays,
+ * images are objects {content.image_name.data[0].url}
+ * 
+ * This file is expected to be customized; because of that, it is not overwritten by the integration script.
+ * Model and field changes in Zesty.io will not be reflected in this comment.
+ * 
+ * View and Edit this model's current schema on Zesty.io at https://8-aaeffee09b-7w6v22.manager.zesty.io/schema/6-082af4-ckkm8z
+ * 
+ * Data Output Example: https://zesty.org/services/web-engine/introduction-to-parsley/parsley-index#tojson
+ * Images API: https://zesty.org/services/media-storage-micro-dam/on-the-fly-media-optimization-and-dynamic-image-manipulation
+ */
+
+import React  from 'react';
+
+function ReviewZestyModel({content}) {
+    return (
+        <>
+            {/* Zesty.io Output Example and accessible JSON object for this component. Delete or comment out when needed.  */}
+            <h1 dangerouslySetInnerHTML={{__html:content.meta.web.seo_meta_title}}></h1>
+            <div>{content.meta.web.seo_meta_description}</div>
+            <div style={{background: '#eee', border: '1px #000 solid', margin: '10px', padding: '20px'}}>
+                <h2>Accessible Zesty.io JSON Object</h2>
+                <pre>{JSON.stringify(content, null, 2)}</pre>
+            </div>
+            {/* End of Zesty.io output example */}
+        </>
+    );
+}
+  
+export default ReviewZestyModel;
