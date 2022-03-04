665fdc5bdc04a5df5e4e87848c4cd363665bb882
Bobbi Tarkany <bl.tarkany@gmail.com>
2022-03-04 07:26:31 -0800

    Add headliner component to AboutZestyModel.js

--- src/components/zesty/AboutZestyModel.js
+++ src/components/zesty/AboutZestyModel.js
@@ -29,23 +29,152 @@
  * Images API: https://zesty.org/services/media-storage-micro-dam/on-the-fly-media-optimization-and-dynamic-image-manipulation
  */
 
-import React  from 'react';
+import React from 'react';
+import Box from '@mui/material/Box';
+import Divider from '@mui/material/Divider';
+import { useTheme } from '@mui/material/styles';
 
-function AboutZestyModel({content}) {
-    return (
-        <>
-            {/* Zesty.io Output Example and accessible JSON object for this component. Delete or comment out when needed.  */}
-            <br/><br/>
-            <h1 dangerouslySetInnerHTML={{__html:content.hero_content}}></h1>
-            <h1 dangerouslySetInnerHTML={{__html:content.meta.web.seo_meta_title}}></h1>
-            <div>{content.meta.web.seo_meta_description}</div>
-            <div style={{background: '#eee', border: '1px #000 solid', margin: '10px', padding: '20px'}}>
-                <h2>Accessible Zesty.io JSON Object</h2>
-                <pre>{JSON.stringify(content, null, 2)}</pre>
-            </div>
-            {/* End of Zesty.io output example */}
-        </>
-    );
-}
+import Main from '../../layouts/Main';
+import Container from 'components/Container';
+
+// Headliner imports
+import { alpha, useTheme } from '@mui/material/styles';
+import Typography from '@mui/material/Typography';
+// Gallery imports
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import Lightbox from 'react-image-lightbox';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import Box from '@mui/material/Box';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import Button from '@mui/material/Button';
+
+function AboutZestyModel({ content }) {
+    const theme = useTheme();
+    const [currentImage, setCurrentImage] = useState(0);
+    const [viewerIsOpen, setViewerIsOpen] = useState(false);
   
+    const openLightbox = (index) => {
+      setCurrentImage(index);
+      setViewerIsOpen(true);
+    };
+  
+    const closeLightbox = () => {
+      setCurrentImage(0);
+      setViewerIsOpen(false);
+    };
+  
+    const isMd = useMediaQuery(theme.breakpoints.up('md'), {
+      defaultMatches: true,
+    });
+  
+    const photos = [
+      {
+        src: 'https://assets.maccarianagency.com/backgrounds/img25.jpg',
+        source: 'https://assets.maccarianagency.com/backgrounds/img25.jpg',
+        rows: 1,
+        cols: 2,
+      },
+      {
+        src: 'https://assets.maccarianagency.com/backgrounds/img22.jpg',
+        source: 'https://assets.maccarianagency.com/backgrounds/img22.jpg',
+        rows: 1,
+        cols: 1,
+      },
+      {
+        src: 'https://assets.maccarianagency.com/backgrounds/img24.jpg',
+        source: 'https://assets.maccarianagency.com/backgrounds/img24.jpg',
+        rows: 1,
+        cols: 1,
+      },
+      {
+        src: 'https://assets.maccarianagency.com/backgrounds/img21.jpg',
+        source: 'https://assets.maccarianagency.com/backgrounds/img21.jpg',
+        rows: 1,
+        cols: 2,
+      },
+    ];
+  return (
+    <>
+      <Main />
+      <Box>
+        <Container>
+          <Box
+            sx={{
+              position: 'relative',
+              '&::after': {
+                position: 'absolute',
+                content: '""',
+                width: '30%',
+                zIndex: 1,
+                top: 0,
+                right: 0,
+                height: '100%',
+                backgroundSize: '18px 18px',
+                backgroundImage: `radial-gradient(${alpha(
+                  theme.palette.primary.dark,
+                  0.4,
+                )} 20%, transparent 20%)`,
+                opacity: 0.2,
+              },
+            }}
+          >
+            <Box position="relative" zIndex={2}>
+              <Typography
+                fontWeight={600}
+                variant={'h2'}
+                gutterBottom
+                align={'center'}
+              >
+                About us
+              </Typography>
+              <Typography
+                variant="h6"
+                color={'text.secondary'}
+                align={'center'}
+                gutterBottom
+              >
+                We take you by hand on each step of the process
+              </Typography>
+              <Typography
+                variant="h6"
+                color={'text.secondary'}
+                align={'center'}
+              >
+                As experts in both design & development, we help you go through
+                the complete process. From your new website idea, to design,
+                development, launch and scale!
+              </Typography>
+            </Box>
+          </Box>
+        </Container>
+        <Container paddingY={'0 !important'}>
+          
+        </Container>
+        {/* Zesty.io Output Example and accessible JSON object for this component. Delete or comment out when needed.  */}
+        <br />
+        <br />
+        <h1 dangerouslySetInnerHTML={{ __html: content.hero_content }}></h1>
+        <h1
+          dangerouslySetInnerHTML={{ __html: content.meta.web.seo_meta_title }}
+        ></h1>
+        <div>{content.meta.web.seo_meta_description}</div>
+        <div
+          style={{
+            background: '#eee',
+            border: '1px #000 solid',
+            margin: '10px',
+            padding: '20px',
+          }}
+        >
+          <h2>Accessible Zesty.io JSON Object</h2>
+          <pre>{JSON.stringify(content, null, 2)}</pre>
+        </div>
+        {/* End of Zesty.io output example */}
+      </Box>
+      <Main />
+    </>
+  );
+}
+
 export default AboutZestyModel;
