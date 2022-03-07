b204844180e7f90973e1ec81e6d36abc85347ddc
Bobbi Tarkany <bl.tarkany@gmail.com>
2022-03-07 08:32:35 -0800

    added component imports from blocks

--- src/views/zesty/New.js
+++ src/views/zesty/New.js
@@ -25,13 +25,25 @@
  */
 
 import React  from 'react';
+import Box from '@mui/material/Box';
+import { useTheme } from '@mui/material/styles';
+
+import Container from 'components/Container';
+
+// blocks
+import { SimpleHeroWithCta } from '../../blocks/heroes';
+import { Breadcrumb } from '../../blocks/progressSteps';
+import { Result } from '../../blocks/formLayouts';
+import { Newsletter } from '../../blocks/newsletters';
+
 
 function New({content}) {
     return (
         <>
+
             {/* Zesty.io Output Example and accessible JSON object for this component. Delete or comment out when needed.  */}
-            <h1 dangerouslySetInnerHTML={{__html:content.meta.web.seo_meta_title}}></h1>
-            <div>{content.meta.web.seo_meta_description}</div>
+            {/* <h1 dangerouslySetInnerHTML={{__html:content.meta.web.seo_meta_title}}></h1>
+            <div>{content.meta.web.seo_meta_description}</div> */}
             <div style={{background: '#eee', border: '1px #000 solid', margin: '10px', padding: '20px'}}>
                 <h2>Accessible Zesty.io JSON Object</h2>
                 <pre>{JSON.stringify(content, null, 2)}</pre>
