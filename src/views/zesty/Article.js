041fb690466988bdeb7b20fe93222d976453a625
David Naimi <d88naimi@gmail.com>
2022-03-08 12:44:24 -0800

    loaded in articles template

--- src/views/zesty/Article.js
+++ src/views/zesty/Article.js
@@ -1,14 +1,14 @@
 /**
  * Zesty.io Content Model Component
  * When the ZestyLoader [..slug].js file is used, this component will autoload if it associated with the URL
- * 
- * Label: Articles 
- * Name: articles 
+ *
+ * Label: Articles
+ * Name: articles
  * Model ZUID: 6-45a908-qfw88c
  * File Created On: Fri Mar 04 2022 12:16:05 GMT-0800 (Pacific Standard Time)
- * 
+ *
  * Model Fields:
- * 
+ *
   * hero_image (images)
  * article (wysiwyg_advanced)
  * title (text)
@@ -17,34 +17,135 @@
  * date (date)
  * tags (one_to_many)
 
- * 
+ *
  * In the render function, text fields can be accessed like {content.field_name}, relationships are arrays,
  * images are objects {content.image_name.data[0].url}
- * 
+ *
  * This file is expected to be customized; because of that, it is not overwritten by the integration script.
  * Model and field changes in Zesty.io will not be reflected in this comment.
- * 
+ *
  * View and Edit this model's current schema on Zesty.io at https://8-aaeffee09b-7w6v22.manager.zesty.io/schema/6-45a908-qfw88c
- * 
+ *
  * Data Output Example: https://zesty.org/services/web-engine/introduction-to-parsley/parsley-index#tojson
  * Images API: https://zesty.org/services/media-storage-micro-dam/on-the-fly-media-optimization-and-dynamic-image-manipulation
  */
 
 import React  from 'react';
+import Box from '@mui/material/Box';
+import { useTheme } from '@mui/material/styles';
+import Grid from '@mui/material/Grid';
+import useMediaQuery from '@mui/material/useMediaQuery';
+
+import FillerContent from '../../../src/components/FillerContent'
+
+
+import Container from '../../components/Container';
+import {
+  Content,
+  FooterNewsletter,
+  Hero,
+  SidebarArticles,
+  SidebarNewsletter,
+  SimilarStories,
+} from '../BlogArticle/components';
 
 function Article({content}) {
+      const theme = useTheme();
+      const isMd = useMediaQuery(theme.breakpoints.up('md'), {
+        defaultMatches: true,
+      });
     return (
-        <>
-            {/* Zesty.io Output Example and accessible JSON object for this component. Delete or comment out when needed.  */}
-            <h1 dangerouslySetInnerHTML={{__html:content.meta.web.seo_meta_title}}></h1>
-            <div>{content.meta.web.seo_meta_description}</div>
-            <div style={{background: '#eee', border: '1px #000 solid', margin: '10px', padding: '20px'}}>
-                <h2>Accessible Zesty.io JSON Object</h2>
-                <pre>{JSON.stringify(content, null, 2)}</pre>
-            </div>
-            {/* End of Zesty.io output example */}
-        </>
+      <>
+        <Box>
+          <Hero
+            image={
+              content.hero_image?.data
+                ? content.hero_image.data[0].url
+                : FillerContent.image
+            }
+          />
+          <Container>
+            <Grid container spacing={4}>
+              <Grid item xs={12} md={8}>
+                <Box
+                  dangerouslySetInnerHTML={{
+                    __html: content.article,
+                  }}
+                ></Box>
+
+              </Grid>
+              <Grid item xs={12} md={4}>
+                {isMd ? (
+                  <Box marginBottom={4}>
+                    <SidebarArticles />
+                  </Box>
+                ) : null}
+                <SidebarNewsletter />
+              </Grid>
+            </Grid>
+          </Container>
+          <Box
+            component={'svg'}
+            preserveAspectRatio="none"
+            xmlns="http://www.w3.org/2000/svg"
+            x="0px"
+            y="0px"
+            viewBox="0 0 1920 100.1"
+            sx={{
+              marginBottom: -1,
+              width: 1,
+            }}
+          >
+            <path
+              fill={theme.palette.alternate.main}
+              d="M0,0c0,0,934.4,93.4,1920,0v100.1H0L0,0z"
+            ></path>
+          </Box>
+        </Box>
+        <Box bgcolor={'alternate.main'}>
+          <Container>
+            <SimilarStories />
+          </Container>
+          <Container>
+            <FooterNewsletter />
+          </Container>
+          <Box
+            component={'svg'}
+            preserveAspectRatio="none"
+            xmlns="http://www.w3.org/2000/svg"
+            x="0px"
+            y="0px"
+            viewBox="0 0 1920 100.1"
+            sx={{
+              marginBottom: -1,
+              width: 1,
+            }}
+          >
+            <path
+              fill={theme.palette.background.paper}
+              d="M0,0c0,0,934.4,93.4,1920,0v100.1H0L0,0z"
+            ></path>
+          </Box>
+        </Box>
+        {/* Zesty.io Output Example and accessible JSON object for this component. Delete or comment out when needed.  */}
+        <h1
+          dangerouslySetInnerHTML={{ __html: content.meta.web.seo_meta_title }}
+        ></h1>
+        <div>{content.meta.web.seo_meta_description}</div>
+        <div
+          style={{
+            background: '#eee',
+            border: '1px #000 solid',
+            margin: '10px',
+            padding: '20px',
+          }}
+        >
+          <h2>Accessible Zesty.io JSON Object</h2>
+          <pre>{JSON.stringify(content, null, 2)}</pre>
+        </div>
+        {/* End of Zesty.io output example */}
+      </>
     );
 }
-  
+
 export default Article;
