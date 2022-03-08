f5b981253e35e08cae6cbc7065a401ca581b261a
Bobbi Tarkany <bl.tarkany@gmail.com>
2022-03-08 09:00:14 -0800

    created simple hero with single cta for News Page

---
+++ src/blocks/heroes/SimpleHeroWithSingleCta/SimpleHeroWithSingleCta.js
@@ -0,0 +1,87 @@
+/* eslint-disable react/no-unescaped-entities */
+import React from 'react';
+import { alpha, useTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+import Container from 'components/Container';
+
+const Hero = () => {
+  const theme = useTheme();
+  const isMd = useMediaQuery(theme.breakpoints.up('md'), {
+    defaultMatches: true,
+  });
+
+  return (
+    <Container
+      sx={{
+        position: 'relative',
+        '&::after': {
+          position: 'absolute',
+          content: '""',
+          width: '20%',
+          zIndex: 1,
+          top: 0,
+          left: 0,
+          height: '100%',
+          backgroundSize: '18px 18px',
+          backgroundImage: `radial-gradient(${alpha(
+            theme.palette.primary.dark,
+            0.4,
+          )} 20%, transparent 20%)`,
+          opacity: 0.2,
+        },
+      }}
+    >
+      <Box position={'relative'} zIndex={2}>
+        <Box marginBottom={2}>
+          <Typography
+            variant="h3"
+            color="text.primary"
+            align={'center'}
+            sx={{
+              fontWeight: 700,
+            }}
+          >
+            Welcome to theFront.
+            <br />
+            Develop anything your business needs.
+          </Typography>
+          <Typography
+            variant="h6"
+            component="p"
+            color="text.secondary"
+            sx={{ fontWeight: 400 }}
+            align={'center'}
+          >
+            Build a beautiful, modern website with flexible, fully customizable,
+            atomic MUI components.
+          </Typography>
+        </Box>
+        <Box
+          display="flex"
+          flexDirection={{ xs: 'column', sm: 'row' }}
+          alignItems={{ xs: 'stretched', sm: 'center' }}
+          justifyContent={'center'}
+          pt={2}
+        >
+          <Button
+            component={'a'}
+            variant="contained"
+            color="primary"
+            size="large"
+            fullWidth={isMd ? false : true}
+            href={'https://mui.com/store/items/the-front-landing-page/'}
+            target={'_blank'}
+          >
+            Purchase now
+          </Button>
+        </Box>
+      </Box>
+    </Container>
+  );
+};
+
+export default Hero;
