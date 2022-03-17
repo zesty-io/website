296a203d12e411aedbdd86b6a1db01cd87d95b59
Stefanie Serpa <smserpa@gmail.com>
2022-03-17 09:07:38 -0700

    renamed js file to match directory

--- src/views/zesty/Contact.js
+++ src/views/zesty/Contact.js
@@ -28,116 +28,34 @@ import React from 'react';
 import { useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 
-// component imports 
+// component imports
 import Container from 'components/Container';
 
 // blocks
 import { ContactFormWithSideBarMap } from 'blocks/formLayouts';
 import { ContactDetails } from 'blocks/contentBlocks';
 
-import Typography from '@mui/material/Typography';
-import ListItem from '@mui/material/ListItem';
-import ListItemAvatar from '@mui/material/ListItemAvatar';
-import ListItemText from '@mui/material/ListItemText';
-import Avatar from '@mui/material/Avatar';
-
 function Contact({ content }) {
   const theme = useTheme();
   return (
     <Box>
       <ContactFormWithSideBarMap />
-      <ContactDetails />
+      <Container>
+        <Box position={'relative'} bgcolor={'alternate.main'}>
+          <ContactDetails />
+        </Box>
+      </Container>
+      {/* Zesty.io Output Example and accessible JSON object for this component. Delete or comment out when needed.  */}
+{/* <div style={{background: '#eee', border: '1px #000 solid', margin: '10px', padding: '20px'}}>
+    <h2>Accessible Zesty.io JSON Object</h2>
+    <pre>{JSON.stringify(content, null, 2)}</pre>
+    </div>
+{/* End of Zesty.io output example */
+}
     </Box>
   );
 }
 
 
-
-
-      </Container>
-      <Box position={'relative'} bgcolor={'alternate.main'}>
-        <Container>
-          {/*Begin Contact Component*/}
-          <Box>
-            <Box marginBottom={2}>
-              <Typography
-                variant={'h4'}
-                sx={{ fontWeight: 700 }}
-                gutterBottom
-                align={'center'}
-              >
-                Contact details
-              </Typography>
-              <Typography color="text.secondary" align={'center'}>
-                Keep track of what's happening with your data, change
-                permissions, and run reports against your data anywhere in the
-                world. Keep track of what's happening with your data, change
-                permissions.
-              </Typography>
-            </Box>
-            <Box
-              display={'flex'}
-              flexDirection={{ xs: 'column', md: 'row' }}
-              justifyContent={'space-between'}
-              marginBottom={4}
-            >
-              {mock.map((item, i) => (
-                <Box
-                  key={i}
-                  component={ListItem}
-                  disableGutters
-                  width={'auto'}
-                  padding={0}
-                >
-                  <Box
-                    component={ListItemAvatar}
-                    minWidth={'auto !important'}
-                    marginRight={2}
-                  >
-                    <Box
-                      component={Avatar}
-                      bgcolor={theme.palette.secondary.main}
-                      width={40}
-                      height={40}
-                    >
-                      {item.icon}
-                    </Box>
-                  </Box>
-                  <ListItemText primary={item.label} secondary={item.value} />
-                </Box>
-              ))}
-            </Box>
-          </Box>
-          {/*End Contact Component*/}
-        </Container>
-        <Box
-          component={'svg'}
-          preserveAspectRatio="none"
-          xmlns="http://www.w3.org/2000/svg"
-          x="0px"
-          y="0px"
-          viewBox="0 0 1920 100.1"
-          sx={{
-            width: '100%',
-            marginBottom: theme.spacing(-1),
-          }}
-        >
-          <path
-            fill={theme.palette.background.paper}
-            d="M0,0c0,0,934.4,93.4,1920,0v100.1H0L0,0z"
-          ></path>
-        </Box>
-      </Box>
-
-      {/* Zesty.io Output Example and accessible JSON object for this component. Delete or comment out when needed.  */}
-      {/*
-             <div style={{background: '#eee', border: '1px #000 solid', margin: '10px', padding: '20px'}}>
-                 <h2>Accessible Zesty.io JSON Object</h2>
-                 <pre>{JSON.stringify(content, null, 2)}</pre>
-             </div>
-             {/* End of Zesty.io output example */}
-    </>
-  );
-}
 
 export default Contact;
