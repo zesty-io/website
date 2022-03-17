87635bfe27cffa903cc8fdf445f26e6998b696ea
Stefanie Serpa <smserpa@gmail.com>
2022-03-17 16:53:24 -0700

    pulling contact details content dynamically

--- src/blocks/contentBlocks/ContactDetails/ContactDetails.js
+++ src/blocks/contentBlocks/ContactDetails/ContactDetails.js
@@ -8,63 +8,8 @@ import ListItemAvatar from '@mui/material/ListItemAvatar';
 import ListItemText from '@mui/material/ListItemText';
 import Avatar from '@mui/material/Avatar';
 
-const ContactInformation = [
-  {
-    label: 'Phone',
-    value: "",
-    icon: (
-      <svg
-        width={20}
-        height={20}
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-        fill="currentColor"
-      >
-        <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
-      </svg>
-    ),
-  },
-  {
-    label: 'Email',
-    value: "",
-    icon: (
-      <svg
-        width={20}
-        height={20}
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-        fill="currentColor"
-      >
-        <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
-        <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
-      </svg>
-    ),
-  },
-  {
-    label: 'Address',
-    value: "",
-    icon: (
-      <svg
-        width={20}
-        height={20}
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-        fill="currentColor"
-      >
-        <path
-          fillRule="evenodd"
-          d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
-          clipRule="evenodd"
-        />
-      </svg>
-    ),
-  },
-]
-
-
-const ContactDetails = ({ title, subtitle, }) => {
+const ContactDetails = ({ title, subtitle, phone, email, address }) => {
   const theme = useTheme();
-  
 
   return (
     <Box>
@@ -75,10 +20,10 @@ const ContactDetails = ({ title, subtitle, }) => {
           gutterBottom
           align={'center'}
         >
-        {title}
+          {title}
         </Typography>
         <Typography color="text.secondary" align={'center'}>
-        {subtitle}
+          {subtitle}
         </Typography>
       </Box>
       <Box
@@ -86,32 +31,86 @@ const ContactDetails = ({ title, subtitle, }) => {
         flexDirection={{ xs: 'column', md: 'row' }}
         justifyContent={'space-between'}
         marginBottom={4}
-        
       >
-        {ContactInformation.map((item, i) => (
+        <Box component={ListItem} disableGutters width={'auto'} padding={0}>
           <Box
-            component={ListItem}
-            disableGutters
-            width={'auto'}
-            padding={0}
+            component={ListItemAvatar}
+            minWidth={'auto !important'}
+            marginRight={2}
           >
             <Box
-              component={ListItemAvatar}
-              minWidth={'auto !important'}
-              marginRight={2}
+              component={Avatar}
+              bgcolor={theme.palette.secondary.main}
+              width={40}
+              height={40}
             >
-              <Box
-                component={Avatar}
-                bgcolor={theme.palette.secondary.main}
-                width={40}
-                height={40}
+              <svg
+                width={20}
+                height={20}
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
               >
-                {item.icon}
-              </Box>
+                <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
+              </svg>
             </Box>
-            <ListItemText primary={item.label} secondary={item.value} />
           </Box>
-        ))}
+          <ListItemText primary="Phone" secondary={phone} />
+        </Box>
+        <Box component={ListItem} disableGutters width={'auto'} padding={0}>
+          <Box
+            component={ListItemAvatar}
+            minWidth={'auto !important'}
+            marginRight={2}
+          >
+            <Box
+              component={Avatar}
+              bgcolor={theme.palette.secondary.main}
+              width={40}
+              height={40}
+            >
+              <svg
+                width={20}
+                height={20}
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
+              </svg>
+            </Box>
+          </Box>
+          <ListItemText primary="Email" secondary={email} />
+        </Box>
+        <Box component={ListItem} disableGutters width={'auto'} padding={0}>
+          <Box
+            component={ListItemAvatar}
+            minWidth={'auto !important'}
+            marginRight={2}
+          >
+            <Box
+              component={Avatar}
+              bgcolor={theme.palette.secondary.main}
+              width={40}
+              height={40}
+            >
+              <svg
+                width={20}
+                height={20}
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </Box>
+          </Box>
+          <ListItemText primary="Address" secondary={address} />
+        </Box>
       </Box>
     </Box>
   );
