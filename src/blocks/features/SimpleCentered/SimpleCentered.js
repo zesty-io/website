92f2e4e7fc1e33045ce52ade568b9827e35eb7ea
David Naimi <d88naimi@gmail.com>
2022-03-18 13:55:43 -0700

    hacky way of inputing svgs

--- src/blocks/features/SimpleCentered/SimpleCentered.js
+++ src/blocks/features/SimpleCentered/SimpleCentered.js
@@ -9,7 +9,8 @@ import Grid from '@mui/material/Grid';
 import Container from 'components/Container';
 import FillerContent from 'components/FillerContent';
 
-
+import FastForwardIcon from '@mui/icons-material/FastForward';
+import NetworkCheckIcon from '@mui/icons-material/NetworkCheck';
 
 const mock = [
   {
@@ -80,8 +81,12 @@ const mock = [
   },
 ];
 
-const SimpleCentered = ({ header, description, cards=[] }) => {
-
+const IconComponent = ({ name, ...props }) => {
+  let Icon = iconTypes[name];
+  return <SvgIcon {...props} />;
+};
+const SimpleCentered = ({ header, description, cards = [] }) => {
+console.log("🚀 ~ file: SimpleCentered.js ~ line 89 ~ SimpleCentered ~ cards", cards)
   const theme = useTheme();
 
   return (
@@ -139,7 +144,32 @@ const SimpleCentered = ({ header, description, cards=[] }) => {
                     bgcolor={alpha(theme.palette.primary.main, 0.1)}
                     color={theme.palette.primary.main}
                   >
-                    {item.icon_name}
+                    {/* { ?  } */}
+
+                    <svg
+                      height={24}
+                      width={24}
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+
+                        d={
+                          item.icon_name === 'speed'
+                            ? 'M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122'
+                            : item.icon_name === 'attach_money'
+                            ? 'M7 21a4 4 0 01-4-4V5a2 2 0 012-2h4a2 2 0 012 2v12a4 4 0 01-4 4zm0 0h12a2 2 0 002-2v-4a2 2 0 00-2-2h-2.343M11 7.343l1.657-1.657a2 2 0 012.828 0l2.829 2.829a2 2 0 010 2.828l-8.486 8.485M7 17h.01'
+                            : item.icon_name === 'domain_verification'
+                            ? 'M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4'
+                            : 'M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4'
+                        }
+                      />
+                    </svg>
                   </Box>
                   <Typography
                     variant={'h6'}
