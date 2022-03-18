20bf8a27852b65f06de5c3ef11cc0e30087bbbac
David Naimi <d88naimi@gmail.com>
2022-03-18 15:20:53 -0700

    refactor icon loop

--- src/blocks/features/SimpleCentered/SimpleCentered.js
+++ src/blocks/features/SimpleCentered/SimpleCentered.js
@@ -5,86 +5,19 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Avatar from '@mui/material/Avatar';
 import Grid from '@mui/material/Grid';
+import { SvgIcon } from '@mui/material';
+
+import { speedIcon, moneyIcon, domainIcon } from './CustomIcons';
 
 import Container from 'components/Container';
 import FillerContent from 'components/FillerContent';
 
-import FastForwardIcon from '@mui/icons-material/FastForward';
-import NetworkCheckIcon from '@mui/icons-material/NetworkCheck';
-
-const mock = [
-  {
-    title: '99.99% uptime',
-    subtitle:
-      'Zesty.io builds, renders, and delivers web pages at 99.99% uptime.',
-    icon: (
-      <svg
-        height={24}
-        width={24}
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth={2}
-          d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122"
-        />
-      </svg>
-    ),
-  },
-  {
-    title: 'Minimize IT costs',
-    subtitle:
-      'Save up to 70% and reallocate your resources toward initiatives that drive your business forward.',
-    icon: (
-      <svg
-        height={24}
-        width={24}
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth={2}
-          d="M7 21a4 4 0 01-4-4V5a2 2 0 012-2h4a2 2 0 012 2v12a4 4 0 01-4 4zm0 0h12a2 2 0 002-2v-4a2 2 0 00-2-2h-2.343M11 7.343l1.657-1.657a2 2 0 012.828 0l2.829 2.829a2 2 0 010 2.828l-8.486 8.485M7 17h.01"
-        />
-      </svg>
-    ),
-  },
-  {
-    title: 'Sub second page load times',
-    subtitle:
-      'Zesty.io sites are automatically cached and served globally, so your pages will load faster without any additional optimization on your end.',
-    icon: (
-      <svg
-        height={24}
-        width={24}
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth={2}
-          d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4"
-        />
-      </svg>
-    ),
-  },
-];
-
-const IconComponent = ({ name, ...props }) => {
-  let Icon = iconTypes[name];
-  return <SvgIcon {...props} />;
+const iconMap = {
+  speed: speedIcon,
+  attach_money: moneyIcon,
+  domain_verification: domainIcon,
 };
+
 const SimpleCentered = ({ header, description, cards = [] }) => {
   const theme = useTheme();
 
@@ -100,30 +33,6 @@ const SimpleCentered = ({ header, description, cards = [] }) => {
                 }}
               ></Box>
             )}
-            {/* <Typography
-              variant="h4"
-              color="text.primary"
-              align={'center'}
-              gutterBottom
-              sx={{
-                fontWeight: 700,
-              }}
-            >
-              {header || 'Build accessible React apps with speed'}
-            </Typography> */}
-            {/* <Typography
-              variant="h6"
-              component="p"
-              color="text.secondary"
-              sx={{ fontWeight: 400 }}
-              align={'center'}
-            >
-              {description ||
-                `
-              Build a beautiful, modern website with flexible, fully
-              customizable, atomic MUI components.
-              `}
-            </Typography> */}
           </Box>
         </Box>
         <Grid container spacing={2}>
@@ -143,32 +52,10 @@ const SimpleCentered = ({ header, description, cards = [] }) => {
                     bgcolor={alpha(theme.palette.primary.main, 0.1)}
                     color={theme.palette.primary.main}
                   >
-                    {/* { ?  } */}
-
-                    <svg
-                      height={24}
-                      width={24}
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-
-                        d={
-                          item.icon_name === 'speed'
-                            ? 'M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122'
-                            : item.icon_name === 'attach_money'
-                            ? 'M7 21a4 4 0 01-4-4V5a2 2 0 012-2h4a2 2 0 012 2v12a4 4 0 01-4 4zm0 0h12a2 2 0 002-2v-4a2 2 0 00-2-2h-2.343M11 7.343l1.657-1.657a2 2 0 012.828 0l2.829 2.829a2 2 0 010 2.828l-8.486 8.485M7 17h.01'
-                            : item.icon_name === 'domain_verification'
-                            ? 'M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4'
-                            : 'M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4'
-                        }
-                      />
-                    </svg>
+                    {/* Icon loop */}
+                    {iconMap[item.icon_name]
+                      ? iconMap[item.icon_name]
+                      : domainIcon}
                   </Box>
                   <Typography
                     variant={'h6'}
