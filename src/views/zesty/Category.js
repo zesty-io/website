6046177cc8ec333f3a2c8776deebc5c036e1bc92
Bobbi Tarkany <bl.tarkany@gmail.com>
2022-03-18 12:06:43 -0700

    updated category page with data error handling, removed unused props, added correct newsletter cta

--- src/views/zesty/Category.js
+++ src/views/zesty/Category.js
@@ -30,7 +30,7 @@ import React, { useEffect, useState } from 'react';
 import { FullScreenHeroWithImageSlider, SlashImageHero } from 'blocks/heroes';
 import { Breadcrumb } from 'blocks/progressSteps';
 import { Result } from 'blocks/formLayouts';
-import { Newsletter } from 'blocks/newsletters';
+import { CtaWithInputField } from 'blocks/cta';
 import Box from '@mui/material/Box';
 import { useTheme, alpha } from '@mui/material/styles';
 // filler content
@@ -180,9 +180,7 @@ function Category({ content }) {
         <SlashImageHero
           title={content.category || FillerContent.header}
           description={content.description || FillerContent.description}
-          cta={content.cta_button || FillerContent.cta}
-          ctaHref={content.cta_href.data[0]?.meta.web.uri || FillerContent.href}
-          image={content.header_image.data[0]?.url || FillerContent.dashboard_image} />
+          image={content.header_image?.data[0]?.url || FillerContent.category_fallback_image} />
         </Box>
       </Box>
       {/* can be swapped */}
@@ -232,7 +230,10 @@ function Category({ content }) {
           ></path>
         </Box>
         <Container>
-          <Newsletter />
+          <CtaWithInputField
+          title={'Subscribe to the zestiest newsletter in the industry'}
+          description={'Get the latest from the Zesty team, from whitepapers to product updates.'}
+          cta={'Subscribe'} />
         </Container>
       </Box>
 
