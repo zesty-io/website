8d25501d722f3329b7a758ef3c0dfedf84af37d1
David Naimi <d88naimi@gmail.com>
2022-03-24 13:47:01 -0700

    moving contact us button to bottom of card for mobile view

--- src/views/zesty/Pricing.js
+++ src/views/zesty/Pricing.js
@@ -1,14 +1,14 @@
 /**
  * Zesty.io Content Model Component
  * When the ZestyLoader [..slug].js file is used, this component will autoload if it associated with the URL
- * 
- * Label: Pricing 
- * Name: pricing 
+ *
+ * Label: Pricing
+ * Name: pricing
  * Model ZUID: 6-ef1038-d9tb16
  * File Created On: Fri Mar 04 2022 12:16:05 GMT-0800 (Pacific Standard Time)
- * 
+ *
  * Model Fields:
- * 
+ *
   * title (text)
  * content (wysiwyg_advanced)
  * fine_print (wysiwyg_basic)
@@ -18,15 +18,15 @@
  * volume_discount_description (wysiwyg_advanced)
  * instance_definition (textarea)
 
- * 
+ *
  * In the render function, text fields can be accessed like {content.field_name}, relationships are arrays,
  * images are objects {content.image_name.data[0].url}
- * 
+ *
  * This file is expected to be customized; because of that, it is not overwritten by the integration script.
  * Model and field changes in Zesty.io will not be reflected in this comment.
- * 
+ *
  * View and Edit this model's current schema on Zesty.io at https://8-aaeffee09b-7w6v22.manager.zesty.io/schema/6-ef1038-d9tb16
- * 
+ *
  * Data Output Example: https://zesty.org/services/web-engine/introduction-to-parsley/parsley-index#tojson
  * Images API: https://zesty.org/services/media-storage-micro-dam/on-the-fly-media-optimization-and-dynamic-image-manipulation
  */
