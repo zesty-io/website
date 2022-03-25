8190ffa532fad9c88823a55b99d6440765186214
ardeay <apuzzo@gmail.com>
2022-03-25 14:24:57 -0700

    condition for GTM code

--- src/components/ZestyHead.js
+++ src/components/ZestyHead.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
+import { EventNote } from '@mui/icons-material';
 
 export default function ZestyHead(props) {
   return (
@@ -22,11 +23,12 @@ export default function ZestyHead(props) {
       />
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
       rel="stylesheet"></link>
+      {props.content.zestyProduction &&
       <script dangerouslySetInnerHTML={{__html:`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});
       var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-MSPH3C8');`}}/>
+})(window,document,'script','dataLayer','GTM-MSPH3C8');`}}/>}
     </Head>
   );
 }
