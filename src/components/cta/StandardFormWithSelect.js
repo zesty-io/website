9ac8c3463418483a56c75767f23816811fb44b9b
Bobbi Tarkany <bl.tarkany@gmail.com>
2022-03-25 17:13:01 -0700

    updated object commented out keys that create confliects, formatted modal, placecd post function

--- src/components/cta/StandardFormWithSelect.js
+++ src/components/cta/StandardFormWithSelect.js
@@ -1,5 +1,5 @@
 /* eslint-disable react/no-unescaped-entities */
-import React from 'react';
+import React, {useState} from 'react';
 import Box from '@mui/material/Box';
 
 import { useFormik } from 'formik';
@@ -19,26 +19,26 @@ import Backdrop from '@mui/material/Backdrop';
 import Modal from '@mui/material/Modal';
 import Fade from '@mui/material/Fade';
 
-const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
-};
 
-function TransitionsModal() {
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+
+function TransitionsModal({open, handleOpen, handleClose}) {
+
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: '#fff',
+    border: '2px solid #FF5D0A',
+    borderRadius: 3,
+    boxShadow: 24,
+    p: 4,
+  };
 
   return (
     <div>
-      <Button onClick={handleOpen}>Open modal</Button>
+      {/* <Button onClick={handleOpen}>Open modal</Button> */}
       <Modal
         aria-labelledby="transition-modal-title"
         aria-describedby="transition-modal-description"
@@ -53,10 +53,10 @@ function TransitionsModal() {
         <Fade in={open}>
           <Box sx={style}>
             <Typography id="transition-modal-title" variant="h6" component="h2">
-              Text in a modal
+              Thank You for Your Inquiry
             </Typography>
             <Typography id="transition-modal-description" sx={{ mt: 2 }}>
-              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+              We look forward to assisting you.
             </Typography>
           </Box>
         </Fade>
@@ -67,28 +67,28 @@ function TransitionsModal() {
 
 /* validation for form component */
 
-const getLeadObjectZOHO = () => {
+const getLeadObjectZOHO = (obj, select) => {
 
-    let country = document.querySelector('#ac-country');
+  let country = document.querySelector('#ac-country');
 
-    return {
-        
-        "First_Name": document.querySelector('#ac-firstname input').value,
-        "Last_Name": document.querySelector('#ac-lastname input').value,
-        "Email": document.querySelector('#ac-email input').value,
-        "Country": country.options[country.selectedIndex].getAttribute('data-countryCode'),
-        "Phone": '+'+country.value + ' ' + document.querySelector('#ac-phone input').value,
-        "Current_CMS": acCMS,
-        "How_Using_Zesty_io": acHow,
-        "Website": document.querySelector('#ac-url').value,
-        "Lead_Source": "Website",
-        "Description": document.querySelector('#ac-description').value,
-        "Role": acRole,
-        'Project_Timeline' : document.querySelector('#ac-timeline').value,
-        "Lead_Source_Detail": acLeadtype,
-        "Business_Type": "Direct",
-        "Lead_Status": "Not Contacted"
-    }
+  return {
+    
+    "First_Name": obj.firstName,
+    "Last_Name": obj.lastName,
+    "Email": obj.email,
+    // "Country": country.options[country.selectedIndex].getAttribute('data-countryCode'),
+    // "Phone": '+'+country.value + ' ' + document.querySelector('#ac-phone input').value,
+    // "Current_CMS": acCMS,
+    // "How_Using_Zesty_io": acHow,
+    // "Website": document.querySelector('#ac-url').value,
+    "Lead_Source": "Website",
+    // "Description": document.querySelector('#ac-description').value,
+    // "Role": acRole,
+    // 'Project_Timeline' : document.querySelector('#ac-timeline').value,
+    // "Lead_Source_Detail": acLeadtype,
+    "Business_Type": "Direct",
+    "Lead_Status": "Not Contacted"
+  }
 
 }
 
@@ -96,7 +96,7 @@ const postToZOHO = (payloadJSON) => {
  
     fetch('https://us-central1-zesty-prod.cloudfunctions.net/zoho',{
         method: 'POST',
-        body: JSON.stringify(payload),
+        body: JSON.stringify(payloadJSON),
         headers: {
             "Content-Type": "application/json"
         }
@@ -131,187 +131,197 @@ const validationSchema = yup.object({
 });
 
 function StandardFormWithSelect({selectedValue=0, hideSelect=false, hideMessage=true, defaultMessage=''}) {
- 
-    let inquiryReasons = [
-        'General',
-        'Agency Signup',
-        'Request a Demo',
-        'Support',
-        'Billing',
-        'Press Relations',
-        
-    ]
-    const [selectValue, setSelectValue] = React.useState(inquiryReasons[selectedValue]);
+  
+  const theme = useTheme();
 
-    const handleChange = (event) => {
-        setSelectValue(event.target.value);
-    };
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  
+  let inquiryReasons = [
+    'General',
+    'Agency Signup',
+    'Request a Demo',
+    'Support',
+    'Billing',
+    'Press Relations',
+  ]
+  const [selectValue, setSelectValue] = useState(inquiryReasons[selectedValue]);
+  
+  const handleChange = (event) => {
+    setSelectValue(event.target.value);
+  };
 
-    const theme = useTheme();
 
-    const initialValues = {
-    firstName: '',
-    lastName: '',
-    email: '',
-    message: '',
-    };
+  const initialValues = {
+  firstName: '',
+  lastName: '',
+  email: '',
+  message: '',
+  };
 
-    const onSubmit = (values) => {
-      console.log(values, selectValue)
-        alert(JSON.stringify(values))
-        //  1. validate value
-            // return errors if 
-        // 2. shape data to match zoho object
-        // 3. post to zoho
-        // 4. POP modal thanking them
-        TransitionsModal()
-        return values;
-    };
+  const onSubmit = (values) => {
+    console.log(values, selectValue);
+    //  1. validate value XX
+        // return errors if XX
+    // 2. shape data to match zoho object
+    console.log(getLeadObjectZOHO(values, selectValue));
+    // 3. post to zoho
+    // not sure all of what inof needs to go into object - test before running
+    postToZOHO(getLeadObjectZOHO(values, selectValue));
+    // 4. POP modal thanking them
+    handleOpen();
+    return values;
+  };
 
-    const formik = useFormik({
-      initialValues,
-      validationSchema: validationSchema,
-      onSubmit,
-    });
+  const formik = useFormik({
+    initialValues,
+    validationSchema: validationSchema,
+    onSubmit,
+  });
 
-    return (
-    
-    <Box>
-        <form noValidate onSubmit={formik.handleSubmit}>
-            <Grid container spacing={4}>
-            <Grid item xs={12} sm={6}>
-                <TextField
-                sx={{ height: 54 }}
-                label="First name"
-                variant="outlined"
-                color="primary"
-                size="medium"
-                name="firstName"
-                fullWidth
-                value={formik.values.firstName}
-                onChange={formik.handleChange}
-                error={
-                    formik.touched.firstName && Boolean(formik.errors.firstName)
-                }
-                helperText={
-                    formik.touched.firstName && formik.errors.firstName
-                }
-                />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-                <TextField
-                sx={{ height: 54 }}
-                label="Last name"
-                variant="outlined"
-                color="primary"
-                size="medium"
-                name="lastName"
-                fullWidth
-                value={formik.values.lastName}
-                onChange={formik.handleChange}
-                error={
-                    formik.touched.lastName && Boolean(formik.errors.lastName)
-                }
-                helperText={formik.touched.lastName && formik.errors.lastName}
-                />
-            </Grid>
-            <Grid item xs={12}>
-                <TextField
-                sx={{ height: 54 }}
-                label="Email"
-                type="email"
-                variant="outlined"
-                color="primary"
-                size="medium"
-                name="email"
-                fullWidth
-                value={formik.values.email}
-                onChange={formik.handleChange}
-                error={formik.touched.email && Boolean(formik.errors.email)}
-                helperText={formik.touched.email && formik.errors.email}
-                />
-            </Grid>
-            {/* logic to hide the select */}
-           {hideSelect && <Grid item xs={12}><input value={selectValue}  name="inquiryReason" type="hidden" /></Grid>}
-           {/* logic to hide the select */}
-           {!hideSelect && 
-            <Grid item xs={12}>
-              <Box sx={{ minWidth: 120 }}>
-                <FormControl fullWidth>
-                  <InputLabel id="demo-simple-select-label">Inquiry Reason</InputLabel>
-                  <Select
-                  labelId="demo-simple-select-label"
-                  id="demo-simple-select"
-                  value={selectValue}
-                  label="Inquiry Reason"
-                  onChange={handleChange}
-                  name="inquiryReason"
-                  >
-                    <MenuItem value=''><em>None</em></MenuItem>
-                    {inquiryReasons.map(value => <MenuItem value={value}>{value}</MenuItem>)}
-                  </Select>
-                </FormControl>
-              </Box>
-            </Grid>
-           }
-            <Grid item xs={12}>
-                <TextField
-                label="Message"
-                multiline
-                rows={6}
-                variant="outlined"
-                color="primary"
-                size="medium"
-                name="message"
-                fullWidth
-                value={formik.values.message}
-                onChange={formik.handleChange}
-                error={
-                    formik.touched.message && Boolean(formik.errors.message)
-                }
-                helperText={formik.touched.message && formik.errors.message}
-                />
-            </Grid>
-            <Grid item xs={12}>
-                <Button
-                sx={{ height: 54, minWidth: 150 }}
-                variant="contained"
-                color="secondary"
-                size="medium"
-                type="submit"
+  return (
+  
+  <Box>
+      <form noValidate onSubmit={formik.handleSubmit}>
+          <Grid container spacing={4}>
+          <Grid item xs={12} sm={6}>
+              <TextField
+              sx={{ height: 54 }}
+              label="First name"
+              variant="outlined"
+              color="primary"
+              size="medium"
+              name="firstName"
+              fullWidth
+              value={formik.values.firstName}
+              onChange={formik.handleChange}
+              error={
+                  formik.touched.firstName && Boolean(formik.errors.firstName)
+              }
+              helperText={
+                  formik.touched.firstName && formik.errors.firstName
+              }
+              />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+              <TextField
+              sx={{ height: 54 }}
+              label="Last name"
+              variant="outlined"
+              color="primary"
+              size="medium"
+              name="lastName"
+              fullWidth
+              value={formik.values.lastName}
+              onChange={formik.handleChange}
+              error={
+                  formik.touched.lastName && Boolean(formik.errors.lastName)
+              }
+              helperText={formik.touched.lastName && formik.errors.lastName}
+              />
+          </Grid>
+          <Grid item xs={12}>
+              <TextField
+              sx={{ height: 54 }}
+              label="Email"
+              type="email"
+              variant="outlined"
+              color="primary"
+              size="medium"
+              name="email"
+              fullWidth
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              error={formik.touched.email && Boolean(formik.errors.email)}
+              helperText={formik.touched.email && formik.errors.email}
+              />
+          </Grid>
+          {/* logic to hide the select */}
+          {hideSelect && <Grid item xs={12}><input value={selectValue}  name="inquiryReason" type="hidden" /></Grid>}
+          {/* logic to hide the select */}
+          {!hideSelect && 
+          <Grid item xs={12}>
+            <Box sx={{ minWidth: 120 }}>
+              <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-label">Inquiry Reason</InputLabel>
+                <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={selectValue}
+                label="Inquiry Reason"
+                onChange={handleChange}
+                name="inquiryReason"
                 >
-                Submit
-                </Button>
-            </Grid>
-            <Grid item xs={12}>
-                <Typography color="text.secondary">
-                We'll get back to you in 1-2 business days.
-                </Typography>
-            </Grid>
-            <Grid item xs={12}>
-                <Divider />
-            </Grid>
-            <Grid item xs={12}>
-                <Box>
-                <Typography component="p" variant="body2" align="left">
-                    By clicking on "submit" you agree to our{' '}
-                    <Box
-                    component="a"
-                    href="/legal/privacy-policy/"
-                    color={theme.palette.text.primary}
-                    fontWeight={'700'}
-                    >
-                    Privacy Policy
-                    </Box>
-                    .
-                </Typography>
-                </Box>
-            </Grid>
-            </Grid>
-        </form>
-    </Box>
+                  {/* <MenuItem value=''><em>None</em></MenuItem> */}
+                  {inquiryReasons.map(value => <MenuItem value={value}>{value}</MenuItem>)}
+                </Select>
+              </FormControl>
+            </Box>
+          </Grid>
+          }
+          <Grid item xs={12}>
+              <TextField
+              label="Message"
+              multiline
+              rows={6}
+              variant="outlined"
+              color="primary"
+              size="medium"
+              name="message"
+              fullWidth
+              value={formik.values.message}
+              onChange={formik.handleChange}
+              error={
+                  formik.touched.message && Boolean(formik.errors.message)
+              }
+              helperText={formik.touched.message && formik.errors.message}
+              />
+          </Grid>
+          <Grid item xs={12}>
+              <Button
+              sx={{ height: 54, minWidth: 150 }}
+              variant="contained"
+              color="secondary"
+              size="medium"
+              type="submit"
+              >
+              Submit
+              </Button>
+          </Grid>
+          <Grid item xs={12}>
+              <Typography color="text.secondary">
+              We'll get back to you in 1-2 business days.
+              </Typography>
+          </Grid>
+          <Grid item xs={12}>
+              <Divider />
+          </Grid>
+          <Grid item xs={12}>
+              <Box>
+              <Typography component="p" variant="body2" align="left">
+                  By clicking on "submit" you agree to our{' '}
+                  <Box
+                  component="a"
+                  href="/legal/privacy-policy/"
+                  color={theme.palette.text.primary}
+                  fontWeight={'700'}
+                  >
+                  Privacy Policy
+                  </Box>
+                  .
+              </Typography>
+              </Box>
+          </Grid>
+          </Grid>
+      </form>
+      <TransitionsModal
+      open={open}
+      handleClose={handleClose}
+      handleOpen={handleOpen} />
+  </Box>
 
-    );
+  );
 
 }
 
