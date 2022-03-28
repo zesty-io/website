9849e9f4dee2ad5b11efa326dd1d35feaef1695e
ardeay <apuzzo@gmail.com>
2022-03-28 10:20:02 -0700

    og image

--- src/components/ZestyHead.js
+++ src/components/ZestyHead.js
@@ -3,6 +3,17 @@ import Head from 'next/head';
 import { EventNote } from '@mui/icons-material';
 
 export default function ZestyHead(props) {
+
+  // default OG image
+  let ogimage = 'https://kfg6bckb.media.zestyio.com/zesty-share-image-generic.png?width=1200'
+  // determine if there is a custom og image
+  if(props.content?.og_image){
+    ogimage = props.content.og_image.data[0].url + '?width=1200'
+  // if custom og not set, find the first fiedl with image in the name and set that
+  } else if (Object.keys(props.content).find(name => name.contains('image'))){
+    let imageKey = Object.keys(props.content).find(name => name.contains('image'))
+    ogimage = props.content[imageKey].data[0].url + '?width=1200'
+  }
   return (
     <Head>
       
@@ -21,6 +32,12 @@ export default function ZestyHead(props) {
         property="og:description"
         content={props.content.meta.web.seo_meta_description}
       />
+      {}
+      <meta 
+        property="og:image"
+        content={ogimage}
+        />
+
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
       rel="stylesheet"></link>
       {props.content.zestyProduction &&
