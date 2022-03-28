a5dc30041ebdecc11963e8ca8a556d4c96e17216
David Naimi <d88naimi@gmail.com>
2022-03-28 15:03:22 -0700

    bring back cta

--- src/blocks/sidebars/SidebarArticles/SidebarArticles.js
+++ src/blocks/sidebars/SidebarArticles/SidebarArticles.js
@@ -14,6 +14,7 @@ import FillerContent from 'components/FillerContent';
 const SidebarArticles = ({latestArticles}) => {
 
 
+
   const theme = useTheme();
   return (
     <Box component={Card} variant={'outlined'} padding={2}>
