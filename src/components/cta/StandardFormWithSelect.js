03dc52d6cb797858034e4c7e120a8a07f2611d59
Bobbi Tarkany <bl.tarkany@gmail.com>
2022-03-28 11:51:11 -0700

    removed comment block

--- src/components/cta/StandardFormWithSelect.js
+++ src/components/cta/StandardFormWithSelect.js
@@ -21,50 +21,6 @@ import Fade from '@mui/material/Fade';
 import TransitionsModal from './TransitionModal';
 
 
-
-// function TransitionsModal({open, handleOpen, handleClose}) {
-
-//   const style = {
-//     position: 'absolute',
-//     top: '50%',
-//     left: '50%',
-//     transform: 'translate(-50%, -50%)',
-//     width: 400,
-//     bgcolor: '#fff',
-//     border: '2px solid #FF5D0A',
-//     borderRadius: 3,
-//     boxShadow: 24,
-//     p: 4,
-//   };
-
-//   return (
-//     <div>
-//       <Modal
-//         aria-labelledby="transition-modal-title"
-//         aria-describedby="transition-modal-description"
-//         open={open}
-//         onClose={handleClose}
-//         closeAfterTransition
-//         BackdropComponent={Backdrop}
-//         BackdropProps={{
-//           timeout: 500,
-//         }}
-//       >
-//         <Fade in={open}>
-//           <Box sx={style}>
-//             <Typography id="transition-modal-title" variant="h6" component="h2">
-//             Thank you for contacting Zesty.io
-//             </Typography>
-//             <Typography id="transition-modal-description" sx={{ mt: 2 }}>
-//             Our team will be in touch soon regarding your request.
-//             </Typography>
-//           </Box>
-//         </Fade>
-//       </Modal>
-//     </div>
-//   );
-// }
-
 /* validation for form component */
 
 const getLeadObjectZOHO = (obj, select, leadDetail, businessType) => {
