0b673c07e1f55cefd838494b1ece24d684036143
darwin1 <darwin.apolinario12@gmail.com>
2022-03-31 03:31:54 +0800

    remove topnav if ppc page

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -3,12 +3,21 @@ import PropTypes from 'prop-types';
 import Box from '@mui/material/Box';
 import Link from '@mui/material/Link';
 import { zestyLink } from 'lib/zestyLink';
+import { useRouter } from 'next/router';
 
 import ThemeModeToggler from 'components/ThemeModeToggler';
 
 const TopNav = ({ nav, colorInvert = false }) => {
+  const router = useRouter();
+  //check if page is from ppc
+  const isPpcPage = router.asPath === '/ppc/digital-experience-platform/';
+
   return (
-    <Box display={'flex'} justifyContent={'flex-end'} alignItems={'center'}>
+    <Box
+      display={isPpcPage ? 'none' : 'flex'}
+      justifyContent={'flex-end'}
+      alignItems={'center'}
+    >
       <Box marginRight={{ xs: 1, sm: 2 }}>
         <Link
           underline="none"
@@ -16,7 +25,7 @@ const TopNav = ({ nav, colorInvert = false }) => {
           href="https://www.zesty.org"
           color={colorInvert ? 'common.white' : 'text.primary'}
         >
-         Docs
+          Docs
         </Link>
       </Box>
       <Box marginRight={{ xs: 1, sm: 2 }}>
@@ -30,18 +39,16 @@ const TopNav = ({ nav, colorInvert = false }) => {
           Github
         </Link>
       </Box>
-      
+
       <Box marginRight={{ xs: 1, sm: 2 }}>
         <Link
           underline="none"
           component="a"
-          href={zestyLink(nav,'7-cec987fcf5-9bht2z')}
+          href={zestyLink(nav, '7-cec987fcf5-9bht2z')}
           color={colorInvert ? 'common.white' : 'text.primary'}
           sx={{ display: 'flex', alignItems: 'center' }}
         >
           Contact Us
-          
-            
         </Link>
       </Box>
       <Box>
