70c1d66b6f8057a3d9f91d39eb4e4bf310d42968
David Naimi <d88naimi@gmail.com>
2022-03-30 08:24:23 -0700

    updating paddingY to mui syntax

--- src/blocks/features/FeaturesWithMobileScreenshot/FeaturesWithMobileScreenshot.js
+++ src/blocks/features/FeaturesWithMobileScreenshot/FeaturesWithMobileScreenshot.js
@@ -20,7 +20,7 @@ const FeaturesWithMobileScreenshot = ({
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
   return (
-    <Container paddingTop={'0 !important'}>
+    <Container paddingY={0}>
       {feature_list_h1 && (
         <Typography
           variant="p"
