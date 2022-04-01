f5aa7928fd851dd92de291b5389668f5413081ba
darwin1 <darwin.apolinario12@gmail.com>
2022-04-01 16:27:25 +0800

    remove haburger menu is /ppc page

--- src/layouts/Main/components/Topbar/Topbar.js
+++ src/layouts/Main/components/Topbar/Topbar.js
@@ -36,8 +36,8 @@ const Topbar = ({ onSidebarOpen, customRouting, colorInvert = false }) => {
           component={'img'}
           src={
             mode === 'light' && !colorInvert
-            ? 'https://brand.zesty.io/zesty-io-logo-horizontal.svg'
-            : 'https://brand.zesty.io/zesty-io-logo-horizontal-light-color.svg'
+              ? 'https://brand.zesty.io/zesty-io-logo-horizontal.svg'
+              : 'https://brand.zesty.io/zesty-io-logo-horizontal-light-color.svg'
           }
           height={1}
           width={1}
@@ -90,21 +90,26 @@ const Topbar = ({ onSidebarOpen, customRouting, colorInvert = false }) => {
           </Button>
         </Box>
       </Box>
-      <Box sx={{ display: { xs: 'block', md: 'none' } }} alignItems={'center'}>
-        <Button
-          onClick={() => onSidebarOpen()}
-          aria-label="Menu"
-          variant={'outlined'}
-          sx={{
-            borderRadius: 2,
-            minWidth: 'auto',
-            padding: 1,
-            borderColor: alpha(theme.palette.divider, 0.2),
-          }}
+      {!isPpcPage && (
+        <Box
+          sx={{ display: { xs: 'block', md: 'none' } }}
+          alignItems={'center'}
         >
-          <MenuIcon />
-        </Button>
-      </Box>
+          <Button
+            onClick={() => onSidebarOpen()}
+            aria-label="Menu"
+            variant={'outlined'}
+            sx={{
+              borderRadius: 2,
+              minWidth: 'auto',
+              padding: 1,
+              borderColor: alpha(theme.palette.divider, 0.2),
+            }}
+          >
+            <MenuIcon />
+          </Button>
+        </Box>
+      )}
     </Box>
   );
 };
