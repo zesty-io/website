c3ae92eca7e7ee766664f166f9829739e904c6cf
darwin808 <darwin.apolinario12@gmail.com>
2022-04-19 13:31:01 +0800

    remove clg in integration pages

--- src/views/zesty/CapterraLandingPage.js
+++ src/views/zesty/CapterraLandingPage.js
@@ -38,21 +38,36 @@
  * Images API: https://zesty.org/services/media-storage-micro-dam/on-the-fly-media-optimization-and-dynamic-image-manipulation
  */
 
-import React  from 'react';
+import React from 'react';
+import HeroWithFormAndBackgroundGradient from 'blocks/heroes/HeroWithFormAndBackgroundGradient';
+import FillerContent from 'components/FillerContent';
 
-function CapterraLandingPage({content}) {
-    return (
-        <>
-            {/* Zesty.io Output Example and accessible JSON object for this component. Delete or comment out when needed.  */}
-            <h1 dangerouslySetInnerHTML={{__html:content.meta.web.seo_meta_title}}></h1>
+function CapterraLandingPage({ content }) {
+  console.log(content, 1111111111111111111111111111);
+  return (
+    <>
+      <HeroWithFormAndBackgroundGradient
+        headelineTitle={content.hero_h1 || FillerContent.header}
+        description={content.zesty_overview || FillerContent.description}
+        imageCollection={
+          content.client_logos?.data?.slice(0, 3) || [FillerContent.image]
+        }
+        backgroundImage={
+          content?.background_image?.data &&
+          content?.background_image?.data[0]?.url
+        }
+        form_title={content.form_title || FillerContent.header}
+      />
+      {/* Zesty.io Output Example and accessible JSON object for this component. Delete or comment out when needed.  */}
+      {/* <h1 dangerouslySetInnerHTML={{__html:content.meta.web.seo_meta_title}}></h1>
             <div>{content.meta.web.seo_meta_description}</div>
             <div style={{background: '#eee', border: '1px #000 solid', margin: '10px', padding: '20px'}}>
                 <h2>Accessible Zesty.io JSON Object</h2>
                 <pre>{JSON.stringify(content, null, 2)}</pre>
-            </div>
-            {/* End of Zesty.io output example */}
-        </>
-    );
+            </div> */}
+      {/* End of Zesty.io output example */}
+    </>
+  );
 }
-  
+
 export default CapterraLandingPage;
