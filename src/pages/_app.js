ab6c14bb1703286911f8cf91804819dcc2c48639
darwin808 <darwin.apolinario12@gmail.com>
2022-05-11 11:54:45 +0800

    testing the new explorer

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,19 +2,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Head from 'next/head';
 import Page from '../components/Page';
+import Script from 'next/script';
 
 import 'react-lazy-load-image-component/src/effects/blur.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import 'react-image-lightbox/style.css';
 import 'aos/dist/aos.css';
-import '../../public/styles/custom.css'
-
-
+import '../../public/styles/custom.css';
 
 export default function App({ Component, pageProps }) {
   return (
     <React.Fragment>
+      <Script
+        type="text/javascript"
+        strategy="beforeInteractive"
+        src="https://cdn.jsdelivr.net/gh/darwin808/comp-library-ts-react-webpack@0.0.3/build/bundle.js"
+      />
       <Head>
         <meta
           name="viewport"
