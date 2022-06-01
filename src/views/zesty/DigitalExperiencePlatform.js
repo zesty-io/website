c4483f0786e9675859ea37d102679e837364a564
darwin808 <darwin.apolinario12@gmail.com>
2022-06-01 14:07:41 +0800

    solutions>dxp pages update

--- src/views/zesty/DigitalExperiencePlatform.js
+++ src/views/zesty/DigitalExperiencePlatform.js
@@ -72,75 +72,206 @@
  * Images API: https://zesty.org/services/media-storage-micro-dam/on-the-fly-media-optimization-and-dynamic-image-manipulation
  */
 
-import { Box, Button, Container, Typography, useTheme } from '@mui/material';
+import {
+  Box,
+  Button,
+  Container,
+  Grid,
+  Typography,
+  useTheme,
+} from '@mui/material';
 import React from 'react';
 import SimpleHeroWithImageAndCtaButtonsPage from 'blocks/heroes/SimpleHeroWithImageAndCtaButtons/SimpleHeroWithImageAndCtaButtons.js';
 import FillerContent from 'components/FillerContent';
+import TryFreeButton from 'components/cta/TryFreeButton';
+import * as helper from 'utils';
 
+const HeroSection = ({ content, theme }) => {
+  return (
+    <Box
+      paddingTop={15}
+      paddingBottom={25}
+      sx={{
+        background:
+          'linear-gradient(180deg, rgba(31,93,207,1) 45%, rgba(112,152,224,1) 70%, rgba(255,255,255,1) 100%);',
+      }}
+    >
+      <Container sx={{ display: 'grid', gridTemplateColumns: '1fr 1fr' }}>
+        <Box
+          sx={{
+            background: '',
+            display: 'flex',
+            justifyContent: 'center',
+            flexDirection: 'column',
+          }}
+        >
+          <Typography
+            component={'h2'}
+            variant={'p'}
+            fontWeight={600}
+            sx={{
+              color: theme.palette.common.white,
+              fontSize: '32px',
+              opacity: '0.8',
+            }}
+          >
+            {'DXP'}
+          </Typography>
+          <Typography
+            component={'h1'}
+            variant={'p'}
+            sx={{
+              color: theme.palette.common.white,
+              fontWeight: 700,
+              fontSize: '48px',
+            }}
+          >
+            {content.header_eyebrow}
+          </Typography>
+          <Typography
+            paddingY={2}
+            component={'h3'}
+            variant={'p'}
+            sx={{
+              color: theme.palette.common.white,
+              fontWeight: 500,
+              fontSize: '20px',
+            }}
+          >
+            {content.header_h1}
+          </Typography>
+          <Box>
+            {/* <TryFreeButton sx={{
+                  backgroundColor: theme.palette.common.white,
+                  color: theme.palette.zesty.zestyOrange,
+                  padding: '.6rem 4rem',
+                  fontSize: '16px',
+                }}
+                variant="contained"
+                component="a"
+              /> */}
+            <Button
+              href={''}
+              component={Button}
+              sx={{
+                backgroundColor: theme.palette.common.white,
+                color: theme.palette.zesty.zestyOrange,
+                padding: '.6rem 4rem',
+                fontSize: '16px',
+              }}
+            >
+              {content.bottom_cta_button_primary}
+            </Button>
+          </Box>
+        </Box>
+        <Box sx={{}}>
+          <img src={content.header_graphic.data[0].url} width={700} />
+        </Box>
+      </Container>
+    </Box>
+  );
+};
+
+const SecondSection = ({ content, theme }) => {
+  return (
+    <Box>
+      <Container>
+        <Box>
+          <Typography
+            component={'h2'}
+            variant={'p'}
+            paddingTop={10}
+            paddingBottom={10}
+            sx={{
+              color: theme.palette.secondary.darkCharcoal,
+              fontWeight: 800,
+              fontSize: '32px',
+              textAlign: 'center',
+            }}
+            dangerouslySetInnerHTML={{
+              __html: helper.strColorChanger(
+                content.solutions_h2,
+                'digital experience',
+                theme.palette.zesty.zestyOrange,
+              ),
+            }}
+          />
+        </Box>
+        <Box sx={{ display: 'grid', gridTemplateColumns: '1fr 1fr' }}>
+          <Box>
+            <img src={content.solution_1_graphic.data[0].url} alt="" />
+          </Box>
+          <Box>
+            <img src={content.solution_1_graphic.data[0].url} alt="" />
+          </Box>
+        </Box>
+        <Grid item xs={12} md={9}>
+          <Box
+            paddingTop={10}
+            paddingBottom={10}
+            sx={{
+              textAlign: 'center',
+              color: theme.palette.secondary.darkCharcoal,
+            }}
+            dangerouslySetInnerHTML={{
+              __html: helper.strColorChanger(
+                content.about_zesty_dxp,
+                'Zesty',
+                theme.palette.zesty.zestyOrange,
+              ),
+            }}
+          ></Box>
+        </Grid>
+      </Container>
+    </Box>
+  );
+};
+
+const ThirdSection = ({ content, theme }) => {
+  return (
+    <Box>
+      <Container>
+        <Box>
+          <Typography
+            component={'h2'}
+            variant={'p'}
+            paddingTop={10}
+            paddingBottom={10}
+            sx={{
+              color: theme.palette.secondary.darkCharcoal,
+              textAlign: 'center',
+            }}
+            dangerouslySetInnerHTML={{
+              __html: helper.strColorChanger(
+                content.about_dxp,
+                'Digital Experience Platform?',
+                theme.palette.zesty.zestyOrange,
+              ),
+            }}
+          />
+        </Box>
+        <Box
+          sx={{
+            display: 'flex',
+            justifyItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <img src={content.about_dxp_graphic.data[0].url} alt="" />
+        </Box>
+      </Container>
+    </Box>
+  );
+};
 function DigitalExperiencePlatform({ content }) {
   console.log(content, 12222);
   const theme = useTheme();
 
   return (
     <>
-      <Box
-        paddingTop={15}
-        paddingBottom={25}
-        sx={{
-          background:
-            'linear-gradient(180deg, rgba(31,93,192,1) 0%, rgba(112,152,224,1) 85%, rgba(255,255,255,1) 100%);',
-        }}
-      >
-        <Container sx={{ display: 'grid', gridTemplateColumns: '1fr 1fr' }}>
-          <Box
-            sx={{
-              background: '',
-              display: 'flex',
-              justifyContent: 'center',
-              flexDirection: 'column',
-            }}
-          >
-            <Typography
-              component={'h2'}
-              variant={'p'}
-              fontWeight={600}
-              sx={{
-                color: theme.palette.common.white,
-                fontSize: '32px',
-                opacity: '0.8',
-              }}
-            >
-              {'DXP'}
-            </Typography>
-            <Typography
-              component={'h1'}
-              variant={'p'}
-              sx={{
-                color: theme.palette.common.white,
-                fontWeight: 700,
-                fontSize: '48px',
-              }}
-            >
-              {content.header_eyebrow}
-            </Typography>
-            <Typography
-              component={'h3'}
-              variant={'p'}
-              sx={{
-                color: theme.palette.common.white,
-                fontWeight: 500,
-                fontSize: '20px',
-              }}
-            >
-              {content.header_h1}
-            </Typography>
-            <Button>Try Free</Button>
-          </Box>
-          <Box sx={{}}>
-            <img src={content.header_graphic.data[0].url} width={700} />
-          </Box>
-        </Container>
-      </Box>
+      <HeroSection content={content} theme={theme} />
+      <SecondSection content={content} theme={theme} />
+      <ThirdSection content={content} theme={theme} />
     </>
   );
 }
