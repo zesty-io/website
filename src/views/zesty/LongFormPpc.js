f15eac07f7e55cbd780b570ab53bec827f7bb912
Jomar Montuya <61284357+jomarmontuya@users.noreply.github.com>
2022-06-15 02:59:29 +0800

    revisions incomplete

--- src/views/zesty/LongFormPpc.js
+++ src/views/zesty/LongFormPpc.js
@@ -75,6 +75,8 @@ import { zestyLink } from 'lib/zestyLink';
 import MuiMarkdown from 'mui-markdown';
 import * as helper from 'utils';
 import tech_stack from '../../../public/assets/images/headless-cms/tech-stack.png';
+import HeroWithFormAndBackgroundGradient from 'blocks/heroes/HeroWithFormAndBackgroundGradient';
+import { color } from '@mui/system';
 
 function LongFormPpc({ content }) {
   const router = useRouter();
@@ -134,12 +136,7 @@ function LongFormPpc({ content }) {
           onClick={scrollToContactUs}
         />
       ) : (
-        <Box
-          position={'relative'}
-          sx={{
-            backgroundColor: theme.palette.alternate.main,
-          }}
-        >
+        <Box sx={{ position: 'relative', pb: 5 }}>
           <Hero scrollToContactUs={scrollToContactUs} {...headerProps} />
         </Box>
       )}
@@ -195,7 +192,11 @@ function LongFormPpc({ content }) {
       )}
 
       {/* Benefits */}
-      <Box marginTop={6} padding={isMobile ? 0 : 8} bgcolor={'alternate.main'}>
+      <Box
+        marginTop={6}
+        padding={isMobile ? 0 : 8}
+        bgcolor={theme.palette.zesty.zestyDarkBlue}
+      >
         <NewsletterWithImage
           header={content.outline_of_benefits || FillerContent.header}
           image={
@@ -212,11 +213,12 @@ function LongFormPpc({ content }) {
       )}
 
       {/* Form */}
-      <ContactUsForm
+      <PpcShortForm theme={theme} content={content} />
+      {/* <ContactUsForm
         theme={theme}
         content={content}
         formContent={formContent}
-      />
+      /> */}
     </>
   );
 }
@@ -276,25 +278,38 @@ const NewsletterWithImage = ({ image, header, testimonial }) => {
         <Grid item xs={12} md={6}>
           <Box>
             <Box marginBottom={3}>
-              <Grid item xs={12} md={9}>
+              <Grid item xs={12}>
                 <MuiMarkdown
+                  style={{ width: '100%' }}
                   overrides={{
                     h2: {
-                      component: 'h2',
+                      component: Typography,
                       props: {
-                        style: { fontSize: 32, lineHeight: 1.2 },
+                        variant: 'h4',
+                        component: 'h2',
+                        sx: {
+                          color: theme.palette.common.white,
+                          fontWeight: 'bold',
+                          lineHeight: 1.2,
+                        },
                       },
                     },
                     p: {
-                      component: 'p',
+                      component: Typography,
                       props: {
-                        style: { fontSize: 20 },
+                        variant: 'h6',
+                        component: 'p',
+                        sx: { color: theme.palette.common.white },
                       },
                     },
                     ul: {
-                      component: 'ul',
+                      component: Typography,
                       props: {
-                        style: { paddingLeft: 12 },
+                        component: 'ul',
+                        sx: {
+                          paddingLeft: 2,
+                          color: theme.palette.common.white,
+                        },
                       },
                     },
                   }}
@@ -306,7 +321,7 @@ const NewsletterWithImage = ({ image, header, testimonial }) => {
             <Box marginTop={{ xs: 4, sm: 6, md: 8 }} textAlign={'left'}>
               <Grid container spacing={4}>
                 {testimonials.map((item, i) => (
-                  <Grid item xs={12} md={12} key={i}>
+                  <Grid item xs={12} key={i}>
                     <Box
                       width={1}
                       height={1}
@@ -510,11 +525,12 @@ const BgDecorations = ({ theme }) => {
 
 const TechStack = ({ theme, isMobile, content }) => {
   return (
-    <Box component="section" sx={{ px: 4, my: 10 }}>
+    <Box
+      component="section"
+      sx={{ px: 4, background: theme.palette.zesty.zestySeaShell }}
+    >
       <Box
         sx={{
-          background: theme.palette.zesty.zestySeaShell,
-          borderRadius: 10,
           py: 10,
         }}
       >
@@ -563,26 +579,6 @@ const TechStack = ({ theme, isMobile, content }) => {
               >
                 {content.tech_stack_description}
               </Typography>
-
-              <Box sx={{ width: '100%', mt: 4 }}>
-                <Button
-                  href={
-                    content.tech_stack_integration_link.data[0].meta.web.uri
-                  }
-                  component={'a'}
-                  target="_blank"
-                  fullWidth={isMobile}
-                  variant="contained"
-                  sx={{
-                    background: theme.palette.zesty.zestyOrange,
-                    color: theme.palette.common.white,
-                    px: 6,
-                  }}
-                  size="large"
-                >
-                  See All
-                </Button>
-              </Box>
             </Grid>
             <Grid item sm={12} md={6}>
               <Box sx={{ mt: isMobile ? 4 : 0 }}>
@@ -683,8 +679,10 @@ const Hero = ({
               <Typography
                 variant="h3"
                 component="h1"
-                color="text.primary"
-                sx={{ fontWeight: 700 }}
+                sx={{
+                  fontWeight: 700,
+                  color: theme.palette.zesty.zestyZambezi,
+                }}
               >
                 {title}
               </Typography>
@@ -717,21 +715,14 @@ const Hero = ({
             >
               <Box
                 onClick={() => scrollToContactUs()}
-                // href={cta_right_url || FillerContent.href}
                 component={Button}
-                variant="outlined"
-                color={theme.palette.zestyOrange}
+                variant="contained"
                 size="large"
                 marginTop={{ xs: 2, sm: 0 }}
                 fullWidth={isMd ? false : true}
                 sx={{
-                  color: '#FF5D0A',
-                  borderColor: '#FF5D0A',
-                  '&:hover': {
-                    borderColor: '#FF5D0A',
-                    backgroundColor: '#FF5D0A',
-                    color: 'white',
-                  },
+                  color: theme.palette.common.white,
+                  backgroundColor: theme.palette.zesty.zestyOrange,
                 }}
               >
                 {cta_right_text || FillerContent.cta}
@@ -1055,6 +1046,44 @@ const HowItWorks = ({
         </Box>
       </Container>
       <FeatureGridWithBackgrounds images={images || FillerContent.demos} />
+    </>
+  );
+};
+
+const PpcShortForm = ({ content }) => {
+  const theme = useTheme();
+
+  const formContent = {
+    leadDetail: 'Adwords',
+    businessType: 'Direct',
+    leadSource: 'Advertisement',
+    selectedValue: 2,
+    hideSelect: true,
+    hideMessage: true,
+    ctaText: FillerContent.cta,
+    modalTitle: 'Thank you for submitting your information.',
+    modalMessage: 'Our team will be in touch soon to discuss next steps.',
+    displayMsgUnderButton: ' ',
+    additionalTextfield: { company: true, jobTitle: true },
+    buttonFullWidth: true,
+    hidePrivacySection: true,
+    messageLabel: 'Is there anything you would like to cover in the demo?',
+    phoneNumber: true,
+  };
+  return (
+    <>
+      <HeroWithFormAndBackgroundGradient
+        headelineTitle={content.contact_form_h3 || FillerContent.header}
+        description={
+          content.contact_form_description || FillerContent.description
+        }
+        imageCollection={
+          content.logos?.data?.slice(0, 3) || [FillerContent.image]
+        }
+        backgroundImage={content.form_background_image.data[0].url}
+        form_title={content.form_title || FillerContent.header}
+        formContent={formContent}
+      />
     </>
   );
 };
