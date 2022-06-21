0aff716d5679cdce88885fd20637f7eb5933acdc
Jomar Montuya <61284357+jomarmontuya@users.noreply.github.com>
2022-06-22 02:11:08 +0800

    marketplace headers

--- src/components/marketplace/landing/Hero.js
+++ src/components/marketplace/landing/Hero.js
@@ -10,12 +10,7 @@ import MuiMarkdown from 'mui-markdown';
  */
 import FillerContent from 'components/globals/FillerContent';
 
-/**
- * Temporary Assets
- */
-import rocket from '../../../../public/assets/images/marketplace/rocket.png';
-
-const Hero = ({ title, description, features_logos }) => {
+const Hero = ({ title, description, features_logos, hero_image }) => {
   const theme = useTheme();
 
   console.log(features_logos);
@@ -24,68 +19,90 @@ const Hero = ({ title, description, features_logos }) => {
     <>
       <Box
         sx={{
+          position: 'relative',
           minHeight: 560,
+          display: 'flex',
+          flexDirection: 'column',
         }}
         component="header"
       >
-        <Box sx={{ position: 'absolute' }} component="img" src={rocket.src} />
-        <Box sx={{ position: 'relative', zIndex: 1 }}>
-          <Container disableGutters>
+        <Box
+          sx={{
+            flexShrink: 0,
+            minHeight: 560,
+          }}
+          component="img"
+          src={hero_image.data[0].url}
+        />
+        <Box
+          sx={{
+            top: '20%',
+            position: 'absolute',
+            width: '100%',
+            zIndex: 1,
+          }}
+        >
+          <Container>
             <Grid container>
               <Grid item sm={12} md={6}>
-                <Typography
-                  component="h1"
-                  variant="h3"
-                  sx={{ fontWeight: 'bold', color: theme.palette.common.white }}
-                >
-                  {title || FillerContent.header}
-                </Typography>
+                <Box sx={{ display: 'flex', flexDirection: 'column', gap: 3 }}>
+                  <Typography
+                    component="h1"
+                    variant="h3"
+                    sx={{
+                      fontWeight: 'bold',
+                      color: theme.palette.common.white,
+                    }}
+                  >
+                    {title || FillerContent.header}
+                  </Typography>
 
-                <MuiMarkdown
-                  overrides={{
-                    p: {
-                      component: Typography,
-                      props: {
-                        variant: 'h6',
-                        component: 'p',
-                        sx: {
-                          color: theme.palette.common.white,
+                  <MuiMarkdown
+                    overrides={{
+                      p: {
+                        component: Typography,
+                        props: {
+                          variant: 'h6',
+                          component: 'p',
+                          sx: {
+                            color: theme.palette.common.white,
+                          },
                         },
                       },
-                    },
-                  }}
-                >
-                  {description || FillerContent.description}
-                </MuiMarkdown>
+                    }}
+                  >
+                    {description || FillerContent.description}
+                  </MuiMarkdown>
 
-                <Box
-                  sx={{
-                    display: 'flex',
-                    gap: 1,
-                  }}
-                >
-                  {features_logos.data.map((item, index) => (
-                    <Box
-                      sx={{
-                        borderRadius: 1,
-                        display: 'flex',
-                        gap: 1,
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        width: 96,
-                        height: 96,
-                        background: theme.palette.common.white,
-                      }}
-                    >
-                      <Box>
-                        <Box
-                          sx={{ width: '100%', height: 'auto' }}
-                          component="img"
-                          src={item.image.data[0].url}
-                        />
+                  <Box
+                    sx={{
+                      display: 'flex',
+                      gap: 1,
+                    }}
+                  >
+                    {features_logos.data.map((item, index) => (
+                      <Box
+                        sx={{
+                          borderRadius: 1,
+                          display: 'flex',
+                          gap: 1,
+                          justifyContent: 'center',
+                          alignItems: 'center',
+                          width: 90,
+                          height: 90,
+                          background: theme.palette.common.white,
+                        }}
+                      >
+                        <Box sx={{ p: 2 }}>
+                          <Box
+                            sx={{ width: '100%', height: '100%' }}
+                            component="img"
+                            src={item.image.data[0].url}
+                          />
+                        </Box>
                       </Box>
-                    </Box>
-                  ))}
+                    ))}
+                  </Box>
                 </Box>
               </Grid>
               <Grid item sm={12} md={6}></Grid>
