c4e84a05e0a8911607861da658638678cddc1ec4
ardeay <apuzzo@gmail.com>
2022-06-23 09:48:54 -0700

    moving zesty head to _app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -22,15 +22,9 @@ if (process.env.NODE_ENV === 'production') {
 export default function App({ Component, pageProps }) {
   return (
     <React.Fragment>
-      {pageProps?.content ||
-        <ZestyHead content={pageProps.content} />
+      {pageProps?.meta?.web &&
+        <ZestyHead content={pageProps} />
       }
-      <Head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, shrink-to-fit=no"
-        />
-      </Head>
       <Page>
         <Component {...pageProps} />
       </Page>
