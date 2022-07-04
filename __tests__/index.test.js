08a318e74e5ab2bf1ca9737d3285bfa1a4d1c219
darwin808 <darwin.apolinario12@gmail.com>
2022-07-04 21:48:20 +0800

    jest add additional config

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -16,22 +16,23 @@ const getTestingRoutes = async () => {
   return json;
 };
 
-describe('Home', () => {
-  it('***FETCH ALL ROUTES***', async () => {
+describe('ZESTY WEBSITE', () => {
+  test('***FETCH ALL ROUTES***', async () => {
     const routes = await getTestingRoutes();
     const newroutes = routes.slice(0, 10);
     const list = [];
+    const url = env == 'prod' ? prodURL : stageURL;
 
     for await (const element of newroutes) {
-      const response = await fetch(prodURL + element.uri, {
+      const response = await fetch(url + element.uri, {
         method: 'GET',
         redirect: 'manual',
         follow: 0,
       });
-      list.push({ route: element.uri, status: response.status });
-      expect(response.status).not.toBe(700);
+      list.push({ route: url + element.uri, status: response.status });
+      expect(response.status).not.toBe(2200);
     }
 
     console.table(list);
-  });
+  }, 30000);
 });
