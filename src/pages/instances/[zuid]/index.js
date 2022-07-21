b2a1a7bfdc8ead260d319c8ead9687f2c1b64596
darwin808 <darwin.apolinario12@gmail.com>
2022-07-22 01:00:01 +0800

    add intances/webhooks page

---
+++ src/pages/instances/[zuid]/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Box } from '@mui/material';
+import AppBar from 'components/console/AppBar';
+import { Container } from '@mui/system';
+import Main from 'layouts/Main';
+import { useZestyStore } from 'store';
+import Login from 'components/console/Login';
+import { InstancesApp } from 'views/InstancesApp/InstancesApp';
+
+export default function Intances() {
+  const { ZestyAPI, isAuthenticated } = useZestyStore((state) => state);
+
+  return (
+    <Main>
+      <AppBar />
+
+      <Container>
+        instanceView
+        {!isAuthenticated && <Login />}
+        {isAuthenticated && <InstancesApp />}
+      </Container>
+    </Main>
+  );
+}
