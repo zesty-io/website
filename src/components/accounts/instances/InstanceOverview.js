2233a4fa4025fa9d43cd1f1909f44dd53a5ddf3d
Bobbi Tarkany <bl.tarkany@gmail.com>
2022-07-22 12:16:39 -0700

    working with pulling from zesty api

--- src/components/accounts/instances/InstanceOverview.js
+++ src/components/accounts/instances/InstanceOverview.js
@@ -1,5 +1,31 @@
-import React from 'react'
-export default function InstanceOverview(){
+import React, { useEffect, useState } from 'react'
+import { useZestyStore } from 'store';
+import { getCookie } from 'cookies-next';
 
-    return <>My Overview!</>
+
+export default function InstanceOverview(){
+    const { ZestyAPI, workingInstance } = useZestyStore((state)=> state);
+    const instanceZUID = getCookie('ZESTY_WORKING_INSTANCE');
+    console.log("🚀 ~ file: InstanceOverview.js ~ line 9 ~ InstanceOverview ~ instanceZUID", instanceZUID)
+    
+    /* accountsAPIEndpoints:
+    instanceGET: "/instances/INSTANCE_ZUID"
+    */
+
+   try {
+    const gettingIns = async (zuid)=> {
+        let res = await ZestyAPI.getInstance(instanceZUID);
+        console.log("🚀 ~ file: InstanceOverview.js ~ line 18 ~ gettingIns ~ res", res)
+    };
+    gettingIns(instanceZUID);
+   } catch (error) {
+    console.log("🚀 ~ file: InstanceOverview.js ~ line 17 ~ InstanceOverview ~ error", error)
+   }
+    // let data = ZestyAPI.getInstance(instanceZUID);
+    
+
+
+    return (
+        <>My Overview!</>
+    );
 }
