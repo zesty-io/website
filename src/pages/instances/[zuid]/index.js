81ab4765961e8b7be6ddfddebd599e31d9d1e7f9
ardeay <apuzzo@gmail.com>
2022-07-25 11:23:35 -0400

    test

--- src/pages/instances/[zuid]/index.js
+++ src/pages/instances/[zuid]/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Box } from '@mui/material';
 import AppBar from 'components/console/AppBar';
 import { Container } from '@mui/system';
 import Main from 'layouts/Main';
@@ -8,7 +7,7 @@ import Login from 'components/console/Login';
 import { InstancesApp } from 'views/InstancesApp/InstancesApp';
 
 export default function Intances() {
-  const { ZestyAPI, isAuthenticated } = useZestyStore((state) => state);
+  const { isAuthenticated } = useZestyStore((state) => state);
   return (
     <Main>
       <AppBar />
--- src/pages/instances/index.js
+++ src/pages/instances/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { Box } from '@mui/material';
 import AppBar from 'components/console/AppBar';
 import { Container } from '@mui/system';
 import Main from 'layouts/Main';
 import { useZestyStore } from 'store';
 import Login from 'components/console/Login';
 import { InstancesDashboard } from 'views/InstancesApp/InstancesDashboard';
-import { useRouter } from 'next/router';
+
 
 export default function Intances() {
   const { isAuthenticated } = useZestyStore((state) => state);
@@ -15,8 +14,7 @@ export default function Intances() {
       <AppBar />
 
       <Container>
-        {isAuthenticated ? <InstancesDashboard /> : <Login />}
-      </Container>
+        </Container>
     </Main>
   );
 }
