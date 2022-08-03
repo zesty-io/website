17fafd2f12123c371c25d485517bf418761f6831
ardeay <apuzzo@gmail.com>
2022-08-03 15:43:29 -0400

    homepage button

--- src/blocks/heroes/HeroWithIllustrationAndCta/HeroWithIllustrationAndCta.js
+++ src/blocks/heroes/HeroWithIllustrationAndCta/HeroWithIllustrationAndCta.js
@@ -114,7 +114,7 @@ const HeroWithIllustrationAndCta = ({
                   >
                     <Button
                       component={'a'}
-                      href={hero_button_right_link ? hero_button_right_link : '/demos/'}
+                      href={button_right_link ? button_right_link : '/demos/'}
                       variant="outlined"
                       color="secondary"
                       size="large"
