a9cbbc41ed0159b47c1d707a66cfc4d0401175cc
jomarmontuya <jomar.montuya@outlook.com>
2022-08-08 17:38:51 +0800

    merge stage

--- src/views/zesty/HeadlessCmsFeature.js
+++ src/views/zesty/HeadlessCmsFeature.js
@@ -1,24 +1,23 @@
 /**
  * Zesty.io Content Model Component
  * When the ZestyLoader [..slug].js file is used, this component will autoload if it associated with the URL
- * 
- * Label: Headless CMS Features 
- * Name: headless_cms_features 
+ *
+ * Label: Headless CMS Features
+ * Name: headless_cms_features
  * Model ZUID: 6-b0ad9acbea-721hmm
  * File Created On: Wed Jun 15 2022 00:51:14 GMT+0800 (Philippine Standard Time)
- * 
+ *
  * Model Fields:
- * 
- 
- * 
+ *
+ *
  * In the render function, text fields can be accessed like {content.field_name}, relationships are arrays,
  * images are objects {content.image_name.data[0].url}
- * 
+ *
  * This file is expected to be customized; because of that, it is not overwritten by the integration script.
  * Model and field changes in Zesty.io will not be reflected in this comment.
- * 
+ *
  * View and Edit this model's current schema on Zesty.io at https://8-aaeffee09b-7w6v22.manager.zesty.io/schema/6-b0ad9acbea-721hmm
- * 
+ *
  * Data Output Example: https://zesty.org/services/web-engine/introduction-to-parsley/parsley-index#tojson
  * Images API: https://zesty.org/services/media-storage-micro-dam/on-the-fly-media-optimization-and-dynamic-image-manipulation
  */
