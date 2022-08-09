ca0972c19721e19243ffd0fd072384adbbb8f4ac
Randy Apuzzo <apuzzo@gmail.com>
2022-08-09 17:56:44 -0400

    welcome screen shell

---
+++ src/components/marketing/Join/WelcomeScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Box} from '@mui/material'
+export const WelcomeScreen = () => {
+
+  return (
+    <Box sx={{height: '400px', background: '#e8e8e8'}}>
+         Welcome to Zesty
+
+    </Box>
+  )
+}
