e5da62fe1a33d0d8c88e92b3159f34407e96843b
Randy Apuzzo <apuzzo@gmail.com>
2022-08-08 23:46:19 -0400

    adding signup inputs

--- src/components/marketing/Join/Signup.jsx
+++ src/components/marketing/Join/Signup.jsx
@@ -1,13 +1,40 @@
-import React from 'react'
-import { Container, Stack, Button, Box, TextField, Typography } from '@mui/material';
+import * as React from 'react';
+import { Container, Stack, IconButton, Button,InputLabel,FilledInput,FormControl, InputAdornment, Box, TextField, Typography } from '@mui/material';
+import { EmailOutlined } from '@mui/icons-material';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+
 
 export const Signup = ({
     message = 'What team are you from?', 
   }) => {
+
+    const [values, setValues] = React.useState({
+      lastname: '',
+      firtname: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+      showPassword: false,
+   });
+   const handleChange = e => {
+      setValues({...values, [e.target.name]: e.target.value})
+   }
+   const handleMouseDownPassword = () => {
+    event.preventDefault();
+  };
+    const handleClickShowPassword = () => {
+      setValues({
+        ...values,
+        showPassword: !values.showPassword,
+      });
+    };
   return (
+
     <Container>
         
         <Typography>{message}</Typography>
+        <EmailOutlined />
         <Box
           component="form"
           sx={{
@@ -18,18 +45,52 @@ export const Signup = ({
         >
           <div>
             <TextField
-              error
-              id="outlined-error"
-              label="Error"
-              defaultValue="Hello World"
+              // error
+              label="First Name"
+              name="firstname"
+              onChange={handleChange}
             />
             <TextField
-              error
-              id="outlined-error-helper-text"
-              label="Error"
-              defaultValue="Hello World"
+              label="Last Name"
               helperText="Incorrect entry."
+              name="lastname"
+              onChange={handleChange}
             />
+            <FormControl variant="standard">
+            <TextField
+              label="Email"
+              name="email"
+              onChange={handleChange}
+              startAdornment={
+                <InputAdornment position="start">
+                  <EmailOutlined />
+                </InputAdornment>
+              }
+            />
+            </FormControl>
+            <FormControl sx={{ m: 1, width: '25ch' }} variant="filled">
+          <InputLabel htmlFor="filled-adornment-password">Password</InputLabel>
+          <FilledInput
+            id="filled-adornment-password"
+            type={values.showPassword ? 'text' : 'password'}
+            value={values.password}
+            name="password"
+            onChange={handleChange}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+          />
+        </FormControl>
+
           </div>
         </Box>
     </Container>
