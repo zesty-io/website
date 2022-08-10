4471a4a2f10526baaf99cb9cd6f7e9254ca24b80
glespinosa <espinosagian1995@gmail.com>
2022-08-11 00:29:11 +0800

    feat(component): mUI styling Instance dashboard,accounts,navigation,instance
    Styling instance dashboard,accounts,navigation , instance
    
    https://github.com/zesty-io/website/issues/771

--- src/components/accounts/instances/InstanceHeader.js
+++ src/components/accounts/instances/InstanceHeader.js
@@ -1,26 +1,56 @@
-import { Box, Button, Grid, Typography } from '@mui/material';
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography,
+} from '@mui/material';
 import React from 'react';
+import { useMediaQuery } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
 
 export default function InstanceHeader({ instance }) {
   const webengineUrl = `https://${instance?.randomHashID}-dev.webengine.zesty.io`;
   const managerURl = `https://${instance?.ZUID}.manager.zesty.io`;
-  return (
-    <Box>
-      <Grid container>
-        <Grid item md={6} xs={12} lg={6}>
-          <Typography variant="h3">{instance?.name}</Typography>
-          <Typography variant="h6">{instance?.createdAt}</Typography>
-        </Grid>
+  const theme = useTheme();
+  const isSM = useMediaQuery(theme.breakpoints.down('md'));
 
-        <Grid item md={6} xs={12} lg={6}>
-          <Button variant={'contained'} size="large" href={managerURl}>
-            Edit Content
-          </Button>
-          <Button variant={'text'} size="large" href={webengineUrl}>
-            Preview Website
-          </Button>
-        </Grid>
-      </Grid>
-    </Box>
+  return (
+    <Card sx={{ maxWidth: isSM ? '100%' : 345 }}>
+      <CardMedia
+        component="img"
+        height="100%"
+        image={instance?.screenshotURL}
+        alt="screenshot"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {instance?.name}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Updated at: {instance.updatedAt}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button
+          size="small"
+          color="secondary"
+          target="_blank"
+          href={managerURl}
+        >
+          Edit Content
+        </Button>
+
+        <Button
+          size="small"
+          target="_blank"
+          color="secondary"
+          href={webengineUrl}
+        >
+          Preview Website
+        </Button>
+      </CardActions>
+    </Card>
   );
 }
--- src/components/accounts/instances/InstancesDashboard.js
+++ src/components/accounts/instances/InstancesDashboard.js
@@ -1,12 +1,96 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import { useRouter } from 'next/router';
 import { useZestyStore } from 'store';
-import { Box, Button, TextField } from '@mui/material';
-import { HorizontalRule } from '@mui/icons-material';
-import SearchIcon from '@mui/icons-material/Search';
+import {
+  Box,
+  Card,
+  CardMedia,
+  TextField,
+  Typography,
+  Grid,
+  InputAdornment,
+  ToggleButtonGroup,
+  ToggleButton,
+  Stack,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from '@mui/material';
 import { setCookie } from 'cookies-next';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import FormatListBulletedOutlinedIcon from '@mui/icons-material/FormatListBulletedOutlined';
+import GridViewOutlinedIcon from '@mui/icons-material/GridViewOutlined';
+
+const testarr = [
+  {
+    ZUID: '8-45a294a-btwkv9',
+    name: '! DEV Parsley',
+    screenshotURL:
+      'https://storage.googleapis.com/zesty-prod-instance-screenshots/parsley.zesty.io_1920_1080._400_200.png',
+    updatedAt: '2022-06-24T13:30:49Z',
+  },
+  {
+    ZUID: '8-45a294a-btwkv9',
+    name: '! DEV Parsley',
+    screenshotURL:
+      'https://storage.googleapis.com/zesty-prod-instance-screenshots/parsley.zesty.io_1920_1080._400_200.png',
+    updatedAt: '2022-06-24T13:30:49Z',
+  },
+  {
+    ZUID: '8-45a294a-btwkv9',
+    name: '! DEV Parsley',
+    screenshotURL:
+      'https://storage.googleapis.com/zesty-prod-instance-screenshots/parsley.zesty.io_1920_1080._400_200.png',
+    updatedAt: '2022-06-24T13:30:49Z',
+  },
+  {
+    ZUID: '8-45a294a-btwkv9',
+    name: '! DEV Parsley',
+    screenshotURL:
+      'https://storage.googleapis.com/zesty-prod-instance-screenshots/parsley.zesty.io_1920_1080._400_200.png',
+    updatedAt: '2022-06-24T13:30:49Z',
+  },
+  {
+    ZUID: '8-45a294a-btwkv9',
+    name: '! DEV Parsley',
+    screenshotURL:
+      'https://storage.googleapis.com/zesty-prod-instance-screenshots/parsley.zesty.io_1920_1080._400_200.png',
+    updatedAt: '2022-06-24T13:30:49Z',
+  },
+  {
+    ZUID: '8-45a294a-btwkv9',
+    name: '! DEV Parsley',
+    screenshotURL:
+      'https://storage.googleapis.com/zesty-prod-instance-screenshots/parsley.zesty.io_1920_1080._400_200.png',
+    updatedAt: '2022-06-24T13:30:49Z',
+  },
+  {
+    ZUID: '8-45a294a-btwkv9',
+    name: '! DEV Parsley',
+    screenshotURL:
+      'https://storage.googleapis.com/zesty-prod-instance-screenshots/parsley.zesty.io_1920_1080._400_200.png',
+    updatedAt: '2022-06-24T13:30:49Z',
+  },
+  {
+    ZUID: '8-45a294a-btwkv9',
+    name: '! DEV Parsley',
+    screenshotURL:
+      'https://storage.googleapis.com/zesty-prod-instance-screenshots/parsley.zesty.io_1920_1080._400_200.png',
+    updatedAt: '2022-06-24T13:30:49Z',
+  },
+  {
+    ZUID: '8-45a294a-btwkv9',
+    name: '! DEV Parsley',
+    screenshotURL:
+      'https://storage.googleapis.com/zesty-prod-instance-screenshots/parsley.zesty.io_1920_1080._400_200.png',
+    updatedAt: '2022-06-24T13:30:49Z',
+  },
+];
+
 export const InstancesDashboard = () => {
+  const [view, setView] = useState('grid');
   const router = useRouter();
   const { ZestyAPI } = useZestyStore((state) => state);
 
@@ -18,11 +102,15 @@ export const InstancesDashboard = () => {
     setInstances(instances?.data ? instances.data : []);
   }
 
+  const handleChangeView = (e, value) => {
+    setView(value);
+  };
+
   React.useEffect(() => {
     instances.length === 0 && getInstances();
   }, [instances]);
 
-  const handleChange = (zuid) => {
+  const handleRoute = (zuid) => {
     setCookie('ZESTY_WORKING_INSTANCE', zuid);
     router.push({
       pathname: `/instances/${zuid}/`,
@@ -33,24 +121,85 @@ export const InstancesDashboard = () => {
   };
 
   return (
-    <Box>
-      <HorizontalRule />
-      <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
-        <SearchIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+    <Box py={3}>
+      <Stack direction="row" spacing={2}>
         <TextField
-          onChange={(event) => handleSearch(event.target.value)}
-          label="Search"
-          variant="standard"
+          label="Search by instance name"
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="start">
+                <SearchOutlinedIcon />
+              </InputAdornment>
+            ),
+          }}
+          variant="outlined"
+          fullWidth
+          color="secondary"
+          onChange={(e) => handleSearch(e.target.value)}
         />
-      </Box>
-      {instances
-        .filter((inst) => inst?.name?.toLowerCase().includes(search))
-        .map((instance) => (
-          <Box key={`${instance.ZUID}-instancelist`}>
-            {instance.ZUID} {instance.name}
-            <Button onClick={() => handleChange(instance.ZUID)}>View</Button>
-          </Box>
-        ))}
+      </Stack>
+
+      <Stack mt={2} direction="row" spacing={2}>
+        <ToggleButtonGroup
+          value={view}
+          exclusive
+          color="secondary"
+          onChange={handleChangeView}
+        >
+          <ToggleButton value="grid">
+            <GridViewOutlinedIcon />
+          </ToggleButton>
+          <ToggleButton value="list">
+            <FormatListBulletedOutlinedIcon />
+          </ToggleButton>
+        </ToggleButtonGroup>
+      </Stack>
+
+      <Grid container direction="row" my={2} spacing={2}>
+        {view === 'grid' &&
+          testarr
+            .filter((inst) => inst?.name?.toLowerCase().includes(search))
+            .map((instance, index) => (
+              <Grid item xs={12} sm={4} lg={3} key={index}>
+                <Card sx={{ cursor: 'pointer' }}>
+                  <CardMedia
+                    height="100%"
+                    width="100%"
+                    component="img"
+                    image={instance.screenshotURL}
+                    onClick={() => handleRoute(instance.ZUID)}
+                  />
+                  <Typography p={1} gutterBottom variant="h6">
+                    {instance.name}
+                  </Typography>
+                </Card>
+              </Grid>
+            ))}
+      </Grid>
+
+      {view === 'list' && (
+        <List>
+          {testarr
+            .filter((inst) => inst?.name?.toLowerCase().includes(search))
+            .map((instance, index) => (
+              <ListItem divider key={index} disablePadding>
+                <ListItemButton onClick={() => handleRoute(instance.ZUID)}>
+                  <ListItemIcon>
+                    <img
+                      height="50px"
+                      width="50px"
+                      src={instance.screenshotURL}
+                    />
+                  </ListItemIcon>
+                  <ListItemText
+                    primary={instance.name}
+                    secondary={`Updated ${instance.updatedAt}`}
+                  />
+                </ListItemButton>
+              </ListItem>
+            ))}
+        </List>
+      )}
     </Box>
   );
 };
