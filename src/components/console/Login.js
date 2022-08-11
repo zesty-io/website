f8849ea818462ca9b1bd54cf65449fbbaf55de1f
darwin808 <darwin.apolinario12@gmail.com>
2022-08-11 16:53:21 +0800

    style(accounts/login): added logo

--- src/components/console/Login.js
+++ src/components/console/Login.js
@@ -12,6 +12,7 @@ import { useFormik } from 'formik';
 import React from 'react';
 import { useZestyStore } from 'store';
 import * as helpers from 'utils';
+import { Box } from '@mui/system';
 
 const Login = () => {
   const { ZestyAPI } = useZestyStore((state) => state);
@@ -56,21 +57,46 @@ const Login = () => {
   });
 
   return (
-    <Grid container>
-      <Grid item xs={12}>
-        <form noValidate onSubmit={formik.handleSubmit}>
-          <FormInput name={'email'} formik={formik} />
-          <FormInput name={'password'} type="password" formik={formik} />
-          <Button color="secondary" variant="contained" fullWidth type="submit">
-            {loading ? (
-              <CircularProgress color="primary" />
-            ) : (
-              <Typography variant="h6">Login</Typography>
-            )}
-          </Button>
-        </form>
+    <Box>
+      <Grid container>
+        <Grid item xs={4} />
+        <Grid item xs={4}>
+          <Box
+            display={'flex'}
+            flexDirection="column"
+            justifyItems={'center'}
+            justifyContent="center"
+          >
+            <Box
+              paddingY={2}
+              component="img"
+              sx={{
+                height: 200,
+              }}
+              alt="Zesty-io Login"
+              src="https://brand.zesty.io/zesty-io-logo-vertical.svg"
+            />
+            <form noValidate onSubmit={formik.handleSubmit}>
+              <FormInput name={'email'} formik={formik} />
+              <FormInput name={'password'} type="password" formik={formik} />
+              <Button
+                color="secondary"
+                variant="contained"
+                fullWidth
+                type="submit"
+              >
+                {loading ? (
+                  <CircularProgress color="primary" />
+                ) : (
+                  <Typography variant="h6">Login</Typography>
+                )}
+              </Button>
+            </form>
+          </Box>
+        </Grid>
+        <Grid item xs={4} />
       </Grid>
-    </Grid>
+    </Box>
   );
 };
 
