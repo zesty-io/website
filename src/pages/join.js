99137edc46888c13ca99052c3eb98b006fbf99ea
Randy Apuzzo <apuzzo@gmail.com>
2022-08-13 14:47:33 -0400

    zoho lead capture

--- src/pages/join.js
+++ src/pages/join.js
@@ -1,3 +1,4 @@
+// REact and MUI Imports
 import { React, useEffect, useState, useRef, useCallback } from 'react';
 import { useTheme } from '@emotion/react';
 import { Container, Box, Button, Grid, Typography } from '@mui/material';
@@ -5,6 +6,7 @@ import { Container, Box, Button, Grid, Typography } from '@mui/material';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination, Navigation } from "swiper";
+
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
@@ -195,6 +197,7 @@ export default function Join(props) {
       sliderRef.current.swiper.slidePrev();
     }, []);
   
+    // moves user forward a slide in the onboard process
     const handleNext = useCallback(() => {
       if (!sliderRef.current) return;
       sliderRef.current.swiper.slideNext();
@@ -202,6 +205,7 @@ export default function Join(props) {
 
     }, []);
 
+    // captures the user question 
     const handleAnswers = async (question,answer,store=false) => {
         if(store !== false){
             if(store=='role') { setRole(answer); setCookie('persona',answer) }
@@ -213,22 +217,40 @@ export default function Join(props) {
         await slackQuestionPost(question,answer,email)
     }
 
+    const stringifyLead = (obj) => {
+        let str = '';
+        for (var key in obj) {
+            if (obj.hasOwnProperty(key)) {
+                str += key + ": " + obj[key] + '\n';
+            }
+        }
+        return str;
+    }
+    // creates a zesty user and send data into ZOHO leads
     const signUpSuccess = async  (userDetails) => {
+        // send user forward visually, then capture their data
+        handleNext();
         //store email to state
         setEmail(userDetails.email);
+        // notify the team in slack
         await slackNotify(`Captured: ${userDetails.email}`)
         // map additional userDetails for zoho object
         userDetails.message = `Project type: ${projectType}`;
         // instantiate zoho object
         userDetails.user = true;
-        console.log(zohoPostObject(userDetails))
-        let zohoData = await postToZOHO(zohoPostObject(userDetails))
+        // setup zoho object 
+        let zohoLeadObject = zohoPostObject(userDetails);
+        // zoho capture backup
+        slackNotify(`ZOHO lead slack fallback info: \n ${stringifyLead(zohoLeadObject)}`)
+        // post lead to zoho
+        let zohoData = await postToZOHO(zohoLeadObject)
         let zoholeadlink = 'https://one.zoho.com/zohoone/zestyio/home/cxapp/crm/org749642405/tab/Leads/'
         await slackNotify(`View lead for ${userDetails.email} on ZOHO @ ${zoholeadlink}${zohoData.data[0].details.id}`)
         setCurrentAnimation('party');
-        handleNext();
+        
     }
 
+    // leaves the onboard program
     const handleExit = () => {
         window.location = '/'
     }
@@ -242,135 +264,111 @@ export default function Join(props) {
         handleNext();
     }
 
+    // modifies the logo animation
     const handleAnimation = (ani) => {
         setCurrentAnimation(ani)
     }
 
 
     return (
-    <Box>
-        <DancingLogo animation={currentAnimation} />
-        <Swiper
-            pagination={{
-            type: "progressbar",
-            }}
-            ref={sliderRef}
-            navigation={false}
-            modules={[Pagination, Navigation]}
-        >
-            <SwiperSlide> 
-                <Grid container>  
-                    <Grid item lg={7} md={7} xs={12}>
-                        <WelcomeScreen />
-                    </Grid>  
-                    <Grid item lg={5} md={5} xs={12}>
-                        <Container sx={{padding: '5em'}}>
+        <Box>
+            <DancingLogo animation={currentAnimation} />
+            <Swiper
+                pagination={{
+                type: "progressbar",
+                }}
+                ref={sliderRef}
+                navigation={false}
+                modules={[Pagination, Navigation]}
+            >
+                <SwiperSlide> 
+                    <Grid container>  
+                        <Grid item lg={7} md={7} xs={12}>
+                            <WelcomeScreen />
+                        </Grid>  
+                        <Grid item lg={5} md={5} xs={12}>
+                            <Container sx={{padding: '5em'}}>
 
-                            <SlideMessage 
-                                message={firstMessage} 
-                                buttonText={firstButton} 
-                                exitButtonText={'No, get me outta here!'}
-                                exitButtonAction={handleExit}
-                                answerCallBack={handlePrompt} 
-                                hoverAnimation={handleAnimation}
-                                
-                            />
-                        </Container>
-                    </Grid>
-                   
-                </Grid>
-            </SwiperSlide>
-             {/* Question 1  */}
-            <SwiperSlide>
-                <Grid container>
-                    <Grid item lg={6} md={6} xs={12}>
-                        <SlideQuestions 
-                            question={firstQuestion} 
-                            answers={firstAnswers} 
-                            answerCallBack={handleAnswers}
-                            hoverAnimation={handleAnimation}
-                            storeValue='role'
-                            />
-                    </Grid>
-                    <Grid item lg={6} md={6} xs={12}>
-                       What is Zesty component
-                    </Grid>
-                </Grid>
-            </SwiperSlide>
-             {/* Question 2  */}
-            <SwiperSlide>
-                <SlideQuestions 
-                    question={secondQuestion} 
-                    answers={secondAnswers} 
-                    answerCallBack={handleAnswers} 
-                    hoverAnimation={handleAnimation} 
-                    storeValue='projectType'
-                    />
-            </SwiperSlide>
-            {/* Signup  */}
-            <SwiperSlide>
-                <Signup 
-                    message="Thanks, now let's get your started!." 
-                    callback={signUpSuccess}
-                    />
-            </SwiperSlide>
-            <SwiperSlide> 
-                <Grid container>  
+                                <SlideMessage 
+                                    message={firstMessage} 
+                                    buttonText={firstButton} 
+                                    exitButtonText={'No, get me outta here!'}
+                                    exitButtonAction={handleExit}
+                                    answerCallBack={handlePrompt} 
+                                    hoverAnimation={handleAnimation}
+                                    
+                                />
+                            </Container>
+                        </Grid>
                     
-                    <Grid item lg={5} md={5} xs={12}>
-                        <Container sx={{padding: '5em'}}>
-
-                            <SlideMessage 
-                                message={<h1>Welcome to Zesty!</h1>}
-                                buttonText={`Let's Go!`} 
-                                exitButtonText={'Wait, let me invite friends ;)'}
-                                exitButtonAction={handleInvite}
-                                answerCallBack={handlePrompt} 
-                                hoverAnimation={handleAnimation}
-                                
-                            />
-                        </Container>
                     </Grid>
-                   
-                </Grid>
-            </SwiperSlide>
-            
-            
-            <SwiperSlide>
-                <>
-                    <SlideQuestions question={thirdQuestion} answers={thirdAnswers} answerCallBack={handleAnswers} hoverAnimation={handleAnimation} />
-                    <Button onClick={handleNext}  variant="outlined">Skip</Button>
-                </>
-            </SwiperSlide>
-            <SwiperSlide>NPM starter, Youtube Video, Join Community Chat, Talk to an onbording specialist</SwiperSlide>
-        </Swiper>
-       
-    </Box>
+                </SwiperSlide>
+                {/* Question 1  */}
+                <SwiperSlide>
+                    <Grid container>
+                        <Grid item lg={6} md={6} xs={12}>
+                            <SlideQuestions 
+                                question={firstQuestion} 
+                                answers={firstAnswers} 
+                                answerCallBack={handleAnswers}
+                                hoverAnimation={handleAnimation}
+                                storeValue='role'
+                                />
+                        </Grid>
+                        <Grid item lg={6} md={6} xs={12}>
+                        What is Zesty component
+                        </Grid>
+                    </Grid>
+                </SwiperSlide>
+                {/* Question 2  */}
+                <SwiperSlide>
+                    <SlideQuestions 
+                        question={secondQuestion} 
+                        answers={secondAnswers} 
+                        answerCallBack={handleAnswers} 
+                        hoverAnimation={handleAnimation} 
+                        storeValue='projectType'
+                        />
+                </SwiperSlide>
+                {/* Signup  */}
+                <SwiperSlide>
+                    <Signup 
+                        message="Thanks, now let's get your started!." 
+                        callback={signUpSuccess}
+                        />
+                </SwiperSlide>
+                <SwiperSlide> 
+                    <Grid container>  
+                        
+                        <Grid item lg={5} md={5} xs={12}>
+                            <Container sx={{padding: '5em'}}>
+
+                                <SlideMessage 
+                                    message={<h1>Welcome to Zesty!</h1>}
+                                    buttonText={`Let's Go!`} 
+                                    exitButtonText={'Wait, let me invite friends ;)'}
+                                    exitButtonAction={handleInvite}
+                                    answerCallBack={handlePrompt} 
+                                    hoverAnimation={handleAnimation}
+                                    
+                                />
+                            </Container>
+                        </Grid>
+                    
+                    </Grid>
+                </SwiperSlide>
+                
+                
+                <SwiperSlide>
+                    <>
+                        <SlideQuestions question={thirdQuestion} answers={thirdAnswers} answerCallBack={handleAnswers} hoverAnimation={handleAnimation} />
+                        <Button onClick={handleNext}  variant="outlined">Skip</Button>
+                    </>
+                </SwiperSlide>
+                <SwiperSlide>NPM starter, Youtube Video, Join Community Chat, Talk to an onbording specialist</SwiperSlide>
+            </Swiper>
+        
+        </Box>
        
     )
 }
-
-
-const payload = {
-    "question": '',
-    "answer": '',
-    "path": '',
-    "email": '',
-  }
-  
-  function sendPayload(payload){
-    if(isLive()){
-      fetch('https://us-central1-zesty-prod.cloudfunctions.net/onboardQuestion', {
-        method: 'POST',
-        credentials: 'omit',
-        body:    JSON.stringify(payload),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      });
-    } else {
-      console.log(payload)
-    }
-  }
-
-  // sendPayload(payload); 
