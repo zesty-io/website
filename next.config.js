b88ddfbe1c3a3a3255377b48a7f700b073811915
Jomar Montuya <jomar.montuya@outlook.com>
2022-08-20 01:13:21 +0700

    fix conflicts

--- next.config.js
+++ next.config.js
@@ -1,16 +1,21 @@
+const { fetchZestyRedirects } = require('./src/lib/zesty/fetchRedirects');
+
 module.exports = {
-  "trailingSlash": true,
-  "env": {
-    "zesty": {
-      "instance_zuid": "8-aaeffee09b-7w6v22",
-      "stage": "https://kfg6bckb-dev.webengine.zesty.io",
-      "production": "https://www.zesty.io",
-      "stage_password": "",
-      "auth": "",
-      "src_dir": "/src"
+  trailingSlash: true,
+  async redirects() {
+    return await fetchZestyRedirects();
+  },
+  env: {
+    zesty: {
+      instance_zuid: '8-aaeffee09b-7w6v22',
+      stage: 'https://kfg6bckb-dev.webengine.zesty.io',
+      production: 'https://www.zesty.io',
+      stage_password: '',
+      auth: '',
+      src_dir: '/src',
     },
-    "eslint": {
-      "ignoreDuringBuilds": true
-    }
-  }
-}
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+  },
+};
