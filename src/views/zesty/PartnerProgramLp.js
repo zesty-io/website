59cb415a5aa002a50be43088d005f77defc93639
Jomar Montuya <jomar.montuya@outlook.com>
2022-08-20 00:40:43 +0700

    remove dots

--- src/views/zesty/PartnerProgramLp.js
+++ src/views/zesty/PartnerProgramLp.js
@@ -79,26 +79,7 @@ function PartnerProgramLp({ content }) {
 
   return (
     <>
-      <Box
-        sx={{
-          position: 'relative',
-          '&::after': {
-            position: 'absolute',
-            content: '""',
-            width: '30%',
-            zIndex: 1,
-            top: 0,
-            left: '5%',
-            height: '100%',
-            backgroundSize: '18px 18px',
-            backgroundImage: `radial-gradient(${alpha(
-              theme.palette.primary.dark,
-              0.4,
-            )} 20%, transparent 20%)`,
-            opacity: 0.2,
-          },
-        }}
-      >
+      <Box>
         <Hero {...pageData} />
       </Box>
       <WhyZesty {...pageData} />
