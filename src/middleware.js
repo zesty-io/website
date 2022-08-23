1f001d79cdc6928742bd4503db7d072973c02df4
darwin808 <darwin.apolinario12@gmail.com>
2022-08-23 17:34:03 +0800

    feat(middleware): added nextjs middleware and upgrade next to v12.2

---
+++ src/middleware.js
@@ -0,0 +1,17 @@
+import { NextResponse } from 'next/server';
+
+export function middleware(request) {
+  const isAuthenticated = request.cookies.get('isAuthenticated');
+  //   const response = NextResponse.next();
+  //   const appSid = request.cookies.get('APP_SID');
+  //   const isUser = request.cookies.get('isUser');
+  //   console.log(response.cookies, appSid, isAuthenticated, isUser, 222);
+
+  if (!isAuthenticated) {
+    return NextResponse.rewrite(new URL('/instances', request.url));
+  }
+}
+
+export const config = {
+  matcher: ['/instances/:path*', '/profile/:path*'],
+};
