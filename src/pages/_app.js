67138289fcad2bf36bc88443e14ec7a6095aae93
Randy Apuzzo <apuzzo@gmail.com>
2022-08-25 15:22:39 -0400

    gtm tag

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -50,15 +50,6 @@ export default function App({ Component, pageProps }) {
         <ZestyHead content={pageProps} />
       }
       <Page>
-      
-        <noscript>
-          <iframe 
-              src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
-              height="0" 
-              width="0" 
-              style="display:none;visibility:hidden"></iframe>
-        </noscript>
-
         <Component {...pageProps} />
       </Page>
     </React.Fragment>
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -29,21 +29,14 @@ export default class MyDocument extends Document {
             href="https://unpkg.com/aos@next/dist/aos.css"
           />
            {/* Global Site Tag (gtag.js) - Google Analytics */}
-      
-       <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${GTM_ID}`}
-          />
-
           <script
             dangerouslySetInnerHTML={{
               __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GTM_ID}', {
-              page_path: window.location.pathname,
-            });
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${GTM_ID}');
           `,
             }}
           />
