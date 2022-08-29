1d50634b5de3e5c3dfc6acbdffda47ee6f339851
darwin808 <darwin.apolinario12@gmail.com>
2022-08-29 12:22:30 +0800

    feat(overview): initial commit

---
+++ src/components/accounts/instances/OverviewTabs.js
@@ -0,0 +1,37 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
+
+const tabList = [
+  { label: 'News', value: 'news' },
+  { label: 'Releases', value: 'releases' },
+  { label: 'Updates', value: 'updates' },
+  { label: 'Status', value: 'status' },
+];
+export const OverviewTabs = () => {
+  const [value, setValue] = React.useState('1');
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ width: '100%', typography: 'body1' }}>
+      <TabContext value={value}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <TabList onChange={handleChange} aria-label="lab API tabs example">
+            {tabList.map((e) => {
+              return <Tab label={e.label} value={e.value} />;
+            })}
+          </TabList>
+        </Box>
+        <TabPanel value="1">Item One</TabPanel>
+        <TabPanel value="2">Item Two</TabPanel>
+        <TabPanel value="3">Item Three</TabPanel>
+      </TabContext>
+    </Box>
+  );
+};
--- src/pages/instances/[zuid]/index.js
+++ src/pages/instances/[zuid]/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { useZestyStore, getZestyAPI } from 'store';
 import { useRouter } from 'next/router';
-import { Typography } from '@mui/material';
+import {
+  Box,
+  Button,
+  InputAdornment,
+  TextField,
+  Typography,
+} from '@mui/material';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import { OverviewTabs } from 'components/accounts';
 
-export default function Instance() {
-  const { setZestyAPI } = useZestyStore((state) => state);
+export default function Overview() {
+  const { setZestyAPI, userInfo } = useZestyStore((state) => state);
   const router = useRouter();
   const { zuid } = router.query;
 
@@ -14,11 +22,37 @@ export default function Instance() {
 
   return (
     <>
-      <Typography variant="h2">Overview!</Typography>
+      <TextField
+        label="Search"
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="start">
+              <SearchOutlinedIcon />
+            </InputAdornment>
+          ),
+        }}
+        variant="outlined"
+        fullWidth
+        color="secondary"
+        onChange={(e) => console.log(e.target.value)}
+      />
+      <Box>
+        <Typography>Welcome {userInfo?.firstName}</Typography>
+        <Typography>Catch up on the latest Zesty news</Typography>
+      </Box>
+      <Box>
+        <Button
+          variant="contained"
+          href="https://accounts.zesty.io/instances/create"
+        >
+          Create Instance
+        </Button>
+        <OverviewTabs />
+      </Box>
     </>
   );
 }
 
-Instance.data = {
+Overview.data = {
   container: 'InstanceContainer',
 };
