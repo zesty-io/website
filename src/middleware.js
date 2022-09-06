798e78e041b369e18846e3e98f76c3a2b452f673
glespinosa <espinosagian1995@gmail.com>
2022-09-07 00:36:34 +0800

    refactor(middleware): middleware refactor
    middleware refactor

--- src/middleware.js
+++ src/middleware.js
@@ -3,6 +3,22 @@ import { NextResponse } from 'next/server';
 export async function middleware(request) {
   const { pathname } = request.nextUrl;
   const PUBLIC_FILE = /\.(.*)$/;
+
+  if (!isAuthenticated(request)) {
+    if (isProtectedRoute(pathname)) {
+      return NextResponse.redirect(new URL('/login/', request.url));
+    }
+    return NextResponse.next();
+  } else {
+    if (isProtectedRoute(pathname) || PUBLIC_FILE.test(pathname)) {
+      return NextResponse.next();
+    } else {
+      return NextResponse.redirect(new URL('/instances/', request.url));
+    }
+  }
+}
+
+const isAuthenticated = async (request) => {
   const NODE_ENV = process.env.NODE_ENV;
   const verifyUrl =
     NODE_ENV === 'development'
@@ -21,23 +37,8 @@ export async function middleware(request) {
   });
   const data = await response.json();
 
-  let isAuthenticated = data?.status === 'OK' ? true : false;
-  console.log({ isAuthenticated, data });
-  console.log({ pathname });
-
-  if (!isAuthenticated) {
-    if (isProtectedRoute(pathname)) {
-      return NextResponse.redirect(new URL('/login/', request.url));
-    }
-    return NextResponse.next();
-  } else {
-    if (isProtectedRoute(pathname) || PUBLIC_FILE.test(pathname)) {
-      return NextResponse.next();
-    } else {
-      return NextResponse.redirect(new URL('/instances/', request.url));
-    }
-  }
-}
+  return data?.status === 'OK' ? true : false;
+};
 
 const isProtectedRoute = (pathname) => {
   if (
