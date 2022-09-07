d879dff53eabb17f76f001fcc1f69b1b370af002
Jomar Montuya <jomar.montuya@outlook.com>
2022-09-08 00:22:34 +0800

    inital commit complet hero

---
+++ src/components/marketing/DxpFeatures/Hero.js
@@ -0,0 +1,99 @@
+/**
+ * MUI Imports
+ */
+
+import { Box, Typography, Grid, Container } from '@mui/material';
+import TryFreeButton from 'components/cta/TryFreeButton';
+import DemoCta from 'components/cta/DemoCta';
+import MuiMarkdown from 'mui-markdown';
+import ZestyImage from 'blocks/Image/ZestyImage';
+
+const Hero = ({ content, FillerContent, theme, isMedium, isSmall }) => {
+  return (
+    <>
+      <Box
+        component="section"
+        sx={{
+          py: 10,
+          minHeight: 700,
+          background: `url(${content.hero_background?.data[0].url}?width=1920&height=859)`,
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Container>
+          <Grid container spacing={2}>
+            <Grid
+              sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+              item
+              sm={12}
+              md={6}
+            >
+              <Box>
+                <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
+                  <Typography
+                    variant="h3"
+                    component="h1"
+                    sx={{
+                      fontWeight: 'bold',
+                      color: theme.palette.common.white,
+                      textAlign: isMedium ? 'center' : 'left',
+                    }}
+                  >
+                    {content.hero_title || FillerContent.description}
+                  </Typography>
+                  <Typography
+                    variant="h6"
+                    component="p"
+                    sx={{
+                      fontWeight: 500,
+                      lineHeight: 1.2,
+                      color: theme.palette.common.white,
+                      textAlign: isMedium ? 'center' : 'left',
+                    }}
+                  >
+                    {content.hero_description || FillerContent.description}
+                  </Typography>
+                </Box>
+
+                <Box sx={{ mt: 2 }}>
+                  <DemoCta
+                    text={content.hero_cta || FillerContent.cta}
+                    variant={'contained'}
+                    fullWidth={isMedium}
+                    sx={{
+                      background: theme.palette.common.white,
+                      color: theme.palette.zesty.zestyOrange,
+                      '&:hover': {
+                        background: theme.palette.zesty.zestyOrange,
+                        color: theme.palette.common.white,
+                      },
+                    }}
+                  />
+                </Box>
+              </Box>
+            </Grid>
+            <Grid item sm={12} md={6}>
+              <ZestyImage
+                width={794}
+                height={508}
+                style={{ width: '100%', height: 'auto' }}
+                src={content.hero_graphic?.data[0].url}
+              />
+            </Grid>
+          </Grid>
+        </Container>
+      </Box>
+    </>
+  );
+};
+
+export default Hero;
--- src/views/zesty/DxpFeature.js
+++ src/views/zesty/DxpFeature.js
@@ -40,28 +40,41 @@
  * Images API: https://zesty.org/services/media-storage-micro-dam/on-the-fly-media-optimization-and-dynamic-image-manipulation
  */
 
+/**
+ * MUI Imports
+ */
+import { useTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
+
+// Helpers Imports
+import FillerContent from 'components/globals/FillerContent';
+import Hero from 'components/marketing/DxpFeatures/Hero';
+
 import React from 'react';
 
 function DxpFeature({ content }) {
+  const theme = useTheme();
+  const isSmall = useMediaQuery(theme.breakpoints.down('sx'));
+  const isMedium = useMediaQuery(theme.breakpoints.down('md'));
+  const isLarge = useMediaQuery(theme.breakpoints.down('lg'));
+  const isExtraLarge = useMediaQuery(theme.breakpoints.down('xl'));
+  const isDarkMode = theme.palette.mode === 'dark';
+
+  const pageData = {
+    theme,
+    isSmall,
+    isMedium,
+    isLarge,
+    isExtraLarge,
+    isDarkMode,
+    content,
+    FillerContent,
+  };
+
+  console.log(content);
   return (
     <>
-      {/* Zesty.io Output Example and accessible JSON object for this component. Delete or comment out when needed.  */}
-      <h1
-        dangerouslySetInnerHTML={{ __html: content.meta.web.seo_meta_title }}
-      ></h1>
-      <div>{content.meta.web.seo_meta_description}</div>
-      <div
-        style={{
-          background: '#eee',
-          border: '1px #000 solid',
-          margin: '10px',
-          padding: '20px',
-        }}
-      >
-        <h2>Accessible Zesty.io JSON Object</h2>
-        <pre>{JSON.stringify(content, null, 2)}</pre>
-      </div>
-      {/* End of Zesty.io output example */}
+      <Hero {...pageData} />
     </>
   );
 }
