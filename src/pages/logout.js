c9ebad88f8b4df4e29d64dca0ab739c4234d4d2a
glespinosa <espinosagian1995@gmail.com>
2022-09-07 20:31:03 +0800

    feat(logout): logout
    logout

---
+++ src/pages/logout.js
@@ -0,0 +1,49 @@
+import {
+  Box,
+  Container,
+  LinearProgress,
+  Stack,
+  Typography,
+} from '@mui/material';
+import React, { useEffect } from 'react';
+import { useZestyStore } from 'store';
+
+const logout = () => {
+  const { ZestyAPI } = useZestyStore((state) => state);
+
+  useEffect(() => {
+    const logout = async () => {
+      await ZestyAPI.logout();
+      window.location.replace('/login/');
+    };
+
+    logout();
+  }, []);
+  return (
+    <Container
+      maxWidth={false}
+      disableGutters
+      sx={(theme) => ({
+        maxWidth: theme.breakpoints.values.xl2,
+        height: '100vh',
+      })}
+    >
+      <Box
+        display="grid"
+        justifyContent="center"
+        alignItems="center"
+        height="100%"
+        px={5}
+      >
+        <Stack>
+          <Typography variant="h3" mb={3} color="secondary">
+            Signing Out of Zesty.io
+          </Typography>
+          <LinearProgress color="secondary" />
+        </Stack>
+      </Box>
+    </Container>
+  );
+};
+
+export default logout;
