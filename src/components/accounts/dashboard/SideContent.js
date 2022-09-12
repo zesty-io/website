f67450187d87bcfcfef84ae9d3dffa9845d7fdf1
glespinosa <espinosagian1995@gmail.com>
2022-09-12 20:26:47 +0800

    feat(main,sidebar content): main,sidebar content, components, refactoring
    Main,sidebar content, components, refactoring

--- src/components/accounts/dashboard/SideContent.js
+++ src/components/accounts/dashboard/SideContent.js
@@ -1,52 +1,52 @@
 import {
   Divider,
   Link,
-  List,
-  ListItem,
   Skeleton,
   Stack,
   TextField,
   Typography,
 } from '@mui/material';
 import React from 'react';
+import SideListItems from './ui/SideListItem';
 
 const repositories = [
   {
     logo: 'https://avatars.githubusercontent.com/u/8280627?s=16&v=4',
     name: 'zesty-io/website',
-    link: '',
+    link: '/',
   },
   {
     logo: 'https://avatars.githubusercontent.com/u/8280627?s=16&v=4',
     name: 'zesty-io/fetch-wrapper',
-    link: '',
+    link: '/',
   },
   {
     logo: 'https://avatars.githubusercontent.com/u/8280627?s=16&v=4',
     name: 'zesty-io/manager-ui',
-    link: '',
+    link: '/',
   },
   {
     logo: 'https://avatars.githubusercontent.com/u/8280627?s=16&v=4',
     name: 'zesty-io/app-layouts',
-    link: '',
+    link: '/',
   },
   {
     logo: 'https://avatars.githubusercontent.com/u/8280627?s=16&v=4',
     name: 'zesty-io/react-autolayout',
-    link: '',
+    link: '/',
   },
 ];
 
 const SideContent = ({ firstName }) => {
   return (
-    <Stack p={3} pl={1} pr={3}>
+    <Stack p={3} pl={1} pr={{ xs: 0, md: 3 }}>
       <Stack>
         <Typography color="text.secondary" fontWeight="bold">
           {firstName ? firstName : <Skeleton />}
         </Typography>
+        <Divider sx={{ my: 2 }} />
       </Stack>
-      <Divider sx={{ my: 2 }} />
+
       <SideListItems
         label="Recent Repositories"
         lists={repositories}
@@ -64,9 +64,7 @@ const SideContent = ({ firstName }) => {
         />
       </SideListItems>
 
-      <Divider sx={{ my: 2 }} />
       <SideListItems label="Recent Activity" lists={repositories} />
-      <Divider sx={{ my: 2 }} />
       <SideListItems label="Your Teams" lists={repositories}>
         <TextField
           size="small"
@@ -80,41 +78,3 @@ const SideContent = ({ firstName }) => {
 };
 
 export default SideContent;
-
-const SideListItems = ({ label, lists, children, bottomAction }) => {
-  return (
-    <Stack>
-      <Typography fontWeight="bold" color="text.secondary" mb={2}>
-        {label}
-      </Typography>
-      {children}
-      <List disablePadding>
-        {lists.map((list, index) => (
-          <ListItem
-            alignItems="center"
-            key={index}
-            sx={{ pb: 1 }}
-            disablePadding
-          >
-            <img src={list.logo} />
-            <Link
-              href={list.link}
-              underline="none"
-              color="text.primary"
-              sx={{
-                cursor: 'pointer',
-                '&:hover': {
-                  textDecoration: 'underline',
-                },
-              }}
-              ml={1}
-            >
-              {list.name}
-            </Link>
-          </ListItem>
-        ))}
-      </List>
-      {bottomAction}
-    </Stack>
-  );
-};
