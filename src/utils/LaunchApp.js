ab01cbee1f75b345a6a52d1725bf58171e326c14
darwin808 <darwin.apolinario12@gmail.com>
2022-09-26 23:02:28 +0800

    feat(settings): added export download

---
+++ src/utils/LaunchApp.js
@@ -0,0 +1,59 @@
+import axios from 'axios';
+import { ErrorMsg } from 'components/accounts';
+import FileSaver from 'file-saver';
+import { isProd } from 'utils';
+
+const baseUrl = `https://installer-m3rbwjxm5q-uc.a.run.app`;
+
+export const downloadTemplate = async (instance_zuid, token) => {
+  const headers = {
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${token}`,
+  };
+
+  const body = {
+    zuid: instance_zuid,
+    token,
+  };
+  const url = `${baseUrl}/download`;
+
+  try {
+    await axios
+      .post(url, body, {
+        responseType: 'arraybuffer',
+        headers,
+      })
+      .then((response) => {
+        const blob = new Blob([response.data], { type: 'application/zip' });
+        FileSaver.saveAs(blob, `${instance_zuid}`);
+      })
+      .catch((error) => {
+        console.log(error);
+        ErrorMsg({
+          title: error.message,
+          text: error?.response.data?.message,
+        });
+      });
+  } catch (error) {
+    ErrorMsg({
+      title: error.message,
+      text: error?.response.data?.message,
+    });
+  }
+};
+
+export const getTemplate = async (zuid, setData) => {
+  const urlStage = `https://39ntbr6g-dev.webengine.zesty.io/data/entity.json?zuid=${zuid}`;
+  const urlProd = `https://extensions.zesty.io/data/entity.json?zuid=${zuid}`;
+  const url = isProd ? urlProd : urlStage;
+  await axios
+    .get(url)
+    .then((response) => {
+      setData(response.data[0]);
+      return response;
+    })
+    .catch((error) => {
+      console.log(error);
+      return error;
+    });
+};
