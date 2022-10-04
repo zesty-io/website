ce3b053d57dab142cce2fe2d85342be87c78e25b
darwin808 <darwin.apolinario12@gmail.com>
2022-10-04 21:39:33 +0800

    feat(teams): added team members

--- src/views/accounts/instances/Teams.js
+++ src/views/accounts/instances/Teams.js
@@ -15,10 +15,12 @@ import { useFormik } from 'formik';
 import React from 'react';
 import Swal from 'sweetalert2';
 import withReactContent from 'sweetalert2-react-content';
+import { hashMD5 } from 'utils/Md5Hash';
 
 const MySwal = withReactContent(Swal);
 
 const CustomTable = ({
+  viewTeamMemberModals,
   loading,
   data,
   handleDeleteTeamModal,
@@ -26,6 +28,7 @@ const CustomTable = ({
   instanceUserWithRoles,
   cta,
 }) => {
+  console.log(instanceUserWithRoles, '99999');
   const ROWS = data?.map((e) => {
     return {
       ...e,
@@ -73,6 +76,10 @@ const CustomTable = ({
       renderCell: (params) => {
         const data = params.row;
         const action = [
+          {
+            title: 'View Team Members',
+            action: () => viewTeamMemberModals(data),
+          },
           {
             title: 'Delete Team',
             action: () => handleDeleteTeamModal({ ZUID: data.ZUID }),
@@ -192,10 +199,88 @@ const Main = ({
     });
   };
 
+  const viewTeamMemberModals = (data) => {
+    const teamMembers = instanceUserWithRoles.filter(
+      (e) => e.teamZUID === data.ZUID,
+    );
+    const rows = teamMembers.map((e) => {
+      return { ...e, id: e.ZUID };
+    });
+
+    const COLUMNS = [
+      {
+        field: 'id',
+        headerName: 'ID',
+        hide: true,
+      },
+      {
+        field: 'name',
+        headerName: 'Name',
+        width: 270,
+        editable: false,
+        sortable: false,
+        renderHeader: () => <Typography variant="body1">Name</Typography>,
+        renderCell: (params) => {
+          const name = `${params.row.firstName} ${params.row.lastName}`;
+          const email = `${params.row.email}`;
+          const profileUrl =
+            'https://www.gravatar.com/avatar/' + hashMD5(params.row?.email);
+          return (
+            <Stack direction="row" alignItems={'center'} gap={2}>
+              <img
+                src={profileUrl}
+                alt="User"
+                height={40}
+                width={40}
+                style={{ borderRadius: '50%' }}
+              />
+              <Stack textAlign={'left'}>
+                <Typography variant="body2">{name}</Typography>
+                <Typography variant="caption" color={'GrayText'}>
+                  {email}
+                </Typography>
+              </Stack>
+            </Stack>
+          );
+        },
+      },
+
+      {
+        field: 'role',
+        headerName: 'Role',
+        width: 100,
+        editable: false,
+        sortable: false,
+        renderHeader: () => <Typography variant="body1">Role</Typography>,
+        renderCell: (params) => {
+          return (
+            <Typography variant="body2">{params.row.role.name}</Typography>
+          );
+        },
+      },
+    ];
+
+    MySwal.fire({
+      title: 'Team Members',
+      html: (
+        <Stack p={4}>
+          <AccountsTable
+            loading={false}
+            rows={rows}
+            columns={COLUMNS}
+            pageSize={100}
+            autoHeight={true}
+          />
+        </Stack>
+      ),
+      showConfirmButton: false,
+    });
+  };
   const headerProps = {
     title: 'Teams',
     description: `Manage your Teams`,
   };
+
   return (
     <Grid container>
       <AccountsHeader {...headerProps}>
@@ -206,12 +291,13 @@ const Main = ({
             onClick={handleAddTeamModal}
           >
             <AddIcon />
-            <Typography>Add Team to Instance</Typography>
+            <Typography ml={0.5}>Add Team to Instance</Typography>
           </Button>
         )}
       </AccountsHeader>
       <Grid item xs={12}>
         <CustomTable
+          viewTeamMemberModals={viewTeamMemberModals}
           instanceUserWithRoles={instanceUserWithRoles}
           loading={loading}
           data={teams}
