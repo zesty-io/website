06dfb16e76ccc3b8e1728dcf4003cd45c0aacbf7
ardeay <apuzzo@gmail.com>
2022-10-07 11:28:40 -0400

    fix(head): gTM user Auth check

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,16 +4,19 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { CacheProvider } from '@emotion/react';
 import createCache from '@emotion/cache';
 import createEmotionServer from '@emotion/server/create-instance';
+import { getCookie } from 'cookies-next';
 
 const getCache = () => {
   const cache = createCache({ key: 'css', prepend: true });
   cache.compat = true;
-
   return cache;
 };
 
 export default class MyDocument extends Document {
   render() {
+    const isAuthenticated =
+      getCookie('isAuthenticated') !== undefined ? true : false;
+
     // tag manager / google analytics tags
     let GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
 
@@ -33,39 +36,45 @@ export default class MyDocument extends Document {
             href="https://unpkg.com/aos@next/dist/aos.css"
           />
           {/* Global Site Tag (gtag.js) - Google Analytics */}
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
+          {!isAuthenticated && (
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
               (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
               })(window,document,'script','dataLayer','${GTM_ID}');
           `,
-            }}
-          />
+              }}
+            />
+          )}
         </Head>
         <body>
           <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
-          <noscript>
-            <iframe
-              src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
-              height="0"
-              width="0"
-              style={{ display: 'none', visibility: 'hidden' }}
-            ></iframe>
-          </noscript>
+          {!isAuthenticated && (
+            <noscript>
+              <iframe
+                src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+                height="0"
+                width="0"
+                style={{ display: 'none', visibility: 'hidden' }}
+              ></iframe>
+            </noscript>
+          )}
 
           {/* Zoominfo */}
-          <noscript>
-            <img
-              src="https://ws.zoominfo.com/pixel/62cc55bc7b3465008f482d68"
-              width="1"
-              height="1"
-              style={{ display: 'none' }}
-              alt="websights"
-            />
-          </noscript>
+          {!isAuthenticated && (
+            <noscript>
+              <img
+                src="https://ws.zoominfo.com/pixel/62cc55bc7b3465008f482d68"
+                width="1"
+                height="1"
+                style={{ display: 'none' }}
+                alt="websights"
+              />
+            </noscript>
+          )}
           <Main />
           <NextScript />
         </body>
