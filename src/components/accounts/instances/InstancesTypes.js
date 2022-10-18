7eae8ecf883c35ab7ba44b9b200b47332b4ea5d7
darwin808 <darwin.apolinario12@gmail.com>
2022-10-18 17:46:33 +0800

    feat(instancestypes): added infinite scroll

--- src/components/accounts/instances/InstancesTypes.js
+++ src/components/accounts/instances/InstancesTypes.js
@@ -1,6 +1,56 @@
 import { Grid, List, Stack, Typography } from '@mui/material';
-import React from 'react';
+import React, { useState } from 'react';
 import ZInstanceItem from '../dashboard/ui/ZInstanceItem';
+import InfiniteScroll from 'react-infinite-scroll-component';
+
+const InstancesGrid = ({
+  fetchData,
+  collectect,
+  renderInstances,
+  isLoading,
+}) => {
+  if (isLoading) {
+    return (
+      <Grid container spacing={2}>
+        {[...new Array(12)].map((i) => (
+          <Grid key={i} item xs={12} sm={6} md={4} lg={3} xl2={2}>
+            <ZInstanceItem isLoading={isLoading} />
+          </Grid>
+        ))}
+      </Grid>
+    );
+  }
+  return (
+    <InfiniteScroll
+      dataLength={collectect?.length}
+      next={fetchData}
+      hasMore={true}
+      style={{
+        display: 'flex',
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'start',
+        gap: '1rem',
+      }}
+    >
+      {collectect?.map((instance) => {
+        return (
+          <Stack
+            direction={'column'}
+            sx={{
+              width: '27rem',
+              display: 'flex',
+              flexWrap: 'wrap',
+            }}
+            spacing={2}
+          >
+            {renderInstances('grid', instance)}
+          </Stack>
+        );
+      })}
+    </InfiniteScroll>
+  );
+};
 
 const InstancesTypes = ({
   view,
@@ -10,6 +60,26 @@ const InstancesTypes = ({
   isLoading,
   renderInstances,
 }) => {
+  const [count, setcount] = useState(8);
+  const [collectect, setcollectect] = useState([]);
+
+  const fetchData = () => {
+    setcount(count + 2);
+  };
+
+  React.useEffect(() => {
+    setcollectect(
+      lists
+        ?.sort(function compare(a, b) {
+          const dateA = new Date(a.createdAt);
+          const dateB = new Date(b.createdAt);
+          const sortedByCreatedAt = dateB - dateA;
+          return sortedByCreatedAt;
+        })
+        .slice(0, count),
+    );
+  }, [count, lists]);
+
   if (isLoading) {
     return (
       <Grid container spacing={2}>
@@ -37,28 +107,21 @@ const InstancesTypes = ({
           {title}
         </Typography>
 
-        {view === 'list' ? (
+        {view === 'grid' && (
+          <InstancesGrid
+            collectect={collectect}
+            fetchData={fetchData}
+            renderInstances={renderInstances}
+            isLoading={isLoading}
+          />
+        )}
+
+        {view === 'list' && (
           <List>
             {lists?.map((instance) => {
               return renderInstances('list', instance);
             })}
           </List>
-        ) : (
-          <Grid container spacing={2}>
-            {isLoading
-              ? [...new Array(12)].map((i) => (
-                  <Grid key={i} item xs={12} sm={6} md={4} lg={3} xl2={2}>
-                    <ZInstanceItem isLoading={isLoading} />
-                  </Grid>
-                ))
-              : lists?.map((instance, index) => {
-                  return (
-                    <Grid key={index} item xs={12} sm={6} md={4} lg={3} xl2={2}>
-                      {renderInstances('grid', instance)}
-                    </Grid>
-                  );
-                })}
-          </Grid>
         )}
       </Stack>
     )
