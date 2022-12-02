324479fd28f00b833ca01551504776d69dd0e328
Allen Pigar <lleannenvm@gmail.com>
2022-12-02 18:48:04 +0800

    Fix marketplace module installation

--- src/components/marketplace/ExtensionsIntaller.js
+++ src/components/marketplace/ExtensionsIntaller.js
@@ -1,41 +1,62 @@
-import { Button } from '@mui/material';
 import { getCookie } from 'cookies-next';
+import { LoadingButton } from '@mui/lab';
 import React from 'react';
 
 const ExtensionsIntaller = ({
-  instance,
   extensionName,
-  githubUrl,
-  ...props
+  githubUrl
 }) => {
+  const [loading, setLoading] = React.useState(false);
+  const [finishInstall, setFinishInstall] = React.useState(false);
+  const instanceZUID = getCookie('ZESTY_WORKING_INSTANCE');
   const install = async () => {
+    setLoading(true);
     const requestOptions = {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
+        Authorization: `Bearer ${getCookie('APP_SID')}`,
       },
       body: JSON.stringify({
-        extentionName: extensionName,
-        instanceZUID: instance?.ZUID,
-        authToken: getCookie('APP_SID'),
-        githubUrl: githubUrl,
+        instance_zuid: instanceZUID,
+        repository: githubUrl,
+        // extentionName: extensionName,
+        // instanceZUID: instance?.ZUID,
+        // authToken: getCookie('APP_SID'),
+        // githubUrl: githubUrl,
       }),
     };
     try {
       let response = await fetch(
-        'https://us-central1-zesty-dev.cloudfunctions.net/installExtension',
+        // 'https://us-central1-zesty-dev.cloudfunctions.net/installExtension',
+        'https://installer-xytrmaqk4a-uc.a.run.app/install/model',
         requestOptions,
       );
       await response.json();
+      setLoading(false);
+      setFinishInstall(true);
     } catch (error) {
+      setLoading(false);
       console.error('There was an error!', error);
     }
   };
 
   return (
-    <Button {...props} onClick={() => install()}>
-      Install {extensionName}
-    </Button>
+    <LoadingButton
+      variant="contained"
+      color="secondary"
+      sx={{ mt: 2 }}
+      fullWidth
+      onClick={install}
+      disabled={finishInstall}
+      loading={loading}
+    >
+      {loading && !finishInstall
+        ? `Installing ${extensionName}`
+        : !loading && finishInstall
+        ? `${extensionName} has been Installed`
+        : `Install ${extensionName}`}
+    </LoadingButton>
   );
 };
 
