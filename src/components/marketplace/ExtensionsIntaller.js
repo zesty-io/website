57d03b928fe6a0776d5dad6995a0bd1481a9f26c
Allen Pigar <lleannenvm@gmail.com>
2022-12-05 22:12:33 +0800

    feat: add selection of parent model on module installation

--- src/components/marketplace/ExtensionsIntaller.js
+++ src/components/marketplace/ExtensionsIntaller.js
@@ -1,43 +1,93 @@
 import { getCookie } from 'cookies-next';
 import { LoadingButton } from '@mui/lab';
+import { useZestyStore } from 'store';
 import React from 'react';
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
+const CustomSwal = withReactContent(Swal);
 
-const ExtensionsIntaller = ({
-  extensionName,
-  githubUrl
-}) => {
+const ExtensionsIntaller = ({ extensionName, githubUrl }) => {
   const [loading, setLoading] = React.useState(false);
   const [finishInstall, setFinishInstall] = React.useState(false);
   const instanceZUID = getCookie('ZESTY_WORKING_INSTANCE');
-  const install = async () => {
-    setLoading(true);
-    const requestOptions = {
+  let ZestyAPI = useZestyStore((state) => state.ZestyAPI);
+  const url = 'https://installer-xytrmaqk4a-uc.a.run.app/';
+
+  const getModuleSchema = async () => {
+    const response = await fetch(`${url}/schema`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        Authorization: `Bearer ${getCookie('APP_SID')}`,
       },
-      body: JSON.stringify({
-        instance_zuid: instanceZUID,
-        repository: githubUrl,
-        // extentionName: extensionName,
-        // instanceZUID: instance?.ZUID,
-        // authToken: getCookie('APP_SID'),
-        // githubUrl: githubUrl,
-      }),
-    };
+      body: JSON.stringify({ repository: githubUrl }),
+    });
+    const result = await response.json();
+    if (!response.ok) throw new Error(result.message);
+    return result.map((item) => item.name);
+  };
+
+  const getInstanceSchema = async () => {
+    const res = await ZestyAPI.getModels();
+    var obj = { 0: 'Root' };
+    res.data.forEach((item) => {
+      obj[item.ZUID] = item.label;
+    });
+    return obj;
+  };
+
+  const selectParent = async () => {
+    const parent = await getModuleSchema();
+    const instanceModels = await getInstanceSchema();
+    return CustomSwal.fire({
+      title: `Are you sure you want to install ${extensionName} module?`,
+      text: `This will install ${parent.join(', ')} content models.`,
+      input: 'select',
+      inputOptions: instanceModels,
+      inputPlaceholder: 'Select a parent.',
+      showCancelButton: true,
+      confirmButtonText: 'Install',
+      inputValidator: (value) => {
+        return new Promise((resolve) => {
+          if (value) {
+            resolve();
+          } else {
+            resolve('You need to select parent model.');
+          }
+        });
+      },
+    });
+  };
+  const install = async () => {
     try {
-      let response = await fetch(
-        // 'https://us-central1-zesty-dev.cloudfunctions.net/installExtension',
-        'https://installer-xytrmaqk4a-uc.a.run.app/install/model',
-        requestOptions,
-      );
-      await response.json();
-      setLoading(false);
-      setFinishInstall(true);
+      const { value: zuid } = await selectParent();
+      if (zuid) {
+        setLoading(true);
+        var requestOptions = {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${getCookie('APP_SID')}`,
+          },
+          body: JSON.stringify({
+            instance_zuid: instanceZUID,
+            repository: githubUrl,
+            parent_zuid: zuid,
+          }),
+        };
+        let response = await fetch(`${url}/install/model`, requestOptions);
+        let result = await response.json();
+        if (!response.ok) throw new Error(result.message);
+        setLoading(false);
+        setFinishInstall(true);
+      }
     } catch (error) {
       setLoading(false);
-      console.error('There was an error!', error);
+      console.error(error);
+      CustomSwal.fire({
+        icon: 'error',
+        title: 'Something went wrong!',
+        text: 'Installation is cancelled.',
+      });
     }
   };
 
