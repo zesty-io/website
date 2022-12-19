9b0076cf3108690cddc12bb9bc4a13fb8253ccd8
darwin808 <darwin.apolinario12@gmail.com>
2022-12-20 00:38:33 +0800

    feat(join app): added google and ms login

--- src/components/marketing/Join/SlideMessage.jsx
+++ src/components/marketing/Join/SlideMessage.jsx
@@ -13,6 +13,11 @@ import {
 } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
 import { useFormik } from 'formik';
+import msLogo from '../../../../public/assets/images/microsoft/microsoft_logo.svg';
+const googleLogo =
+  'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTgiIGhlaWdodD0iMTgiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PGcgZmlsbD0ibm9uZSIgZmlsbC1ydWxlPSJldmVub2RkIj48cGF0aCBkPSJNMTcuNiA5LjJsLS4xLTEuOEg5djMuNGg0LjhDMTMuNiAxMiAxMyAxMyAxMiAxMy42djIuMmgzYTguOCA4LjggMCAwIDAgMi42LTYuNnoiIGZpbGw9IiM0Mjg1RjQiIGZpbGwtcnVsZT0ibm9uemVybyIvPjxwYXRoIGQ9Ik05IDE4YzIuNCAwIDQuNS0uOCA2LTIuMmwtMy0yLjJhNS40IDUuNCAwIDAgMS04LTIuOUgxVjEzYTkgOSAwIDAgMCA4IDV6IiBmaWxsPSIjMzRBODUzIiBmaWxsLXJ1bGU9Im5vbnplcm8iLz48cGF0aCBkPSJNNCAxMC43YTUuNCA1LjQgMCAwIDEgMC0zLjRWNUgxYTkgOSAwIDAgMCAwIDhsMy0yLjN6IiBmaWxsPSIjRkJCQzA1IiBmaWxsLXJ1bGU9Im5vbnplcm8iLz48cGF0aCBkPSJNOSAzLjZjMS4zIDAgMi41LjQgMy40IDEuM0wxNSAyLjNBOSA5IDAgMCAwIDEgNWwzIDIuNGE1LjQgNS40IDAgMCAxIDUtMy43eiIgZmlsbD0iI0VBNDMzNSIgZmlsbC1ydWxlPSJub256ZXJvIi8+PHBhdGggZD0iTTAgMGgxOHYxOEgweiIvPjwvZz48L3N2Zz4=';
+
+const googleUrl = 'https://auth.api.zesty.io/google/login';
 
 import SimpleCardLogo from 'blocks/zesty/LogoGrid/SimpleCardLogo';
 // import DarkBlueCta from 'blocks/zesty/Cta/DarkBlueCta';
@@ -29,6 +34,8 @@ import { useZestyStore } from 'store';
 import { setCookie } from 'cookies-next';
 import { isProd } from 'utils';
 import axios from 'axios';
+import { grey } from '@mui/material/colors';
+import { useRouter } from 'next/router';
 
 export const SlideMessage = ({
   message = 'What team are you from?',
@@ -139,6 +146,26 @@ export const SlideMessage = ({
               </Box>
             </Grid>
             <Grid item lg={6} md={6} xs={12}>
+              <Stack
+                direction={{ xs: 'column', xl: 'row' }}
+                alignItems={'center'}
+                justifyContent="space-evenly"
+                gap={2}
+              >
+                <Stack mx={0}>
+                  <LinkComponent
+                    image={msLogo.src}
+                    title="Sign in with Microsoft"
+                    href={`https://auth.api.zesty.io/azure/login`}
+                  />
+                </Stack>
+
+                <LinkComponent
+                  image={googleLogo}
+                  title="Sign in with Google"
+                  href={googleUrl}
+                />
+              </Stack>
               <CustomForm onSubmit={handleSignUp} />
             </Grid>
           </Grid>
@@ -273,5 +300,47 @@ const CustomForm = ({ onSubmit = () => {} }) => {
         </Stack>
       </Stack>
     </form>
+  );
+};
+
+const LinkComponent = ({
+  image = googleLogo,
+  title = 'Sign in with Google',
+  href,
+}) => {
+  const router = useRouter();
+  const handleClick = () => {
+    router.push(href);
+  };
+  return (
+    <Stack
+      onClick={handleClick}
+      direction={'row'}
+      gap={1}
+      alignItems="center"
+      sx={{
+        border: `1px solid ${grey[500]}`,
+        cursor: 'pointer',
+        background: '#fff',
+      }}
+      py={1}
+      px={2}
+    >
+      <Stack>
+        <img src={image} alt={title} height={'20px'} width="20px" />
+      </Stack>
+      <Stack
+        sx={{
+          width: '10rem',
+          '&:hover': {
+            opacity: 0.7,
+          },
+        }}
+      >
+        <Typography fontWeight={'500'} color={grey[700]} whiteSpace={'nowrap'}>
+          {title}
+        </Typography>
+      </Stack>
+    </Stack>
   );
 };
