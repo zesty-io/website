6a0914e443f08082cf0b36d93a373b188ad62fde
darwin808 <darwin.apolinario12@gmail.com>
2022-12-20 15:35:09 +0800

    feat(joinapp): change check for onboarding

--- src/components/accounts/join/ToolBox.js
+++ src/components/accounts/join/ToolBox.js
@@ -3,29 +3,39 @@ import { Stack } from '@mui/material';
 import React from 'react';
 import { useZestyStore } from 'store';
 
+const emptyPrefs = '';
+const oldUserPrefs = {
+  favorite_sites: [
+    '8-f6c2b69cd2-x5lt9w',
+    '8-ae8d89b2ce-m5m1zd',
+    '8-debbdf9bba-rk2k2j',
+    '8-9c8397d0ab-q5hbdq',
+    '8-90a1e480d4-d0ng4z',
+  ],
+  instance_layout: 'grid',
+  teamOptions: '1',
+};
 export const ToolBox = ({ title = '', role = '', userType = '' }) => {
   const [loading, setloading] = React.useState(false);
   const { userInfo, ZestyAPI } = useZestyStore();
 
   //* for testing only
-  const clearPrefs = async () => {
+  const updatePrefs = async (data) => {
     setloading(true);
     const userZUID = userInfo.ZUID;
     const res = await ZestyAPI.getUser(userZUID);
     if (!res.error) {
-      const { firstName, lastName, prefs } = res?.data || {};
-      const newPrefs = prefs && JSON.parse(prefs);
+      const { firstName, lastName } = res?.data || {};
       const body = {
         firstName,
         lastName,
-        prefs: '',
+        prefs: JSON.stringify(data),
       };
       await ZestyAPI.updateUser(userZUID, body);
       setTimeout(() => {
         window.location.reload();
         setloading(false);
       }, 2000);
-      console.log(newPrefs);
     }
   };
 
@@ -39,10 +49,24 @@ export const ToolBox = ({ title = '', role = '', userType = '' }) => {
         right: 0,
         border: '1px solid #333333',
         zIndex: 9999999999999999,
+        gap: 1,
       }}
     >
-      <LoadingButton loading={loading} variant="contained" onClick={clearPrefs}>
+      <LoadingButton
+        loading={loading}
+        variant="contained"
+        onClick={() => updatePrefs(emptyPrefs)}
+        data-testid="clearPrefs"
+      >
         Clear User Prefs
+      </LoadingButton>
+      <LoadingButton
+        loading={loading}
+        variant="contained"
+        data-testid="oldUserPrefs"
+        onClick={() => updatePrefs(oldUserPrefs)}
+      >
+        Existing user test
       </LoadingButton>
       <h3>{title}</h3>
       <h6>role: {JSON.stringify(role)}</h6>
