83dc976ee1c3975c7d8fdd56e1576f4321aed23f
darwin808 <darwin.apolinario12@gmail.com>
2022-12-21 01:53:43 +0800

    fix(_app): fix gtm css issue

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -13,7 +13,6 @@ import '../../public/styles/custom.css';
 import { SnackbarProvider } from 'notistack';
 import Head from 'next/head';
 import AuthProvider from 'components/context/AuthProvider';
-import Script from 'next/script';
 
 if (process.env.NODE_ENV === 'production') {
   console.log = () => {};
@@ -59,17 +58,19 @@ export default function App({ Component, pageProps }) {
     <AuthProvider value={pageProps?.zesty}>
       {pageProps?.meta?.web && <ZestyHead content={pageProps} />}
       <Head>
-        <Script
-          dangerouslySetInnerHTML={{
-            __html: `
+        {GTM_ID && (
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
               (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
               })(window,document,'script','dataLayer','${GTM_ID}');
           `,
-          }}
-        />
+            }}
+          />
+        )}
       </Head>
       <SnackbarProvider autoHideDuration={2500} preventDuplicate maxSnack={3}>
         <Page>
