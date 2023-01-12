5cc1f7def1e630e4107f86a440a59c737d7df255
Allen Pigar <lleannenvm@gmail.com>
2023-01-12 21:33:55 +0800

    fix: update experience when installing module if not logged in

--- src/components/marketplace/ExtensionsIntaller.js
+++ src/components/marketplace/ExtensionsIntaller.js
@@ -1,17 +1,37 @@
-import { getCookie } from 'cookies-next';
+import { getCookie, setCookie } from 'cookies-next';
 import { LoadingButton } from '@mui/lab';
 import { useZestyStore } from 'store';
 import React from 'react';
 import Swal from 'sweetalert2';
 import withReactContent from 'sweetalert2-react-content';
+import { isProd } from 'utils';
 const CustomSwal = withReactContent(Swal);
+const InstanceSwal = withReactContent(Swal);
 
 const ExtensionsIntaller = ({ extensionName, githubUrl }) => {
   const [loading, setLoading] = React.useState(false);
   const [finishInstall, setFinishInstall] = React.useState(false);
+  const { setworkingInstance } = useZestyStore((state) => state);
+  const [instances, setinstances] = React.useState([]);
   const instanceZUID = getCookie('ZESTY_WORKING_INSTANCE');
+  const instanceName = instances?.data?.find(
+    (e) => e.ZUID === instanceZUID,
+  )?.name;
   let ZestyAPI = useZestyStore((state) => state.ZestyAPI);
-  const url = 'https://installer-xytrmaqk4a-uc.a.run.app';
+  const url = isProd
+    ? 'https://installer-xytrmaqk4a-uc.a.run.app'
+    : 'https://installer-m3rbwjxm5q-uc.a.run.app';
+  const appSID = isProd ? getCookie('APP_SID') : getCookie('DEV_APP_SID');
+
+  const getInstanceRaw = async () => {
+    const res = await ZestyAPI.getInstances();
+    !res.error && setinstances(res);
+    res.error && setinstances([]);
+  };
+
+  React.useState(() => {
+    getInstanceRaw();
+  }, []);
 
   const getModuleSchema = async () => {
     const response = await fetch(`${url}/schema`, {
@@ -29,8 +49,16 @@ const ExtensionsIntaller = ({ extensionName, githubUrl }) => {
   const getInstanceSchema = async () => {
     const res = await ZestyAPI.getModels();
     var obj = { 0: 'Root' };
-    res.data.forEach((item) => {
-      obj[item.ZUID] = item.label;
+    !res.error &&
+      res.data.forEach((item) => {
+        obj[item.ZUID] = item.label;
+      });
+    return obj;
+  };
+  const getInstances = async () => {
+    var obj = {};
+    instances.data.forEach((instance) => {
+      obj[instance.ZUID] = instance.name;
     });
     return obj;
   };
@@ -57,8 +85,40 @@ const ExtensionsIntaller = ({ extensionName, githubUrl }) => {
       },
     });
   };
+  const selectInstance = async () => {
+    const instances = await getInstances();
+    return InstanceSwal.fire({
+      title: `Select your instance.`,
+      input: 'select',
+      inputOptions: instances,
+      showCancelButton: true,
+      confirmButtonText: 'Select',
+      inputValidator: (value) => {
+        return new Promise((resolve) => {
+          if (value) {
+            resolve();
+          } else {
+            resolve('You need to select instance');
+          }
+        });
+      },
+    });
+  };
   const install = async () => {
     try {
+      if (!appSID) {
+        window.location.href = '/login/';
+        return;
+      }
+      if (!instanceZUID) {
+        const { value: zuid } = await selectInstance();
+        if (zuid) {
+          setCookie('ZESTY_WORKING_INSTANCE', zuid);
+          setworkingInstance(zuid);
+          window.location.reload();
+        }
+        return;
+      }
       const { value: zuid } = await selectParent();
       if (zuid) {
         setLoading(true);
@@ -66,7 +126,7 @@ const ExtensionsIntaller = ({ extensionName, githubUrl }) => {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
-            Authorization: `Bearer ${getCookie('APP_SID')}`,
+            Authorization: `Bearer ${appSID}`,
           },
           body: JSON.stringify({
             instance_zuid: instanceZUID,
@@ -79,6 +139,7 @@ const ExtensionsIntaller = ({ extensionName, githubUrl }) => {
         if (!response.ok) throw new Error(result.message);
         setLoading(false);
         setFinishInstall(true);
+        setCookie('ZESTY_WORKING_INSTANCE', null);
       }
     } catch (error) {
       setLoading(false);
@@ -101,11 +162,15 @@ const ExtensionsIntaller = ({ extensionName, githubUrl }) => {
       disabled={finishInstall}
       loading={loading}
     >
-      {loading && !finishInstall
+      {!appSID
+        ? 'Please Login To Continue'
+        : !instanceZUID
+        ? `Select Instance to continue`
+        : loading && !finishInstall
         ? `Installing ${extensionName}`
         : !loading && finishInstall
         ? `${extensionName} has been Installed`
-        : `Install ${extensionName}`}
+        : `Install ${extensionName} to ${instanceName}`}
     </LoadingButton>
   );
 };
