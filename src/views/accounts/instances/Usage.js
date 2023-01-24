5bf11c23f33145b0ec2897bff4d0727b75baf4fa
darwin808 <darwin.apolinario12@gmail.com>
2023-01-24 11:40:49 +0800

    style(usage): added table accounts for data

--- src/views/accounts/instances/Usage.js
+++ src/views/accounts/instances/Usage.js
@@ -9,7 +9,12 @@ import {
   Typography,
 } from '@mui/material';
 import React from 'react';
-import { AccountsHeader } from 'components/accounts';
+import { v4 as uuidv4 } from 'uuid';
+import {
+  AccountsHeader,
+  AccountsTable,
+  AccountsTableHead,
+} from 'components/accounts';
 import dayjs from 'dayjs';
 import LaunchIcon from '@mui/icons-material/Launch';
 import { grey } from '@mui/material/colors';
@@ -47,69 +52,13 @@ const getMonthDates = ({ start = '', end = '' }) => {
       const res = {
         start: e,
         end: endDateList[i],
+        id: uuidv4(),
       };
       return res;
     })
     .reverse();
 
   return dateList;
-};
-
-const MetricReports = ({ dateList = [], zuid, loading }) => {
-  return (
-    <Stack width={1} px={4}>
-      <Stack width={'11rem'}>
-        <Button variant="outlined" color="primary" size="large">
-          Metric Reports
-        </Button>
-      </Stack>
-
-      <Stack px={10}>
-        {loading ? (
-          <Stack width={1} alignItems="center" py={10}>
-            <CircularProgress />
-          </Stack>
-        ) : (
-          <Card
-            sx={{
-              mt: 4,
-              borderRadius: '20px',
-            }}
-          >
-            {dateList.map((e) => {
-              const month = dayjs(e.start).format('MMMM YYYY');
-              const link = `https://reports.zesty.io/usage.html?instanceZUID=${zuid}&dateStart=${e.start}&dateEnd=${e.end}`;
-              const openUrl = (url) => {
-                window.open(url, '_blank').focus();
-              };
-              return (
-                <Stack
-                  justifyContent="space-between"
-                  alignItems={'center'}
-                  direction="row"
-                  width={1}
-                  py={2}
-                  px={14}
-                  sx={{
-                    borderBottom: `1px solid ${grey[300]}`,
-                  }}
-                >
-                  <Typography>{month}</Typography>
-                  <Button
-                    endIcon={<LaunchIcon />}
-                    onClick={() => openUrl(link)}
-                    variant="contained"
-                  >
-                    Open Report
-                  </Button>
-                </Stack>
-              );
-            })}
-          </Card>
-        )}
-      </Stack>
-    </Stack>
-  );
 };
 
 const ReportCard = ({ title = '', data = 0, type = '', limit = 0 }) => {
@@ -168,10 +117,8 @@ const ThisMonthReport = ({ usage, loading }) => {
   ];
   return (
     <Stack width={1} px={4}>
-      <Stack width={'11rem'}>
-        <Button variant="outlined" color="primary" size="large">
-          This month to date
-        </Button>
+      <Stack width={1}>
+        <Typography variant="h5">This month to date</Typography>
       </Stack>
       {loading ? (
         <Stack width={1} alignItems="center" py={10}>
@@ -197,24 +144,93 @@ const ThisMonthReport = ({ usage, loading }) => {
   );
 };
 
-export const Usage = ({ zuid, usage, loading }) => {
+const CustomTable = ({ data, loading, zuid = '' }) => {
+  const ROWS = data;
+  const COLUMNS = [
+    {
+      field: 'id',
+      headerName: 'ID',
+      hide: true,
+    },
+    {
+      field: 'start',
+      headerName: 'Date',
+      minWidth: 500,
+      flex: 1,
+      editable: false,
+      sortable: true,
+      valueGetter: (params) => params.row.start,
+      renderHeader: () => <AccountsTableHead>Date</AccountsTableHead>,
+      renderCell: (params) => {
+        const month = dayjs(params.row.start).format('MMMM YYYY');
+
+        return (
+          <Typography
+            sx={{
+              overflow: 'hidden',
+              textOverflow: 'ellipsis',
+            }}
+            title={params.row.start}
+            variant="body2"
+          >
+            {month}
+          </Typography>
+        );
+      },
+    },
+    {
+      field: 'action',
+      headerName: '',
+      width: 200,
+      editable: false,
+      sortable: false,
+      renderCell: (params) => {
+        const link = `https://reports.zesty.io/usage.html?instanceZUID=${zuid}&dateStart=${params.row.start}&dateEnd=${params.row.end}`;
+        const openUrl = (url) => {
+          window.open(url, '_blank').focus();
+        };
+
+        return (
+          <Button
+            endIcon={<LaunchIcon />}
+            onClick={() => openUrl(link)}
+            variant="contained"
+          >
+            Open Report
+          </Button>
+        );
+      },
+    },
+  ];
+
+  return (
+    <Stack p={4}>
+      <Stack width={1}>
+        <Typography variant="h5" mb={2}>
+          Metric Reports
+        </Typography>
+      </Stack>
+      <Stack px={10}>
+        <AccountsTable
+          loading={loading}
+          rows={ROWS}
+          columns={COLUMNS}
+          pageSize={100}
+          autoHeight={false}
+        />
+      </Stack>
+    </Stack>
+  );
+};
+
+const Main = ({ zuid, usage, loading }) => {
   const [currentpage, setcurrentpage] = React.useState(1);
   const postperpage = 10;
-
-  const headerProps = {
-    title: 'Usage',
-    description: `View your usage`,
-  };
 
   const dateList = getMonthDates({
     start: dayjs(usage?.Account?.CreatedAt).format('YYYY-MM-DD'),
     end: dayjs().format('YYYY-MM-DD'),
   });
-
-  const monthReports = {
-    usage,
-    loading,
-  };
 
   const indexoflast = currentpage * postperpage;
   const indexoffirst = indexoflast - postperpage;
@@ -225,21 +241,32 @@ export const Usage = ({ zuid, usage, loading }) => {
     pagenum.push(i);
   }
 
-  const metricReportsProps = {
-    dateList: posts,
-    zuid,
-    loading,
-  };
-
   const handlePageChange = (_event, value) => {
     setcurrentpage(value);
   };
 
+  const headerProps = {
+    title: 'Usage',
+    description: `View your usage`,
+  };
+  const tableProps = {
+    zuid,
+    data: posts,
+    loading,
+  };
+
+  const monthReports = {
+    usage,
+    loading,
+  };
   return (
     <Grid container>
       <AccountsHeader {...headerProps}></AccountsHeader>
       <ThisMonthReport {...monthReports} />
-      <MetricReports {...metricReportsProps} />
+
+      <Grid item xs={12}>
+        <CustomTable {...tableProps} />
+      </Grid>
 
       <Stack alignItems={'center'} width={1} py={2}>
         {!loading && dateList?.length > 10 && (
@@ -256,3 +283,5 @@ export const Usage = ({ zuid, usage, loading }) => {
     </Grid>
   );
 };
+
+export const Usage = React.memo(Main);
