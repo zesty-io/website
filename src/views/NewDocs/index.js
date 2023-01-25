d6fbd036778b869e949d6d59256727ca3bd94758
darwin808 <darwin.apolinario12@gmail.com>
2023-01-25 23:32:12 +0800

    feat(docs): initialize

--- src/views/NewDocs/index.js
+++ src/views/NewDocs/index.js
@@ -1,50 +1,91 @@
 import { TreeItem, TreeView } from '@mui/lab';
-import Main from 'layouts/Main';
+import MainWrapper from 'layouts/Main';
 import Head from 'next/head';
+import Fuse from 'fuse.js';
 import { v4 as uuidv4 } from 'uuid';
 import React from 'react';
-import INSTANCE_DATA from './instance.data.json';
-import ACCOUNTS_DATA from './accounts.data.json';
-import AUTH_DATA from './auth.data.json';
-// import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-// import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import FolderIcon from '@mui/icons-material/Folder';
 import FolderOpenIcon from '@mui/icons-material/FolderOpen';
-import { Stack, Typography } from '@mui/material';
+import { Grid, Stack, TextField, Typography } from '@mui/material';
+
+import INSTANCE_DATA from './instance.data.json';
+// import ACCOUNTS_DATA from './accounts.data.json';
+// import AUTH_DATA from './auth.data.json';
 
 const title = 'Docs page';
 const description = 'Docs page';
 const ogimage = 'Docs page';
 
+const options = {
+  includeScore: true,
+  useExtendedSearch: true,
+  includeMatches: true,
+  ignoreLocation: true,
+  findAllMatches: true,
+  threshold: 0,
+  isCaseSensitive: false,
+  minMatchCharLength: 1,
+  keys: ['name', 'item.name', 'item.item.name'],
+};
+
 const FolderTreeView = ({ data = {}, header = '', onClick = () => {} }) => {
+  const [search, setsearch] = React.useState('');
   const newData = data?.item || [];
+
   const handleClick = (item) => {
     console.log(item, 555);
     onClick(item);
   };
+
   const getTree = (data = []) => {
-    const res = data.map((e) => {
-      if (Array.isArray(e.item)) {
-        return (
-          <TreeItem nodeId={uuidv4()} label={e.name}>
-            {getTree(e.item)}
-          </TreeItem>
-        );
-      } else {
-        return (
-          <TreeItem
-            nodeId={uuidv4()}
-            label={e.name}
-            onClick={() => handleClick(e)}
-          ></TreeItem>
-        );
-      }
-    });
+    const res = Array.isArray(data)
+      ? data.map((e) => {
+          if (Array.isArray(e.item)) {
+            return (
+              <TreeItem
+                nodeId={uuidv4()}
+                label={<Typography py={1}>{e.name}</Typography>}
+              >
+                {getTree(e.item)}
+              </TreeItem>
+            );
+          } else {
+            return (
+              <TreeItem
+                nodeId={uuidv4()}
+                label={<Typography py={1}>{e.name}</Typography>}
+                onClick={() => handleClick(e)}
+                sx={{
+                  whiteSpace: 'nowrap',
+                }}
+              ></TreeItem>
+            );
+          }
+        })
+      : console.error('Not array');
 
     return res;
   };
+
+  const fuse = new Fuse(newData, options);
+  const searchResult = fuse.search(search || '');
+  const treeData =
+    searchResult.length !== 0 && search.length !== 0
+      ? searchResult[0].item.item
+      : searchResult.length === 0 && search.length === 0
+      ? newData
+      : [];
+
   return (
     <Stack>
+      <TextField
+        id="outlined-basic"
+        label="Search"
+        variant="outlined"
+        value={search}
+        onChange={(e) => setsearch(e.currentTarget.value)}
+      />
+
       <Typography variant="h5">{header}</Typography>
       <TreeView
         aria-label="file system navigator"
@@ -53,21 +94,20 @@ const FolderTreeView = ({ data = {}, header = '', onClick = () => {} }) => {
           <FolderOpenIcon color="secondary" fontSize="large" />
         }
         sx={{
-          fontSize: '40px',
           flexGrow: 1,
           maxWidth: 400,
           overflowY: 'auto',
         }}
       >
-        {getTree(newData)}
+        {getTree(treeData)}
       </TreeView>
     </Stack>
   );
 };
 
-export const NewDocs = () => {
+const Main = () => {
   return (
-    <Main customRouting={[]}>
+    <MainWrapper customRouting={[]}>
       <Head>
         <title>{title}</title>
         <meta property="og:title" content={title} />
@@ -75,9 +115,23 @@ export const NewDocs = () => {
         <meta property="og:description" content={description} />
         <meta property="og:image" content={ogimage} />
       </Head>
-      <FolderTreeView data={INSTANCE_DATA} header="Instance API" />
-      <FolderTreeView data={ACCOUNTS_DATA} header="Accounts API" />
-      <FolderTreeView data={AUTH_DATA} header="Authentication API" />
-    </Main>
+      <Grid container>
+        <Grid item xs={2}>
+          <Stack
+            sx={{
+              height: '80vh',
+              overflow: 'auto',
+            }}
+          >
+            <FolderTreeView data={INSTANCE_DATA} header="Instance API" />
+            {/* <FolderTreeView data={ACCOUNTS_DATA} header="Accounts API" />
+      <FolderTreeView data={AUTH_DATA} header="Authentication API" /> */}
+          </Stack>
+        </Grid>
+        <Grid item xs={10}></Grid>
+      </Grid>
+    </MainWrapper>
   );
 };
+
+export const NewDocs = React.memo(Main);
