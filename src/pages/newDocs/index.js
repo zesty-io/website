20db9e259e94525fbd9fb065f06b87cf017d6bb6
darwin808 <darwin.apolinario12@gmail.com>
2023-01-30 23:09:03 +0800

    feat: wip

--- src/pages/newDocs/index.js
+++ src/pages/newDocs/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { NewDocs } from 'views/NewDocs';
 
+export { default as getServerSideProps } from 'lib/accounts/protectedRouteGetServerSideProps';
+
 const NewDocsPage = () => {
   return <NewDocs />;
 };
