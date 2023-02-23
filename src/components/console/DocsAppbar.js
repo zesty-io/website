38c66b2aa2885fa7bf26dd0e8a2ff1b1a2d74cfa
darwin808 <darwin.apolinario12@gmail.com>
2023-02-23 23:15:44 +0800

    fix(nav): remove support

--- src/components/console/DocsAppbar.js
+++ src/components/console/DocsAppbar.js
@@ -13,7 +13,6 @@ import { SearchModal } from 'views/Docs/SearchModal';
 
 const tabs = [
   { label: 'API Reference', value: 'api-reference' },
-  { label: 'Support', value: 'support' },
   { label: 'Reference', value: 'reference' },
   { label: 'Resources', value: 'resources' },
 ];
