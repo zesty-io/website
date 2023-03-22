2584419c6c2bf03903be505d22c058e0adbeb4da
glespinosa <espinosagian1995@gmail.com>
2023-03-22 20:45:42 +0800

    fix(link to demo): link to demo
    link to demo

--- src/components/cta/TryFreeButton.js
+++ src/components/cta/TryFreeButton.js
@@ -23,14 +23,14 @@ export default function TryFreeButton({
   const [open, setOpen] = useState(false);
   const [anchorEl, setAnchorEl] = useState(null);
   const handleClick = () => {
-    location.href = '/join/';
+    location.href = '/demo/';
 
     // Track Button Click by pushing item to the data layer
     window.dataLayer.push({
       event: 'btn_click',
       buttonText: text,
       currentPage: window.location.href,
-      targetPage: '/join/',
+      targetPage: '/demo/',
     });
     // uncomment this to bring the dropdown back
     //setAnchorEl(event.target);
