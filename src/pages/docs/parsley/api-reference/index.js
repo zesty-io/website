3c05db161ef65b4008efbd30d998446f576e29ba
darwin808 <darwin.apolinario12@gmail.com>
2023-05-04 23:25:28 +0800

    fix(api-reference-index): hide page

--- src/pages/docs/parsley/api-reference/index.js
+++ src/pages/docs/parsley/api-reference/index.js
@@ -1,10 +1,10 @@
 import React, { useEffect } from 'react';
 export { default as getServerSideProps } from 'lib/accounts/protectedRouteGetServerSideProps';
-import MuiMarkdown from 'markdown-to-jsx';
+// import MuiMarkdown from 'markdown-to-jsx';
 import MainWrapper from 'layouts/Main';
 import { useState } from 'react';
-import { Stack } from '@mui/material';
-import { DocsSidebar } from 'components/docs/DocsSidebar';
+// import { Stack } from '@mui/material';
+// import { DocsSidebar } from 'components/docs/DocsSidebar';
 import { useZestyStore } from 'store';
 import { fetchMarkdownFile, parseMarkdownFile } from 'utils/docs';
 import { PARSLEY } from 'utils/docs/constants';
@@ -67,7 +67,7 @@ const ApiReferencePage = (props) => {
   );
 
   useEffect(() => {
-    getMd();
+    // getMd();
   }, []);
 
   useEffect(() => {
@@ -77,21 +77,19 @@ const ApiReferencePage = (props) => {
   }, []);
   return (
     <MainWrapper>
-      <Stack direction={'row'}>
-        {/* SIDEBAR */}
+      {/* <Stack direction={'row'}>
         <DocsSidebar
           search={search}
           setsearch={setsearch}
           data={newNavData}
           onClick={undefined}
         />
-        {/* MAIN PAGE */}
         <Stack pl={4} sx={{ width: 1 }}>
           <Stack width={'70vw'}>
             <MuiMarkdown overrides={muiContentOverrides}>{mdData}</MuiMarkdown>
           </Stack>
         </Stack>
-      </Stack>
+      </Stack> */}
     </MainWrapper>
   );
 };
