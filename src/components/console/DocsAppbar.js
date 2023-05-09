b00ef01fb8f17b9d56ce41d3296dd732d6abc031
darwin808 <darwin.apolinario12@gmail.com>
2023-05-09 14:26:17 +0800

    feat: update

--- src/components/console/DocsAppbar.js
+++ src/components/console/DocsAppbar.js
@@ -34,7 +34,7 @@ export const DocsAppbar = React.memo(() => {
       return tabs[0].value;
     } else if (router.asPath.includes(tabs[1].value)) {
       return tabs[1].value;
-    } else if (router.asPath.includes(tabs[1].value)) {
+    } else if (router.asPath.includes(tabs[2].value)) {
       return tabs[2].value;
     } else {
       return '';
