6595e0df4326be8551f02e35ab15c99d74174956
darwin808 <darwin.apolinario12@gmail.com>
2023-06-06 22:32:51 +0800

    fix(appbar.js): window not defined error

--- src/components/console/AppBar.js
+++ src/components/console/AppBar.js
@@ -56,10 +56,10 @@ export default function AppBar() {
   useEffect(() => {
     if (router.isReady) {
       getInstance();
-      setUrl(window.location.pathname);
+      setUrl(router.asPath);
       setLocationSearch(window.location.search);
     }
-  }, [router.isReady, url, window.location.pathname]);
+  }, [router.isReady, url, router.asPath]);
 
   useEffect(() => {
     setworkingInstance(instanceZUID);
