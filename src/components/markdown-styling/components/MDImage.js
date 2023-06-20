b7f797e8633c9af5a9796bb09d1508e36b8bc9fd
darwin808 <darwin.apolinario12@gmail.com>
2023-06-20 23:07:51 +0800

    refactor(zestymarkdownparser.js): added components

---
+++ src/components/markdown-styling/components/MDImage.js
@@ -0,0 +1,44 @@
+import { Box } from '@mui/material';
+import React from 'react';
+import ModalImage from 'react-modal-image';
+
+const MDLargeImage = ({ node }) => {
+  return (
+    <Box px={5}>
+      <ModalImage
+        small={node.properties.src}
+        large={node.properties.src}
+        alt={node.properties.alt}
+        title={node.properties.title}
+      />
+    </Box>
+  );
+};
+
+const MDSmallImage = ({ node }) => {
+  return (
+    <Box
+      ml={3}
+      mr={2}
+      maxWidth={'200px'}
+      sx={{
+        float: 'right',
+      }}
+    >
+      <ModalImage
+        small={node.properties.src}
+        large={node.properties.src}
+        alt={node.properties.alt}
+        title={node.properties.title}
+      />
+    </Box>
+  );
+};
+
+export const MDImage = ({ node }) => {
+  if (node.properties.title) {
+    return <MDLargeImage node={node} />;
+  } else {
+    return <MDSmallImage node={node} />;
+  }
+};
---
+++ src/components/markdown-styling/components/index.js
@@ -0,0 +1,6 @@
+export * from './MDH1';
+export * from './MDH2';
+export * from './MDH3';
+export * from './MDBlockquote';
+export * from './MDParagraph';
+export * from './MDImage';
