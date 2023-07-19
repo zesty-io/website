22a9a0c0aebd378d7f8c24fb8cd15f8d77016d5b
glespinosa <espinosagian1995@gmail.com>
2023-07-19 18:12:52 +0800

    fix(revert useisloggedin): revert useisloggedin

--- src/components/hooks/useIsLoggedIn.js
+++ src/components/hooks/useIsLoggedIn.js
@@ -2,6 +2,7 @@ import { useContext } from 'react';
 import { AuthContext } from '../context/AuthProvider';
 
 const useIsLoggedIn = () => {
+  // update
   const cookies = useContext(AuthContext);
   return JSON.parse(cookies?.isAuthenticated || false);
 };
