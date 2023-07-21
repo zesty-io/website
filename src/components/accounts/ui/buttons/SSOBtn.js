f88d4387872cb18802b4465bea6a197356b57512
darwin808 <darwin.apolinario12@gmail.com>
2023-07-21 22:24:49 +0800

    fix(login): login redirect from navigation

--- src/components/accounts/ui/buttons/SSOBtn.js
+++ src/components/accounts/ui/buttons/SSOBtn.js
@@ -21,12 +21,18 @@ const Index = ({
     const redirectLogic = (message) => {
       if (message.origin === 'https://auth.api.zesty.io') {
         if (message.data.source === 'zesty' && message.data.status === '200') {
+          let referrer = window.document.referrer;
+          referrer =
+            window.document.referrer === '' ||
+            window.document.referrer.includes('/logout/') ||
+            window.document.referrer.includes('/login/')
+              ? window.location.origin + '/dashboard/'
+              : window.document.referrer;
           message.source.close();
           const sessionPrevUrl = sessionStorage.getItem('prevUrl');
           const prevUrl = window.document.referrer.replace(/^.*\/\/[^\/]+/, '');
           sessionStorage.removeItem('prevUrl');
-
-          if (prevUrl === sessionPrevUrl) {
+          if (prevUrl === sessionPrevUrl || referrer.indexOf('/docs/') === 0) {
             window.location.href = `${prevUrl}`;
           } else {
             window.location.href = '/dashboard/';
