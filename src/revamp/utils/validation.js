0483a3bca9fa8f1b7b16def079d2c9f6d7ab3842
jomarmontuya <jomar.montuya@outlook.com>
2023-07-26 00:26:27 +0800

    feat(getdemosection): change form styling to use row and add terms and conditions

--- src/revamp/utils/validation.js
+++ src/revamp/utils/validation.js
@@ -46,43 +46,20 @@ export const validationSchema = yup.object({
 });
 
 export const contactPageValidation = yup.object({
-  firstName: yup.string().label('First Name').trim().optional(),
-  lastName: yup.string().label('Last Name').trim().optional(),
+  firstName: yup.string().label('First Name').trim().required(),
+  lastName: yup.string().label('Last Name').trim().required(),
   email: yup
     .string()
     .label('Email')
     .trim()
     .email('Please enter a valid email address')
-    .optional(),
-  phoneNumber: yup.string().optional(),
-  // hidden fields
-  jobTitle: yup.string().optional(),
-  businessEmail: yup
+    .required(),
+  phoneNumber: yup
     .string()
-    .required()
-
-    .test(
-      'is-valid-and-not-blocked-email',
-      'Please enter valid business email',
-      (value) => {
-        if (!value) return true; // If the email is not provided, let required validation handle it.
-
-        // Validates if value is a real email format using regex
-        const isValidEmail = validEmailRegex.test(value);
-
-        if (!isValidEmail) return false; // If email is not valid, return false to trigger the error message
-
-        // Extracting the domain from the email address
-        const emailParts = value.split('@');
-        const domain = emailParts[emailParts.length - 1];
-
-        // Checking if the domain is in the allowedDomains array
-        return !blockedDomains.includes(domain);
-      },
-    ),
-
-  linkedIn: yup.string().optional(),
-  company: yup.string().optional(),
+    .label('Phone')
+    .trim()
+    .matches(phoneRegExp, 'You must enter at least 5 digits.')
+    .required(),
 });
 
 export const shortValidationSchema = yup.object({
