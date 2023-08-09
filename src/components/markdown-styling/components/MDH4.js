031141f0beab4539675f73c2ad67747b318aec82
darwin808 <darwin.apolinario12@gmail.com>
2023-08-09 23:41:46 +0800

    fix(mdh4): size

--- src/components/markdown-styling/components/MDH4.js
+++ src/components/markdown-styling/components/MDH4.js
@@ -5,7 +5,7 @@ export const MDH4 = ({ node }) => {
   const id = transformText(node?.children[0]?.value);
   return (
     <Box data-testid="mdh3-container">
-      <Typography variant="h4" component={'h4'} id={id}>
+      <Typography variant="h6" component={'h4'} id={id}>
         {node?.children[0]?.value}
       </Typography>
     </Box>
