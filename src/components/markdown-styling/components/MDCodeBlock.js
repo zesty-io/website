5f41a71a4787709b676738c930ecb0d7f0ba563e
darwin808 <darwin.apolinario12@gmail.com>
2023-08-17 23:00:46 +0800

    fix(mdcodeblock): overflowing content

--- src/components/markdown-styling/components/MDCodeBlock.js
+++ src/components/markdown-styling/components/MDCodeBlock.js
@@ -10,17 +10,25 @@ export const MDCodeBlock = ({ node }) => {
   return (
     <Box
       data-testid="code-block-box"
-      sx={{ display: 'flex', alignItems: 'center', width: '100%' }}
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        justifyItems: 'center',
+        width: '100%',
+      }}
       padding={2}
     >
       <CodeMirror
+        width="700px"
+        key={Math.random() * 123}
         data-testid="code-mirror"
         editable={false}
         value={codeTxt}
         placeholder={'Click Run to view the response'}
         extensions={[javascript({ jsx: true }), EditorView.lineWrapping]}
         onChange={() => {}}
-        style={{ fontSize: '18px', width: '100%' }}
+        style={{ fontSize: '12px', width: '100%' }}
         theme={githubDarkInit({
           settings: {
             caret: '#ff5c0c',
