a76c08f0c0477c2967d5802e59af277e1db93323
darwin.apolinario <darwin.apolinario@gmail.com>
2023-09-06 18:54:33 +0800

    fix(mdcodeblock): not fullwidth

--- src/components/markdown-styling/components/MDCodeBlock.js
+++ src/components/markdown-styling/components/MDCodeBlock.js
@@ -2,19 +2,32 @@ import { githubDarkInit } from '@uiw/codemirror-theme-github';
 import CodeMirror from '@uiw/react-codemirror';
 import { EditorView } from '@codemirror/view';
 import { javascript } from '@codemirror/lang-javascript';
+import { v4 as uuidV4 } from 'uuid';
 
+const fontSize = EditorView.baseTheme({
+  '&': {
+    fontSize: '14px',
+  },
+});
 export const MDCodeBlock = ({ node }) => {
   const codeTxt = node?.children[0]?.children[0]?.value;
 
   return (
     <CodeMirror
+      id={uuidV4()}
+      key={uuidV4()}
       data-testid="code-mirror"
+      width="100%"
       editable={false}
       value={codeTxt}
       placeholder={'Click Run to view the response'}
-      extensions={[javascript({ jsx: true }), EditorView.lineWrapping]}
+      extensions={[
+        fontSize,
+        javascript({ jsx: true }),
+        EditorView.lineWrapping,
+      ]}
       onChange={() => {}}
-      style={{ fontSize: '18px', width: '100%' }}
+      style={{ width: '100%' }}
       theme={githubDarkInit({
         settings: {
           caret: '#ff5c0c',
