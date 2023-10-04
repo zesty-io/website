88cf829c34d35dabe1da2f9ffe8541cf8321d098
darwin.apolinario <darwin.apolinario@gmail.com>
2023-10-04 18:27:52 +0800

    feat: next script worker

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -52,7 +52,7 @@ export default class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
-          <Script strategy="beforeInteractive" src={fetchUrl} />
+          <Script strategy="beforeInteractive" defer src={fetchUrl} />
         </body>
       </Html>
     );
