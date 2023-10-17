488e9f8c88f72920860d45e148751f1a2e5e30af
darwin.apolinario <darwin.apolinario@gmail.com>
2023-10-17 23:38:03 +0800

    fix(accounts): white screen and og image

--- src/components/globals/ZestyHead.js
+++ src/components/globals/ZestyHead.js
@@ -58,6 +58,7 @@ export default function ZestyHead({ content }) {
         content={content.meta?.web.seo_meta_description}
       />
       <meta property="og:image" content={ogimage} />
+      <meta property="og:image:secure_url" content={ogimage} />
       {isPPCPage && <meta name="robots" content="noindex"></meta>}
       <link rel="canonical" href={canonicalURL} />
       {isBlog ? (
