c3db5a40a1c084995b3c7d6b956c311e01e3077a
darwin.apolinario <darwin.apolinario@gmail.com>
2023-10-25 16:05:12 +0800

    fix: clearing of expire cookie

--- src/components/hooks/useIsLoggedIn.js
+++ src/components/hooks/useIsLoggedIn.js
@@ -1,24 +1,56 @@
 import { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../context/AuthProvider';
-import { getCookie } from 'cookies-next';
+import { deleteCookie, getCookie } from 'cookies-next';
 
 const isUserAuthenticated = async () => {
-  let isProd = JSON.parse(getCookie('PRODUCTION') || true);
-
+  const isProd = getCookie('PRODUCTION') === 'true' ? true : false;
   const verifyUrl = !isProd
     ? 'https://auth.api.dev.zesty.io/verify'
     : 'https://auth.api.zesty.io/verify';
 
   const appSid = getCookie(isProd ? 'APP_SID' : 'DEV_APP_SID');
 
-  const response = await fetch(verifyUrl, {
-    headers: {
-      Authorization: `Bearer ${appSid}`,
-    },
-  });
-  const data = await response.json();
+  if (!appSid) {
+    deleteCookie(isProd ? 'APP_SID' : 'DEV_APP_SID', {
+      domain: '.zesty.io',
+      secure: true,
+    });
+    deleteCookie('isAuthenticated');
+    deleteCookie('ZESTY_WORKING_INSTANCE', {});
+    return false;
+  }
 
-  return data?.code === 200 ? true : false;
+  if (appSid) {
+    try {
+      const response = await fetch(verifyUrl, {
+        headers: {
+          Authorization: `Bearer ${appSid}`,
+        },
+      });
+      const data = await response.json();
+
+      if (data?.code === 200) {
+        return true;
+      } else {
+        deleteCookie(isProd ? 'APP_SID' : 'DEV_APP_SID', {
+          domain: '.zesty.io',
+          secure: true,
+        });
+        deleteCookie('isAuthenticated');
+        deleteCookie('ZESTY_WORKING_INSTANCE', {});
+        return false;
+      }
+    } catch (error) {
+      console.log(error, 'error');
+      deleteCookie(isProd ? 'APP_SID' : 'DEV_APP_SID', {
+        domain: '.zesty.io',
+        secure: true,
+      });
+      deleteCookie('isAuthenticated');
+      deleteCookie('ZESTY_WORKING_INSTANCE', {});
+      return false;
+    }
+  }
 };
 
 const useIsLoggedIn = () => {
@@ -30,7 +62,7 @@ const useIsLoggedIn = () => {
     const auth = async () => {
       setIsAuth(await isUserAuthenticated());
     };
-    if (cookies?.isAuthenticated === true) {
+    if (cookies?.isAuthenticated === false) {
       auth();
     }
   }, [isAuth]);
