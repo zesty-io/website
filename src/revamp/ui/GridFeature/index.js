a44fe34999c710e90d78b1dd23b2d9d27e65f627
glespinosa <espinosagian1995@gmail.com>
2023-11-15 18:51:02 +0800

    fix(grid feature list images): grid feature list images
    grid feature list images

--- src/revamp/ui/GridFeature/index.js
+++ src/revamp/ui/GridFeature/index.js
@@ -125,6 +125,7 @@ const GridFeature = ({
   heading = 'Headless Interface/Code',
   supportingText = ``,
   featureLists = cardLists,
+  ...props
 }) => {
   const theme = useTheme();
   const isTablet = useMediaQuery(theme.breakpoints.up('tablet'), {
