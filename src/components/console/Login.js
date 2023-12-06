4c5fd95738e858bcfcba938b1650208073ebc66b
darwin.apolinario <darwin.apolinario@gmail.com>
2023-12-06 17:56:10 +0800

    fix(login): ui issues #2175

--- src/components/console/Login.js
+++ src/components/console/Login.js
@@ -32,10 +32,8 @@ const FormInput = dynamic(() =>
 const AlternateEmailIcon = dynamic(() =>
   import('@mui/icons-material/AlternateEmail'),
 );
-const PlayArrowIcon = dynamic(() => import('@mui/icons-material/PlayArrow'));
-const ImportContactsOutlinedIcon = dynamic(() =>
-  import('@mui/icons-material/ImportContactsOutlined'),
-);
+const TimeIcon = dynamic(() => import('@mui/icons-material/AccessTime'));
+const LaunchIcon = dynamic(() => import('@mui/icons-material/Launch'));
 const LoginIcon = dynamic(() => import('@mui/icons-material/Login'));
 const LoadingButton = dynamic(() => import('@mui/lab/LoadingButton'));
 
@@ -375,9 +373,9 @@ const Login = ({ content, userEmail }) => {
             justifyItems={'center'}
             alignContent={'center'}
             alignItems={'center'}
-            width={{ xs: '55vw', lg: '50vw', xl2: '70vw' }}
-            height={{ xs: '85vh', lg: '85vh' }}
-            spacing={2}
+            width={{ xs: '600px', lg: '650px', xl: '900px' }}
+            height={{ xs: '550px', lg: '650px', xl: '850px' }}
+            spacing={3}
             display={'flex'}
           >
             <Stack textAlign={'start'} justifyContent={'start'} width={1}>
@@ -391,8 +389,9 @@ const Login = ({ content, userEmail }) => {
                 {content?.title}
               </Typography>
               <Typography
+                width={1}
                 color="common.white"
-                fontSize={{ xs: '16px', xl2: '20px' }}
+                fontSize={{ xs: '15px', xl2: '20px' }}
               >
                 {content?.description}
               </Typography>
@@ -417,10 +416,10 @@ const Login = ({ content, userEmail }) => {
                     border: '1px solid white',
                   },
                 }}
-                startIcon={<ImportContactsOutlinedIcon />}
+                startIcon={<LaunchIcon />}
                 size="small"
               >
-                Read Docs
+                Read Announcement
               </Button>
               <Button
                 target="_blank"
@@ -436,7 +435,7 @@ const Login = ({ content, userEmail }) => {
                     bgcolor: 'white',
                   },
                 }}
-                startIcon={<PlayArrowIcon />}
+                startIcon={<TimeIcon />}
                 size="small"
               >
                 {content?.cta_type?.replace('_', ' ')}
--- src/pages/logout.js
+++ src/pages/logout.js
@@ -8,7 +8,7 @@ import {
 import axios from 'axios';
 import { deleteCookie } from 'cookies-next';
 import React, { useEffect } from 'react';
-import { useZestyStore } from 'store';
+// import { useZestyStore } from 'store';
 import * as helpers from 'utils';
 
 export { default as getServerSideProps } from 'lib/accounts/protectedRouteGetServerSideProps';
@@ -17,11 +17,11 @@ const purgeUrl =
   'https://us-central1-zesty-prod.cloudfunctions.net/fastlyPurge?zuid=8-aaeffee09b-7w6v22&instance=8-aaeffee09b-7w6v22';
 
 const logout = () => {
-  const { ZestyAPI } = useZestyStore((state) => state);
+  // const { ZestyAPI } = useZestyStore((state) => state);
 
   useEffect(() => {
     const logout = async () => {
-      await ZestyAPI.logout();
+      // await ZestyAPI.logout();
       // clear websites cache on logout
       await axios.get(purgeUrl);
       deleteCookie(helpers.isProd ? 'APP_SID' : 'DEV_APP_SID', {
