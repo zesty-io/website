09792eecbed955ce0d860b639c75f3c64fd6cf7c
root <root@PHLTZTIO002.gbs.local>
2024-01-24 09:13:23 -0800

    replace p with span tag

--- src/views/zesty/Article.js
+++ src/views/zesty/Article.js
@@ -678,13 +678,6 @@ function CtaComponent({ title, description, ctaText, ctaLink }) {
           <MuiMarkdown
             options={{
               overrides: {
-                p: {
-                  component: Typography,
-                  props: {
-                    variant: 'body1',
-                    color: '#D0D5DD',
-                  },
-                },
                 span: {
                   component: Typography,
                   props: {
