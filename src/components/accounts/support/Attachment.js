88bfd3a1d1cfe898760a7212d777d850210d7df2
Japheth Louie M. Gofredo <83058948+japhethLG@users.noreply.github.com>
2024-02-08 00:37:26 +0800

    feat: support portal (#2349)
    # Description
    
    Improved the UI and functionality of the ticket viewing section in the
    support portal. The updates include fixing the display of messages,
    images, and attachments, adding date and time displays for messages,
    ensuring only the agent's name is displayed per message, and enhancing
    attachment visibility for image and other file types. Implemented a dark
    mode compatible UI, introduced an information bar on the right side, and
    refactored the code for fetching threads and attachments to improve the
    loading experience. A regex fix was added to handle threads without
    content.
    
    
    ## Type of change
    
    Please delete options that are not relevant.
    
    - [x] Bug fix (non-breaking change which fixes an issue)
    - [x] New feature (non-breaking change which adds functionality)
    
    # How Has This Been Tested?
    
    Conducted manual testing by creating a ticket and using the Zoho portal
    to send and receive an email. This process was used to verify the
    correct display of messages, images, attachments, dates, and times, as
    well as the functionality of the new features.
    
    - [x] Manual Test
    
    # Screenshots / Screen recording
    
    ![Screenshot 2024-02-06
    154906](https://github.com/zesty-io/website/assets/83058948/179be2ac-2efc-4c95-9229-19cdfd9281bd)
    ![Screenshot 2024-02-06
    155009](https://github.com/zesty-io/website/assets/83058948/7b64b0c9-d51e-4c4f-b586-7efc4baf9e97)
    ![image
    viewer](https://github.com/zesty-io/website/assets/83058948/43e7a068-b1c9-4c78-bd3d-3fa3e3f957a9)
    ![dark
    mode](https://github.com/zesty-io/website/assets/83058948/8deb767b-6cab-4569-b30d-1a4ac6511a13)

---
+++ src/components/accounts/support/Attachment.js
@@ -0,0 +1,158 @@
+import { Box, Button, Modal, Typography } from '@mui/material';
+import ZestyImage from 'blocks/Image/ZestyImage';
+import DescriptionIcon from '@mui/icons-material/Description';
+import React, { useEffect, useState } from 'react';
+
+const DownloadButton = ({ file }) => {
+  const truncateText = (text, maxLength) => {
+    if (text.length <= maxLength) {
+      return text;
+    }
+    return text.substring(0, maxLength) + '...';
+  };
+
+  const handleDownload = async (event) => {
+    event.preventDefault();
+    const link = document.createElement('a');
+    link.href = file?.previewurl ?? '';
+    console.log('link.href ', link.href);
+    link.download = file?.name;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
+  return (
+    <Button
+      onClick={handleDownload}
+      sx={{
+        display: 'flex',
+        gap: 1,
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        background: '#F2F2F2',
+        borderRadius: 3,
+        p: 2,
+        maxWidth: '250px',
+        marginBottom: 0.2,
+      }}
+    >
+      <Typography
+        sx={{
+          display: 'flex',
+          gap: 1,
+          alignItems: 'center',
+          fontSize: 14,
+        }}
+      >
+        <DescriptionIcon />
+        {truncateText(file?.name, 25)}
+      </Typography>
+    </Button>
+  );
+};
+
+const ImagePreview = ({ file, handleOpen }) => {
+  return (
+    <Box
+      onClick={() => handleOpen(file?.previewurl)}
+      sx={{
+        cursor: 'pointer',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginBottom: 0.2,
+      }}
+    >
+      <ZestyImage
+        style={{
+          borderRadius: 10,
+          width: '100%',
+          maxWidth: '500px',
+        }}
+        alt="test"
+        src={file?.previewurl ?? ''}
+      />
+    </Box>
+  );
+};
+
+const Attachment = ({ attachments, threadIndex, getPreviewURL }) => {
+  const [open, setOpen] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const isImage = (file) => {
+    const imageExtensions = ['.png', '.jpg', '.jpeg', '.gif', '.bmp', '.webp'];
+    return imageExtensions.some((ext) => file.name.toLowerCase().endsWith(ext));
+  };
+
+  const handleOpen = (imageUrl) => {
+    setSelectedImage(imageUrl);
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  useEffect(() => {
+    const fetchPreviewURLs = async () => {
+      const promises = attachments.map((file, index) => {
+        if (!file.previewurl) {
+          return getPreviewURL({
+            attachment: file,
+            threadIndex,
+            attachmentIndex: index,
+          });
+        }
+        return Promise.resolve();
+      });
+
+      await Promise.all(promises);
+    };
+
+    fetchPreviewURLs();
+  }, []);
+
+  return (
+    <>
+      {attachments.map((file) => {
+        return (
+          <>
+            {isImage(file) ? (
+              <ImagePreview file={file} handleOpen={handleOpen} />
+            ) : (
+              <DownloadButton file={file} />
+            )}
+          </>
+        );
+      })}
+
+      <Modal open={open} onClose={handleClose}>
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100%',
+            width: '100%',
+            padding: 2,
+          }}
+          onClick={handleClose}
+        >
+          <ZestyImage
+            style={{
+              width: '80%',
+              height: '90%',
+              objectFit: 'contain',
+            }}
+            alt="image"
+            src={selectedImage}
+          />
+        </Box>
+      </Modal>
+    </>
+  );
+};
+
+export default Attachment;
