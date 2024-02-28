ab392097858383d5e3c731e7c0218893fcb5897f
Arzl James <arzljames15@gmail.com>
2024-02-29 07:41:52 +0800

    fix(forpersonaspage.js): fix broken links

--- src/views/zesty/ForPersonasPage.js
+++ src/views/zesty/ForPersonasPage.js
@@ -130,7 +130,7 @@ function ForPersonasPage({ content }) {
     description: content.case_studies?.data[0]?.summary,
     image: content.case_studies?.data[0]?.image?.data[0]?.url,
     ctaText: content.case_studies?.data[0]?.cta,
-    ctaLink: content.case_studies?.data[0]?.link,
+    ctaLink: content.case_studies?.data[0]?.card_link.data?.[0].meta?.web?.uri,
     stats_1_number: content.case_studies?.data[0]?.stats_1_number,
     stats_1_description: content.case_studies?.data[0]?.stats_1_description,
     stats_2_number: content.case_studies?.data[0]?.stats_2_number,
